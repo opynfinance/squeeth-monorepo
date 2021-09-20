dcfd0cb161c56a082da6012eae84d652c2845f72
Haythem Sellami <haythem96@users.noreply.github.com>
2021-09-20 15:53:24 +0100

    chore: doc generator (#190)
    * chore: doc gen
    
    * chore: add script

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -9,6 +9,7 @@ import "@tenderly/hardhat-tenderly";
 import "hardhat-deploy";
 import "@eth-optimism/hardhat-ovm";
 import "@nomiclabs/hardhat-ethers";
+import "hardhat-docgen";
 import { HardhatUserConfig } from "hardhat/config";
 import * as dotenv from 'dotenv'
 
@@ -58,6 +59,12 @@ const UNISWAP_SETTING = {
     },
   },
 };
+
+const docgen = {
+  path: './docs',
+  clear: true,
+  runOnCompile: false,
+}
 
 const config: HardhatUserConfig = {
   defaultNetwork,
@@ -222,7 +229,8 @@ const config: HardhatUserConfig = {
     gasPrice: 100,
     coinmarketcap: process.env.COINMARKETCAP,
     enabled: process.env.REPORT_GAS === "true",
-  }
+  },
+  docgen
 };
 
 export default config;
