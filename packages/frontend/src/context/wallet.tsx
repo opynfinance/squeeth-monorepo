ef5848f87202fa2abd10296d1bd1745ef097fb6b
Alexis Gauba <alexisgauba@gmail.com>
2022-01-12 11:35:16 -0800

    Fe: Disconnect wallet (#37)
    * fix: disconnect wallet
    
    * fix: check wallet state

--- packages/frontend/src/context/wallet.tsx
+++ packages/frontend/src/context/wallet.tsx
@@ -11,11 +11,13 @@ import useInterval from '@hooks/useInterval'
 import { Networks } from '../types'
 
 const useAlchemy = process.env.NEXT_PUBLIC_USE_ALCHEMY
-const defaultWeb3 = useAlchemy ? new Web3(`https://eth-mainnet.alchemyapi.io/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`) : new Web3(`https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_API_KEY}`)
+const defaultWeb3 = useAlchemy
+  ? new Web3(`https://eth-mainnet.alchemyapi.io/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`)
+  : new Web3(`https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_API_KEY}`)
 if (useAlchemy) {
-  console.log("using alchemy")
+  console.log('using alchemy')
 } else {
-  console.log("not using alchemy")
+  console.log('not using alchemy')
 }
 
 type WalletType = {
@@ -64,9 +66,11 @@ const WalletProvider: React.FC = ({ children }) => {
   const disconnectWallet = useCallback(async () => {
     if (!onboard) return
     await onboard.walletReset()
+    setAddress(null)
+    setBalance(new BigNumber(0))
   }, [onboard])
 
-  const setAddr = (address: string) => setAddress(address.toLowerCase())
+  const setAddr = (address: string) => setAddress(address)
 
   function addEtherscan(transaction: any) {
     if (networkId === Networks.LOCAL) return
@@ -136,6 +140,7 @@ const WalletProvider: React.FC = ({ children }) => {
           })
         }
       } else {
+        if (address === null) return
         onboard.walletCheck()
         console.log('Unsupported network')
       }
@@ -155,10 +160,10 @@ const WalletProvider: React.FC = ({ children }) => {
       networkId === Networks.LOCAL
         ? 'http://127.0.0.1:8545/'
         : networkId === Networks.ARBITRUM_RINKEBY
-          ? 'https://rinkeby.arbitrum.io/rpc'
-          : useAlchemy
-            ? `https://eth-${network}.alchemyapi.io/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`
-            : `https://${network}.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_API_KEY}`
+        ? 'https://rinkeby.arbitrum.io/rpc'
+        : useAlchemy
+        ? `https://eth-${network}.alchemyapi.io/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`
+        : `https://${network}.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_API_KEY}`
 
     const onboard = Onboard({
       dappId: process.env.NEXT_PUBLIC_BLOCKNATIVE_DAPP_ID,
