5e216796f9e98b72d7fa86e2844a629700d6a025
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-01-12 19:40:12 +0100

    fix: updates middleware to add query param to restricted user

--- packages/frontend/pages/_middleware.ts
+++ packages/frontend/pages/_middleware.ts
@@ -5,13 +5,8 @@ const BLOCKED_COUNTRIES = ['US', 'BY', 'CU', 'IR', 'IQ', 'CI', 'LR', 'KP', 'SD',
 export default function middleware(request: NextRequest) {
   const country = request.geo?.country
 
-  console.log(request.headers, 'HEADERS')
-  console.log(request.referrer, 'REFERRER')
-  console.log(request.url, 'URL')
-  console.log({ request })
-
   if (country && BLOCKED_COUNTRIES.includes(country)) {
-    return NextResponse.rewrite('/country-not-supported')
+    return NextResponse.rewrite(request.nextUrl.pathname + '?restricted=true')
   }
 
   return NextResponse.next()
