ca7711c87336b9bf16b781101f510d5f482ec6f6
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-01-12 19:00:37 +0100

    debug: adds logs to debug

--- packages/frontend/pages/_middleware.ts
+++ packages/frontend/pages/_middleware.ts
@@ -5,6 +5,11 @@ const BLOCKED_COUNTRIES = ['US', 'BY', 'CU', 'IR', 'IQ', 'CI', 'LR', 'KP', 'SD',
 export default function middleware(request: NextRequest) {
   const country = request.geo?.country
 
+  console.log(request.headers, 'HEADERS')
+  console.log(request.referrer, 'REFERRER')
+  console.log(request.url, 'URL')
+  console.log({ request })
+
   if (country && BLOCKED_COUNTRIES.includes(country)) {
     return NextResponse.rewrite('/country-not-supported')
   }
