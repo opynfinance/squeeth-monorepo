33c980047474a5f598c8368e02393fc1f94d2c54
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-01-12 17:42:29 +0100

    fix: updates middle ware to rewrite if cookie has not been set

--- packages/frontend/pages/_middleware.ts
+++ packages/frontend/pages/_middleware.ts
@@ -5,9 +5,10 @@ const BLOCKED_COUNTRIES = ['US', 'BY', 'CU', 'IR', 'IQ', 'CI', 'LR', 'KP', 'SD',
 export default function middleware(request: NextRequest) {
   const country = request.geo?.country
 
-  if (country && BLOCKED_COUNTRIES.includes(country)) {
-    // return NextResponse.rewrite('/country-not-supported')
-    return NextResponse.redirect('/country-not-supported')
+  const cookieFromRequest = request.cookies['restriction']
+
+  if (country && BLOCKED_COUNTRIES.includes(country) && !cookieFromRequest) {
+    return NextResponse.rewrite('/country-not-supported')
   }
 
   return NextResponse.rewrite(request.nextUrl)
