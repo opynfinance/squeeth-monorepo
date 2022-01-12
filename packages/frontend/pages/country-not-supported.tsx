246cf4ae7acff9b021dbe328f60845cda1dd88d1
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-01-12 16:21:44 +0100

    fix: updates country not supported to redirect user to app and set isRestricted state

--- packages/frontend/pages/country-not-supported.tsx
+++ packages/frontend/pages/country-not-supported.tsx
@@ -1,20 +1,30 @@
-import { useState } from 'react'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
-import { Modal } from '../src/components/Modal/Modal'
+import { useRestrictUser } from '@context/restrict-user'
+// import { Modal } from '../src/components/Modal/Modal'
 
 const App = () => {
-  const [open] = useState(true)
+  const router = useRouter()
+  const { handleRestrictUser } = useRestrictUser()
+  // const [open] = useState(true)
 
-  return (
-    <>
-      <Modal open={open} title="">
-        <div style={{ padding: '0 1em' }}>
-          <h1 style={{ textAlign: 'center', fontSize: '1.5rem' }}>Country Not supported</h1>
-          <p>Unfortunately, this app is not supported in your region. </p>
-        </div>
-      </Modal>
-    </>
-  )
+  useEffect(() => {
+    handleRestrictUser(true)
+    router.push('/')
+  }, [])
+
+  return null
+  // (
+  //   <>
+  //     <Modal open={open} title="">
+  //       <div style={{ padding: '0 1em' }}>
+  //         <h1 style={{ textAlign: 'center', fontSize: '1.5rem' }}>Country Not supported</h1>
+  //         <p>Unfortunately, this app is not supported in your region. </p>
+  //       </div>
+  //     </Modal>
+  //   </>
+  // )
 }
 
 export default App
