1aca2d064f27bbeeb7731186a07dd159fedb4165
akwritescode <63263190+akwritescode@users.noreply.github.com>
2022-01-13 15:51:03 -0500

    revert useInterval (#50)

--- packages/frontend/src/hooks/useInterval.ts
+++ packages/frontend/src/hooks/useInterval.ts
@@ -1,24 +1,24 @@
-import { useEffect, useLayoutEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 
-function useInterval(callback: () => void, delay: number | null) {
-  const savedCallback = useRef(callback)
+type Callback = () => void
 
-  // Remember the latest callback if it changes.
-  useLayoutEffect(() => {
+export function useInterval(callback: Callback, delay: number) {
+  const savedCallback = useRef<Callback>()
+
+  // Remember the latest callback.
+  useEffect(() => {
     savedCallback.current = callback
   }, [callback])
 
   // Set up the interval.
   useEffect(() => {
-    // Don't schedule if no delay is specified.
-    // Note: 0 is a valid value for delay.
-    if (!delay && delay !== 0) {
-      return
+    function tick() {
+      savedCallback.current?.()
     }
-
-    const id = setInterval(() => savedCallback.current(), delay)
-
-    return () => clearInterval(id)
+    if (delay !== null) {
+      const id = setInterval(tick, delay)
+      return () => clearInterval(id)
+    }
   }, [delay])
 }
 
