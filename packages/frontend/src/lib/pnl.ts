0ed35e60ef204a880719097a6b119f95ebdd0653
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-01-18 22:16:51 +0100

    fix: removes redundant code

--- packages/frontend/src/lib/pnl.ts
+++ packages/frontend/src/lib/pnl.ts
@@ -13,45 +13,6 @@ interface Deposit {
   ethPriceAtDeposit: BigNumber
 }
 
-interface LongDeposit extends Deposit {
-  sellQuote: BigNumber
-}
-
-interface GetCurrentLongParams {
-  positionType: string
-  squeethAmount: BigNumber
-  transactions: any[]
-  getSellQuote: (input: BigNumber) => Promise<Record<string, any>>
-}
-
-export async function getCurrentLong({
-  getSellQuote,
-  positionType,
-  squeethAmount,
-  transactions,
-}: GetCurrentLongParams) {
-  if (positionType === PositionType.SHORT) return []
-  let totalLongSqth = new BigNumber(0)
-
-  const result: LongDeposit[] = []
-  let sellQuote
-  for (let index = 0; index < transactions.length; index++) {
-    if (totalLongSqth.gte(squeethAmount)) break
-    if (totalLongSqth.isLessThan(squeethAmount) && transactions[index].transactionType === TransactionType.BUY) {
-      totalLongSqth = totalLongSqth.plus(transactions[index].squeethAmount)
-      sellQuote = await getSellQuote(transactions[index].squeethAmount)
-      result.push({ ...transactions[index], sellQuote: sellQuote.amountOut })
-    } else if (
-      totalLongSqth.isLessThan(squeethAmount) &&
-      transactions[index].transactionType === TransactionType.SELL
-    ) {
-      totalLongSqth = totalLongSqth.minus(transactions[index].squeethAmount)
-    }
-  }
-
-  return result
-}
-
 interface ShortDeposit extends Deposit {
   buyQuote: BigNumber
 }
@@ -63,17 +24,12 @@ interface GetCurrentShortParams {
   getBuyQuote: (input: BigNumber) => Promise<Record<string, any>>
 }
 
-export async function getCurrentShortDeposits({
-  getBuyQuote,
-  positionType,
-  squeethAmount,
-  transactions,
-}: GetCurrentShortParams) {
+export function getCurrentShortDeposits({ positionType, squeethAmount, transactions }: GetCurrentShortParams) {
   if (positionType === PositionType.LONG) return []
   let totalShortSqth = new BigNumber(0)
 
   const result: ShortDeposit[] = []
-  let buyQuote
+  // let buyQuote
   for (let index = 0; index < transactions.length; index++) {
     if (totalShortSqth.gte(squeethAmount)) break
     if (
@@ -81,8 +37,8 @@ export async function getCurrentShortDeposits({
       transactions[index].transactionType === TransactionType.MINT_SHORT
     ) {
       totalShortSqth = totalShortSqth.plus(transactions[index].squeethAmount)
-      buyQuote = await getBuyQuote(transactions[index].squeethAmount)
-      result.push({ ...transactions[index], buyQuote: buyQuote.amountIn })
+      // buyQuote = await getBuyQuote(transactions[index].squeethAmount)
+      result.push(transactions[index])
     } else if (
       totalShortSqth.isLessThan(squeethAmount) &&
       transactions[index].transactionType === TransactionType.BURN_SHORT
