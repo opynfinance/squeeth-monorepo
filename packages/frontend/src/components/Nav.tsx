67de9f33f3ff1a29607b1773c1813535232aa780
Alexis Gauba <alexisgauba@gmail.com>
2022-01-19 18:35:16 -0800

    fix: update backtest title (#80)
    * Add startegy FE in ropsten
    
    * fix: update historical pnl title a nd add to component
    
    * chore: remove old hook
    
    Co-authored-by: Koushik KM <koushikmohan1996@gmail.com>

--- packages/frontend/src/components/Nav.tsx
+++ packages/frontend/src/components/Nav.tsx
@@ -197,7 +197,7 @@ const Nav: React.FC = () => {
               )}
             </Button>
             <NavLink path="/" name="Trade" />
-            {/* <NavLink path="/strategies" name="Strategies" /> */}
+            <NavLink path="/strategies" name="Strategies" />
             <NavLink path="/positions" name="Positions" />
             <NavLink path="/lp" name="LP" />
             <a href="https://opyn.gitbook.io/squeeth/resources/squeeth-faq" target="_blank" rel="noreferrer">
