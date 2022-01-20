bb3ecfb7d7f9e948f99ab3a61f033d8838303c2b
Koushik KM <koushikmohan1996@gmail.com>
2022-01-20 06:09:58 +0530

    Add crab in position tab

--- packages/frontend/src/constants/enums.ts
+++ packages/frontend/src/constants/enums.ts
@@ -61,6 +61,7 @@ export enum Tooltips {
   ShortDebt = 'This is squeeth that you acquired and sold',
   TotalDebt = 'Debt of the vault',
   VaultLiquidations = 'The strategy is subject to liquidations if it goes below 150% collateral, but rebalancing based on large ETH price changes helps prevent a liquidation from occurring.',
+  MinCrabPnL = 'This is the minimum Pnl that you get because of slippage',
 }
 
 export enum Links {
