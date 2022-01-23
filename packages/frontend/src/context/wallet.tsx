746879f44c98e8af3b876570cc9756ac5b04a099
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-01-23 19:54:35 +0100

    refactor: updates wallet context

--- packages/frontend/src/context/wallet.tsx
+++ packages/frontend/src/context/wallet.tsx
@@ -113,6 +113,10 @@ const WalletProvider: React.FC = ({ children }) => {
   }
 
   useEffect(() => {
+    if (!address) {
+      setBalance(new BigNumber(0))
+      return
+    }
     getBalance()
   }, [address])
 
