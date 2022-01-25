8131d80e359ce282d7ce30fa1dbf24e11572635c
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-01-25 08:55:23 +0100

    fix: updates unrealized pnl calc to ensure a valid value is returned

--- packages/frontend/src/lib/pnl.ts
+++ packages/frontend/src/lib/pnl.ts
@@ -7,7 +7,14 @@ type ShortPnLParams = {
 }
 
 export function calcUnrealizedPnl({ wethAmount, buyQuote, ethPrice }: ShortPnLParams) {
-  if (wethAmount.isEqualTo(0) || buyQuote.isEqualTo(0) || ethPrice.isEqualTo(0)) {
+  if (
+    wethAmount.isEqualTo(0) ||
+    !wethAmount.isFinite() ||
+    buyQuote.isEqualTo(0) ||
+    !buyQuote.isFinite() ||
+    ethPrice.isEqualTo(0) ||
+    !ethPrice.isFinite()
+  ) {
     return new BigNumber(0)
   }
   return wethAmount.minus(buyQuote).multipliedBy(ethPrice)
