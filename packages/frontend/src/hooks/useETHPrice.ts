0a7468ec62ee4693e6c381a85ea9cb373733b5c7
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-02-09 15:18:06 +0100

    feat: adds function to fetch data from api

--- packages/frontend/src/hooks/useETHPrice.ts
+++ packages/frontend/src/hooks/useETHPrice.ts
@@ -47,3 +47,15 @@ export const getETHPriceCoingecko = async (): Promise<BigNumber> => {
   const price = priceStruct.usd
   return new BigNumber(price)
 }
+
+export const getHistoricEthPrice = async (dateString: string): Promise<BigNumber> => {
+  const pair = 'ETH/USD'
+
+  const response = await fetch(
+    `https://api.twelvedata.com/time_series?start_date=${dateString}&end_date=${dateString}&symbol=${pair}&interval=1min&apikey=${process.env.NEXT_PUBLIC_TWELVEDATA_APIKEY}`,
+  ).then((res) => res.json())
+
+  if (response.status === 'error') return new BigNumber(0)
+
+  return new BigNumber(Number(response.values[0].close))
+}
