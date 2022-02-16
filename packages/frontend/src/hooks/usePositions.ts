120083fbe9d48b462926370c8abca9aa30011770
E <>
2022-02-16 00:23:41 -0500

    use index atom in usePositions hook

--- packages/frontend/src/hooks/usePositions.ts
+++ packages/frontend/src/hooks/usePositions.ts
@@ -16,10 +16,11 @@ import { toTokenAmount } from '@utils/calculations'
 import { squeethClient } from '@utils/apollo-client'
 
 import { useSqueethPool } from './contracts/useSqueethPool'
-import { useController } from './contracts/useController'
+import { indexAtom } from './contracts/useController'
 import { useAddresses } from './useAddress'
 import { calcDollarShortUnrealizedpnl, calcETHCollateralPnl, calcDollarLongUnrealizedpnl } from '../lib/pnl'
 import { BIG_ZERO } from '../constants/'
+import { useAtom } from 'jotai'
 
 export const usePnL = () => {
   const [ethCollateralPnl, setEthCollateralPnl] = useState(BIG_ZERO)
@@ -36,7 +37,7 @@ export const usePnL = () => {
     firstValidVault,
     existingCollat,
   } = usePositions()
-  const { index } = useController()
+  const [index] = useAtom(indexAtom)
 
   // const { ethPrice } = useWorldContext()
   const { ready, getSellQuote, getBuyQuote } = useSqueethPool()
