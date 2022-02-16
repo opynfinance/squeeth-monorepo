c66f53a4043190c3c95f7ddc51fb7904fae8d21d
Haythem Sellami <sellamihaythem96@gmail.com>
2022-02-16 15:10:12 -0700

    feat: init

--- packages/hardhat/contracts/periphery/ControllerHelper.sol
+++ packages/hardhat/contracts/periphery/ControllerHelper.sol
@@ -34,7 +34,8 @@ contract ControllerHelper is FlashControllerHelper, IERC721Receiver {
 
     /// @dev enum to differentiate between uniswap swap callback function source
     enum FLASH_SOURCE {
-        FLASH_W_MINT
+        FLASH_W_MINT,
+        FLASH_W_BURN
     }
 
     address public immutable controller;
@@ -141,6 +142,19 @@ contract ControllerHelper is FlashControllerHelper, IERC721Receiver {
         emit FlashswapWMint(msg.sender, _vaultId, _wPowerPerpAmount, amountToFlashswap, _collateralAmount);
     }
 
+    function flashWBurn(uint256 _vaultId, uint256 _wPowerPerpAmount, uint256 _collateralToWithdraw) external {
+        _exactOutFlashSwap(
+            weth,
+            wPowerPerp,
+            IUniswapV3Pool(wPowerPerpPool).fee(),
+            _wPowerPerpAmount,
+            _collateralToWithdraw,
+            uint8(FLASH_SOURCE.FLASH_W_BURN),
+            abi.encodePacked(_vaultId, _wPowerPerpAmount, _collateralToWithdraw)
+        );
+
+    }
+
     /**
      * @notice flash close position and buy long squeeth
      * @dev this function
@@ -227,6 +241,16 @@ contract ControllerHelper is FlashControllerHelper, IERC721Receiver {
             if (address(this).balance > 0) {
                 payable(_caller).sendValue(address(this).balance);
             }
+        }
+        else if (FLASH_SOURCE(_callSource) == FLASH_SOURCE.FLASH_W_BURN) {
+            FlashWBurnData memory data = abi.decode(_callData, (FlashWBurnData));
+
+            IController(controller).burnWPowerPerpAmount(data.vaultId, data.wPowerPerpAmount, data.collateralToWithdraw);
+
+            IWETH9.deposit(_amountToPay);
+            IWETH9(weth).transfer(wPowerPerpPool, _amountToPay);
+
+            /// TODO: buy long or send ETH back
         }
     }
 }
