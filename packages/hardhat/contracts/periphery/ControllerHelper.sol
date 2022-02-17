577b4e4675b570c0d372d4090054d0ce0e6b6896
Haythem Sellami <sellamihaythem96@gmail.com>
2022-02-16 17:11:43 -0700

    fix compile

--- packages/hardhat/contracts/periphery/ControllerHelper.sol
+++ packages/hardhat/contracts/periphery/ControllerHelper.sol
@@ -385,7 +385,7 @@ contract ControllerHelper is FlashControllerHelper, IERC721Receiver {
 
             IController(controller).burnWPowerPerpAmount(data.vaultId, data.wPowerPerpAmount, data.collateralToWithdraw);
 
-            IWETH9.deposit(_amountToPay);
+            IWETH9(weth).deposit{value: _amountToPay}();
             IWETH9(weth).transfer(wPowerPerpPool, _amountToPay);
 
             /// TODO: buy long or send ETH back
