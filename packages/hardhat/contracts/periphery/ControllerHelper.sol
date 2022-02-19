498c0e1ef3428ece0563594a2bc3b22d6b71dfeb
Haythem Sellami <sellamihaythem96@gmail.com>
2022-02-19 11:18:55 -0700

    feat flash burn and buy long

--- packages/hardhat/contracts/periphery/ControllerHelper.sol
+++ packages/hardhat/contracts/periphery/ControllerHelper.sol
@@ -116,6 +116,8 @@ contract ControllerHelper is FlashControllerHelper, IERC721Receiver {
 
     event FlashWBurn(address indexed withdrawer, uint256 vaultId, uint256 wPowerPerpAmount, uint256 collateralAmount, uint256 wPowerPerpBought);    
 
+    event FlashWBurn(address indexed withdrawer, uint256 vaultId, uint256 wPowerPerpAmount, uint256 collateralAmount, uint256 wPowerPerpBought);    
+
     constructor(
         address _controller,
         address _oracle,
