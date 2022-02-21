6ac1f0729dfb6abd3dd29e909716655e8b40a204
EJ Liu <jacvd7@gmail.com>
2022-02-20 17:09:43 -0700

    feat: add vault history query

--- packages/frontend/src/queries/squeeth/vaultHistoryQuery.ts
+++ packages/frontend/src/queries/squeeth/vaultHistoryQuery.ts
@@ -1,7 +1,21 @@
 import { gql } from '@apollo/client'
 
 const VAULT_HISTORY_QUERY = gql`
-  query VaultHistory($vaultId: ID!) {
+  query VaultHistory($vaultId: BigInt!) {
+    vaultHistories(where: { vaultId: $vaultId }) {
+      id
+      timestamp
+      totalEthCollateralAmount
+      oSqthAmount
+      ethCollateralAmount
+      action
+      vaultId
+    }
+  }
+`
+
+export const VAULT_HISTORY_SUBSCRIPTION = gql`
+  subscription subscriptionVaultHistory($vaultId: BigInt!) {
     vaultHistories(where: { vaultId: $vaultId }) {
       id
       timestamp
