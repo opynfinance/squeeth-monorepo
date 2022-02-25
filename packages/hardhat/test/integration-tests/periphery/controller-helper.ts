4ce643135a0106fde4f71682423f5b2b04d1c86d
Haythem Sellami <sellamihaythem96@gmail.com>
2022-02-24 18:26:00 -0700

    rebase:

--- packages/hardhat/test/integration-tests/periphery/controller-helper.ts
+++ packages/hardhat/test/integration-tests/periphery/controller-helper.ts
@@ -73,7 +73,7 @@ describe("Controller helper integration test", function () {
     await controller.connect(owner).setFeeRate(0)
     
     const ControllerHelperContract = await ethers.getContractFactory("ControllerHelper");
-    controllerHelper = (await ControllerHelperContract.deploy(controller.address, oracle.address, shortSqueeth.address, wSqueethPool.address, wSqueeth.address, weth.address, swapRouter.address, uniswapFactory.address)) as ControllerHelper;
+    controllerHelper = (await ControllerHelperContract.deploy(controller.address, oracle.address, shortSqueeth.address, wSqueethPool.address, wSqueeth.address, weth.address, swapRouter.address, positionManager.address, uniswapFactory.address)) as ControllerHelper;
   })
 
   this.beforeAll("Seed pool liquidity", async() => {
@@ -165,23 +165,6 @@ describe("Controller helper integration test", function () {
       expect(vaultAfter.shortAmount.eq(BigNumber.from(0))).to.be.true
       expect(vaultAfter.collateralAmount.eq(BigNumber.from(0))).to.be.true
       expect(longBalanceAfter.sub(longBalanceBefore).eq(squeethToBuy)).to.be.true
-    })
-
-    it("flash close short position and buy long", async () => {
-      const vaultId = (await shortSqueeth.nextId()).sub(1);
-      await controller.connect(depositor).updateOperator(vaultId, controllerHelper.address)
-
-      const vaultBefore = await controller.vaults(vaultId)
-      const longBalanceBefore = await wSqueeth.balanceOf(depositor.address)
-
-      await controllerHelper.connect(depositor).flashWBurn(vaultId, vaultBefore.shortAmount, vaultBefore.collateralAmount, BigNumber.from(0));
-
-      const vaultAfter = await controller.vaults(vaultId)
-      const longBalanceAfter = await wSqueeth.balanceOf(depositor.address)
-
-      expect(vaultAfter.shortAmount.eq(BigNumber.from(0))).to.be.true
-      expect(vaultAfter.collateralAmount.eq(BigNumber.from(0))).to.be.true
-      expect(longBalanceAfter.gt(longBalanceBefore)).to.be.true
     })
   })
 
