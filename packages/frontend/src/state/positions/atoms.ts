3f940098bc4faccd9a5badf0c5ca6fec39a52aca
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-02-28 16:52:07 +0100

    refactor: updates positions atoms

--- packages/frontend/src/state/positions/atoms.ts
+++ packages/frontend/src/state/positions/atoms.ts
@@ -1,6 +1,6 @@
 import { atom } from 'jotai'
 
-import { PositionType } from '../../types'
+import { PositionType, Vault } from '../../types'
 import {
   CONTROLLER,
   USDC,
@@ -17,8 +17,9 @@ import {
   ZERO_ADDR,
   CRAB_STRATEGY,
 } from '@constants/address'
-import { networkIdAtom } from '../wallet/atoms'
+import { networkIdAtom, web3Atom } from '../wallet/atoms'
 import { BIG_ZERO } from '@constants/index'
+import NFTpositionManagerABI from '../../abis/NFTpositionmanager.json'
 
 export const positionTypeAtom = atom(PositionType.NONE)
 export const firstValidVaultAtom = atom(0)
@@ -47,12 +48,24 @@ export const isWethToken0Atom = atom((get) => {
   return parseInt(addresses.weth, 16) < parseInt(addresses.oSqueeth, 16)
 })
 
-export const activePositionsAtom = atom([])
-export const closedPositionsAtom = atom([])
-export const lpPositionsLoading = atom(false)
+export const managerAtom = atom((get) => {
+  const { nftManager } = get(addressesAtom)
+  const web3 = get(web3Atom)
+  return new web3.eth.Contract(NFTpositionManagerABI as any, nftManager?.toLowerCase() || '')
+})
+export const activePositionsAtom = atom<any[]>([])
+export const closedPositionsAtom = atom<any[]>([])
+export const lpPositionsLoadingAtom = atom(false)
 export const squeethLiquidityAtom = atom(BIG_ZERO)
 export const wethLiquidityAtom = atom(BIG_ZERO)
 export const depositedSqueethAtom = atom(BIG_ZERO)
-export const depositedWeth = atom(BIG_ZERO)
-export const withdrawnSqueeth = atom(BIG_ZERO)
-export const withdrawnWeth = atom(BIG_ZERO)
+export const depositedWethAtom = atom(BIG_ZERO)
+export const withdrawnSqueethAtom = atom(BIG_ZERO)
+export const withdrawnWethAtom = atom(BIG_ZERO)
+
+export const vaultAtom = atom<Vault | null>(null)
+export const existingCollatPercentAtom = atom(0)
+export const existingCollatAtom = atom(BIG_ZERO)
+export const existingLiqPriceAtom = atom(BIG_ZERO)
+export const collatPercentAtom = atom(0)
+export const isVaultLoadingAtom = atom(true)
