c330dcfafdc66c35f95325eed2e5c2449c12395d
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-02-28 16:50:39 +0100

    adds squeethPool hooks

---
+++ packages/frontend/src/state/squeethPool/hooks.ts
@@ -0,0 +1,509 @@
+import { Contract } from 'web3-eth-contract'
+import { Token, CurrencyAmount } from '@uniswap/sdk-core'
+import { Pool, Route, Trade } from '@uniswap/v3-sdk'
+import { useUpdateAtom } from 'jotai/utils'
+import { useEffect, useCallback } from 'react'
+import { atom, useAtomValue } from 'jotai'
+import BigNumber from 'bignumber.js'
+import { ethers } from 'ethers'
+
+import useContract from '@hooks/useContract'
+import routerABI from '../../abis/swapRouter.json'
+import uniABI from '../../abis/uniswapPool.json'
+import { addressesAtom, isWethToken0Atom } from '../positions/atoms'
+import { addressAtom, networkIdAtom, web3Atom } from '../wallet/atoms'
+import useUniswapTicks from '@hooks/useUniswapTicks'
+import { DEFAULT_SLIPPAGE, OSQUEETH_DECIMALS, UNI_POOL_FEES } from '@constants/index'
+import { fromTokenAmount, parseSlippageInput } from '@utils/calculations'
+import { useETHPrice } from '@hooks/useETHPrice'
+import { useHandleTransaction } from '../wallet/hooks'
+import {
+  poolAtom,
+  wethTokenAtom,
+  squeethTokenAtom,
+  squeethInitialPriceAtom,
+  squeethPriceeAtom,
+  readyAtom,
+} from './atoms'
+
+const getImmutables = async (squeethContract: Contract) => {
+  return {
+    token0: await squeethContract?.methods.token0().call(),
+    token1: await squeethContract?.methods.token1().call(),
+    fee: await squeethContract?.methods.fee().call(),
+    tickSpacing: await squeethContract?.methods.tickSpacing().call(),
+    maxLiquidityPerTick: await squeethContract?.methods.maxLiquidityPerTick().call(),
+  }
+}
+
+async function getPoolState(squeethContract: Contract) {
+  const slot = await squeethContract?.methods.slot0().call()
+  const PoolState = {
+    liquidity: await squeethContract?.methods.liquidity().call(),
+    sqrtPriceX96: slot[0],
+    tick: slot[1],
+    observationIndex: slot[2],
+    observationCardinality: slot[3],
+    observationCardinalityNext: slot[4],
+    feeProtocol: slot[5],
+    unlocked: slot[6],
+  }
+
+  return PoolState
+}
+
+export const useUpdateSqueethPoolData = () => {
+  const isWethToken0 = useAtomValue(isWethToken0Atom)
+  const networkId = useAtomValue(networkIdAtom)
+  const setPool = useUpdateAtom(poolAtom)
+  const setWethToken = useUpdateAtom(wethTokenAtom)
+  const setSqueethToken = useUpdateAtom(squeethTokenAtom)
+  const { squeethPool } = useAtomValue(addressesAtom)
+  const contract = useContract(squeethPool, uniABI)
+  const { ticks } = useUniswapTicks()
+  useEffect(() => {
+    ;(async () => {
+      const { token0, token1, fee } = await getImmutables(contract!)
+
+      const state = await getPoolState(contract!)
+      const TokenA = new Token(
+        networkId,
+        token0,
+        isWethToken0 ? 18 : OSQUEETH_DECIMALS,
+        isWethToken0 ? 'WETH' : 'SQE',
+        isWethToken0 ? 'Wrapped Ether' : 'oSqueeth',
+      )
+      const TokenB = new Token(
+        networkId,
+        token1,
+        isWethToken0 ? OSQUEETH_DECIMALS : 18,
+        isWethToken0 ? 'SQE' : 'WETH',
+        isWethToken0 ? 'oSqueeth' : 'Wrapped Ether',
+      )
+
+      const pool = new Pool(
+        TokenA,
+        TokenB,
+        Number(fee),
+        state.sqrtPriceX96.toString(),
+        state.liquidity.toString(),
+        Number(state.tick),
+        ticks || [],
+      )
+
+      setPool(pool)
+      setWethToken(isWethToken0 ? TokenA : TokenB)
+      setSqueethToken(isWethToken0 ? TokenB : TokenA)
+    })()
+  }, [contract, isWethToken0, networkId, setPool, setSqueethToken, setWethToken, ticks])
+}
+
+export const useGetBuyQuoteForETH = () => {
+  const pool = useAtomValue(poolAtom)
+  const wethToken = useAtomValue(wethTokenAtom)
+  const squeethToken = useAtomValue(squeethTokenAtom)
+  //If I input an exact amount of ETH I want to spend, tells me how much Squeeth I'd purchase
+  const getBuyQuoteForETH = async (ETHAmount: BigNumber, slippageAmount = new BigNumber(DEFAULT_SLIPPAGE)) => {
+    const emptyState = {
+      amountOut: new BigNumber(0),
+      minimumAmountOut: new BigNumber(0),
+      priceImpact: '0',
+    }
+
+    if (!ETHAmount || !pool) return emptyState
+
+    try {
+      //WETH is input token, squeeth is output token. I'm using WETH to buy Squeeth
+      const route = new Route([pool], wethToken!, squeethToken!)
+      //getting the amount of squeeth I'd get out for putting in an exact amount of ETH
+      const trade = await Trade.exactIn(
+        route,
+        CurrencyAmount.fromRawAmount(wethToken!, fromTokenAmount(ETHAmount, 18).toString()),
+      )
+
+      //the amount of squeeth I'm getting out
+      return {
+        amountOut: new BigNumber(trade.outputAmount.toSignificant(OSQUEETH_DECIMALS)),
+        minimumAmountOut: new BigNumber(
+          trade.minimumAmountOut(parseSlippageInput(slippageAmount.toString())).toSignificant(OSQUEETH_DECIMALS),
+        ),
+        priceImpact: trade.priceImpact.toFixed(2),
+      }
+    } catch (e) {
+      console.log(e)
+    }
+
+    return emptyState
+  }
+
+  return getBuyQuoteForETH
+}
+
+export const useUpdateSqueethPrices = () => {
+  const setSqueethInitialPrice = useUpdateAtom(squeethInitialPriceAtom)
+  const setSqueethPrice = useUpdateAtom(squeethPriceeAtom)
+  const setReady = useUpdateAtom(readyAtom)
+
+  const squeethToken = useAtomValue(squeethTokenAtom)
+  const pool = useAtomValue(poolAtom)
+  const isWethToken0 = useAtomValue(isWethToken0Atom)
+  const getBuyQuoteForETH = useGetBuyQuoteForETH()
+  useEffect(() => {
+    if (!squeethToken?.address || !pool) return
+    getBuyQuoteForETH(new BigNumber(1))
+      .then((val) => {
+        setSqueethPrice(val.amountOut)
+        setSqueethInitialPrice(
+          new BigNumber(
+            !isWethToken0 ? pool?.token0Price.toSignificant(18) || 0 : pool?.token1Price.toSignificant(18) || 0,
+          ),
+        )
+        setReady(true)
+      })
+      .catch(console.log)
+  }, [squeethToken?.address, pool?.token1Price.toFixed(18)])
+}
+
+export const useGetWSqueethPositionValue = () => {
+  const ethPrice = useETHPrice()
+  const squeethInitialPrice = useAtomValue(squeethInitialPriceAtom)
+  const getWSqueethPositionValue = (amount: BigNumber | number) => {
+    return new BigNumber(amount).times(squeethInitialPrice).times(ethPrice)
+  }
+
+  return getWSqueethPositionValue
+}
+
+export const useGetWSqueethPositionValueInETH = () => {
+  const squeethInitialPrice = useAtomValue(squeethInitialPriceAtom)
+  const getWSqueethPositionValueInETH = (amount: BigNumber | number) => {
+    return new BigNumber(amount).times(squeethInitialPrice)
+  }
+
+  return getWSqueethPositionValueInETH
+}
+
+export const useGetBuyQuote = () => {
+  const pool = useAtomValue(poolAtom)
+  const wethToken = useAtomValue(wethTokenAtom)
+  const squeethToken = useAtomValue(squeethTokenAtom)
+  //If I input an exact amount of squeeth I want to buy, tells me how much ETH I need to pay to purchase that squeeth
+  const getBuyQuote = useCallback(
+    async (squeethAmount: BigNumber, slippageAmount = new BigNumber(DEFAULT_SLIPPAGE)) => {
+      const emptyState = {
+        amountIn: new BigNumber(0),
+        maximumAmountIn: new BigNumber(0),
+        priceImpact: '0',
+      }
+
+      if (!squeethAmount || !pool) return emptyState
+
+      try {
+        //WETH is input token, squeeth is output token. I'm using WETH to buy Squeeth
+        const route = new Route([pool], wethToken!, squeethToken!)
+        //getting the amount of ETH I need to put in to get an exact amount of squeeth I inputted out
+        const trade = await Trade.exactOut(
+          route,
+          CurrencyAmount.fromRawAmount(squeethToken!, fromTokenAmount(squeethAmount, OSQUEETH_DECIMALS).toFixed(0)),
+        )
+
+        //the amount of ETH I need to put in
+        return {
+          amountIn: new BigNumber(trade.inputAmount.toSignificant(18)),
+          maximumAmountIn: new BigNumber(
+            trade.maximumAmountIn(parseSlippageInput(slippageAmount.toString())).toSignificant(18),
+          ),
+          priceImpact: trade.priceImpact.toFixed(2),
+        }
+      } catch (e) {
+        console.log(e)
+      }
+
+      return emptyState
+    },
+    [pool?.token0Price, wethToken?.address, squeethToken?.address],
+  )
+
+  return getBuyQuote
+}
+
+export const useGetBuyParam = () => {
+  const address = useAtomValue(addressAtom)
+  const squeethToken = useAtomValue(squeethTokenAtom)
+  const wethToken = useAtomValue(wethTokenAtom)
+  const getBuyQuote = useGetBuyQuote()
+  const getBuyParam = async (amount: BigNumber) => {
+    const amountMax = fromTokenAmount((await getBuyQuote(amount)).maximumAmountIn, 18)
+
+    return {
+      tokenIn: wethToken?.address, // address
+      tokenOut: squeethToken?.address, // address
+      fee: UNI_POOL_FEES, // uint24
+      recipient: address, // address
+      deadline: Math.floor(Date.now() / 1000 + 86400), // uint256
+      amountOut: fromTokenAmount(amount, OSQUEETH_DECIMALS).toString(), // uint256
+      amountInMaximum: amountMax.toString(),
+      sqrtPriceLimitX96: 0, // uint160
+    }
+  }
+
+  return getBuyParam
+}
+
+export const useBuy = () => {
+  const { swapRouter } = useAtomValue(addressesAtom)
+  const address = useAtomValue(addressAtom)
+  const swapRouterContract = useContract(swapRouter, routerABI)
+  const handleTransaction = useHandleTransaction()
+  const getBuyParam = useGetBuyParam()
+  const buy = async (amount: BigNumber) => {
+    const exactOutputParam = await getBuyParam(amount)
+
+    await handleTransaction(
+      swapRouterContract?.methods.exactOutputSingle(exactOutputParam).send({
+        from: address,
+      }),
+    )
+  }
+  return buy
+}
+
+export const useGetBuyParamForETH = () => {
+  const address = useAtomValue(addressAtom)
+  const squeethToken = useAtomValue(squeethTokenAtom)
+  const wethToken = useAtomValue(wethTokenAtom)
+  const getBuyQuoteForETH = useGetBuyQuoteForETH()
+  const getBuyParamForETH = async (amount: BigNumber) => {
+    const quote = await getBuyQuoteForETH(amount)
+
+    return {
+      tokenIn: wethToken?.address,
+      tokenOut: squeethToken?.address,
+      fee: UNI_POOL_FEES,
+      recipient: address,
+      deadline: Math.floor(Date.now() / 1000 + 86400), // uint256
+      amountIn: ethers.utils.parseEther(amount.toString()),
+      amountOutMinimum: fromTokenAmount(quote.minimumAmountOut, OSQUEETH_DECIMALS).toString(),
+      sqrtPriceLimitX96: 0,
+    }
+  }
+
+  return getBuyParamForETH
+}
+
+export const useBuyForWETH = () => {
+  const address = useAtomValue(addressAtom)
+  const { swapRouter } = useAtomValue(addressesAtom)
+  const handleTransaction = useHandleTransaction()
+  const swapRouterContract = useContract(swapRouter, routerABI)
+  const getBuyParamForETH = useGetBuyParamForETH()
+  const buyForWETH = async (amount: BigNumber) => {
+    const exactInputParam = await getBuyParamForETH(new BigNumber(amount))
+
+    const txHash = await handleTransaction(
+      swapRouterContract?.methods.exactInputSingle(exactInputParam).send({
+        from: address,
+        value: ethers.utils.parseEther(amount.toString()),
+      }),
+    )
+
+    return txHash
+  }
+
+  return buyForWETH
+}
+
+export const useBuyAndRefundData = () => {
+  const address = useAtomValue(addressAtom)
+  const web3 = useAtomValue(web3Atom)
+  const getBuyParamForETH = useGetBuyParamForETH()
+  const buyAndRefundData = async (amount: BigNumber) => {
+    if (!web3) return
+    const exactInputParam = await getBuyParamForETH(amount)
+    exactInputParam.recipient = address
+    const tupleInput = Object.values(exactInputParam).map((v) => v?.toString() || '')
+
+    const swapIface = new ethers.utils.Interface(routerABI)
+    const encodedSwapCall = swapIface.encodeFunctionData('exactInputSingle', [tupleInput])
+    const encodedRefundCall = swapIface.encodeFunctionData('refundETH')
+
+    return [encodedSwapCall, encodedRefundCall]
+  }
+
+  return buyAndRefundData
+}
+
+export const useBuyAndRefund = () => {
+  const address = useAtomValue(addressAtom)
+  const { swapRouter } = useAtomValue(addressesAtom)
+  const handleTransaction = useHandleTransaction()
+  const swapRouterContract = useContract(swapRouter, routerABI)
+  const buyAndRefundData = useBuyAndRefundData()
+  const buyAndRefund = async (amount: BigNumber) => {
+    const callData = await buyAndRefundData(amount)
+
+    const txHash = await handleTransaction(
+      swapRouterContract?.methods.multicall(callData).send({
+        from: address,
+        value: ethers.utils.parseEther(amount.toString()),
+      }),
+    )
+
+    return txHash
+  }
+
+  return buyAndRefund
+}
+
+const useGetSellQuote = () => {
+  const pool = useAtomValue(poolAtom)
+  const squeethToken = useAtomValue(squeethTokenAtom)
+  const wethToken = useAtomValue(wethTokenAtom)
+  //I input an exact amount of squeeth I want to sell, tells me how much ETH I'd receive
+  const getSellQuote = useCallback(
+    async (squeethAmount: BigNumber, slippageAmount = new BigNumber(DEFAULT_SLIPPAGE)) => {
+      const emptyState = {
+        amountOut: new BigNumber(0),
+        minimumAmountOut: new BigNumber(0),
+        priceImpact: '0',
+      }
+      if (!squeethAmount || !pool) return emptyState
+
+      try {
+        //squeeth is input token, WETH is output token. I'm selling squeeth for WETH
+        const route = new Route([pool], squeethToken!, wethToken!)
+        //getting the amount of ETH I'd receive for inputting the amount of squeeth I want to sell
+        const trade = await Trade.exactIn(
+          route,
+          CurrencyAmount.fromRawAmount(squeethToken!, fromTokenAmount(squeethAmount, OSQUEETH_DECIMALS).toFixed(0)),
+        )
+
+        //the amount of ETH I'm receiving
+        return {
+          amountOut: new BigNumber(trade.outputAmount.toSignificant(18)),
+          minimumAmountOut: new BigNumber(
+            trade.minimumAmountOut(parseSlippageInput(slippageAmount.toString())).toSignificant(18),
+          ),
+          priceImpact: trade.priceImpact.toFixed(2),
+        }
+      } catch (e) {
+        console.log(e)
+      }
+
+      return emptyState
+    },
+    [pool?.token0Price, wethToken?.address, squeethToken?.address],
+  )
+  return getSellQuote
+}
+
+const useGetSellParam = () => {
+  const address = useAtomValue(addressAtom)
+  const squeethToken = useAtomValue(squeethTokenAtom)
+  const wethToken = useAtomValue(wethTokenAtom)
+  const getSellQuote = useGetSellQuote()
+  const getSellParam = async (amount: BigNumber) => {
+    const amountMin = fromTokenAmount((await getSellQuote(amount)).minimumAmountOut, 18)
+
+    return {
+      tokenIn: squeethToken?.address,
+      tokenOut: wethToken?.address,
+      fee: UNI_POOL_FEES,
+      recipient: address,
+      deadline: Math.floor(Date.now() / 1000 + 86400), // uint256
+      amountIn: fromTokenAmount(amount, OSQUEETH_DECIMALS).toString(),
+      amountOutMinimum: amountMin.toString(),
+      sqrtPriceLimitX96: 0,
+    }
+  }
+  return getSellParam
+}
+
+const useSellAndUnwrapData = () => {
+  const address = useAtomValue(addressAtom)
+  const web3 = useAtomValue(web3Atom)
+  const { swapRouter } = useAtomValue(addressesAtom)
+  const getSellParam = useGetSellParam()
+  const getSellQuote = useGetSellQuote()
+  const sellAndUnwrapData = async (amount: BigNumber) => {
+    if (!web3) return
+    const exactInputParam = await getSellParam(amount)
+    exactInputParam.recipient = swapRouter
+    const tupleInput = Object.values(exactInputParam).map((v) => v?.toString() || '')
+
+    const { minimumAmountOut } = await getSellQuote(amount)
+    const swapIface = new ethers.utils.Interface(routerABI)
+    const encodedSwapCall = swapIface.encodeFunctionData('exactInputSingle', [tupleInput])
+    const encodedUnwrapCall = swapIface.encodeFunctionData('unwrapWETH9', [
+      fromTokenAmount(minimumAmountOut, 18).toString(),
+      address,
+    ])
+    return [encodedSwapCall, encodedUnwrapCall]
+  }
+
+  return sellAndUnwrapData
+}
+
+export const useSell = () => {
+  const address = useAtomValue(addressAtom)
+  const { swapRouter } = useAtomValue(addressesAtom)
+  const handleTransaction = useHandleTransaction()
+  const swapRouterContract = useContract(swapRouter, routerABI)
+  const sellAndUnwrapData = useSellAndUnwrapData()
+  const sell = async (amount: BigNumber) => {
+    const callData = await sellAndUnwrapData(amount)
+
+    const txHash = await handleTransaction(
+      swapRouterContract?.methods.multicall(callData).send({
+        from: address,
+      }),
+    )
+
+    return txHash
+  }
+  return sell
+}
+
+export const useGetSellQuoteForETH = () => {
+  const pool = useAtomValue(poolAtom)
+  const squeethToken = useAtomValue(squeethTokenAtom)
+  const wethToken = useAtomValue(squeethTokenAtom)
+  //I input an exact amount of ETH I want to receive, tells me how much squeeth I'd need to sell
+  const getSellQuoteForETH = useCallback(
+    () =>
+      async (ETHAmount: BigNumber, slippageAmount = new BigNumber(DEFAULT_SLIPPAGE)) => {
+        const emptyState = {
+          amountIn: new BigNumber(0),
+          maximumAmountIn: new BigNumber(0),
+          priceImpact: '0',
+        }
+        if (!ETHAmount || !pool) return emptyState
+
+        try {
+          //squeeth is input token, WETH is output token. I'm selling squeeth for WETH
+          const route = new Route([pool], squeethToken!, wethToken!)
+          //getting the amount of squeeth I'd need to sell to receive my desired amount of ETH
+          const trade = await Trade.exactOut(
+            route,
+            CurrencyAmount.fromRawAmount(wethToken!, fromTokenAmount(ETHAmount, 18).toFixed(0)),
+          )
+
+          //the amount of squeeth I need to sell
+          return {
+            amountIn: new BigNumber(trade.inputAmount.toSignificant(18)),
+            maximumAmountIn: new BigNumber(
+              trade.maximumAmountIn(parseSlippageInput(slippageAmount.toString())).toSignificant(18),
+            ),
+            priceImpact: trade.priceImpact.toFixed(2),
+          }
+        } catch (e) {
+          console.log(e)
+        }
+
+        return emptyState
+      },
+    [pool, squeethToken, wethToken],
+  )
+
+  return getSellQuoteForETH
+}
