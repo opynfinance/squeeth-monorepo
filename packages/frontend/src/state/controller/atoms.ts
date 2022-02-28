5ec50f2f112eaf9e85296ce362e6439eb918de43
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-02-28 16:55:02 +0100

    refactor: adds controller atoms

---
+++ packages/frontend/src/state/controller/atoms.ts
@@ -0,0 +1,18 @@
+import { atom } from 'jotai'
+import { BIG_ZERO } from '@constants/index'
+
+export const markAtom = atom(BIG_ZERO)
+export const indexAtom = atom(BIG_ZERO)
+export const dailyHistoricalFundingAtom = atom({ period: 0, funding: 0 })
+export const currentImpliedFundingAtom = atom(0)
+
+export const impliedVolAtom = atom((get) => {
+  const mark = get(markAtom)
+  const index = get(indexAtom)
+  const currentImpliedFunding = get(currentImpliedFundingAtom)
+  if (mark.isZero()) return 0
+  if (mark.lt(index)) return 0
+  if (currentImpliedFunding < 0) return 0
+
+  return Math.sqrt(currentImpliedFunding * 365)
+})
