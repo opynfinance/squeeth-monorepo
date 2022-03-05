d12a14e9c432451451febdb7f9d4232244a6733f
adam5909099 <adam5909099@gmail.com>
2022-03-04 21:00:06 -0500

    Resolve comment

--- packages/frontend/src/components/Trade/YourVaults.tsx
+++ packages/frontend/src/components/Trade/YourVaults.tsx
@@ -1,7 +1,7 @@
 import LabelWithTooltip from '../LabelWithTooltip'
 import SqueethCard from '../SqueethCard'
 import useYourVaults from '../../hooks/useYourVaults'
-import { Grid, Typography } from '@material-ui/core'
+import { Box, Grid, Typography } from '@material-ui/core'
 import { toTokenAmount } from '../../utils/calculations'
 import BigNumber from 'bignumber.js'
 import Link from 'next/link'
@@ -22,28 +22,44 @@ const YourVaults: FC = () => {
     <>
       {vaults?.map((vault, index) => (
         <Link key={vault.id} href={`/vault/${vault.id}`} passHref>
-          <SqueethCard component="a" mt={index ? 2 : 0}>
-            <Grid container>
-              <Grid item md={4}>
-                <LabelWithTooltip labelVariant="caption" label="Id" tooltip="Vault Id" />
-                <Typography variant="body1">{vault.id}</Typography>
-              </Grid>
+          <a>
+            <SqueethCard mt={index ? 2 : 0}>
+              <Grid container>
+                <Grid item md={4}>
+                  <LabelWithTooltip labelVariant="caption" label="Id" />
+                  <Typography variant="body1">{vault.id}</Typography>
+                </Grid>
 
-              <Grid item md={4}>
-                <LabelWithTooltip labelVariant="caption" label="Short Amount" tooltip="ETH" />
-                <Typography variant="body1">
-                  {toTokenAmount(new BigNumber(vault.shortAmount), 18).toFixed(4)}
-                </Typography>
-              </Grid>
+                <Grid item md={4}>
+                  <LabelWithTooltip labelVariant="caption" label="Short Amount" />
+                  <Box display="flex" alignItems="flex-end">
+                    <Typography variant="body1">
+                      {toTokenAmount(new BigNumber(vault.shortAmount), 18).toFixed(4)}
+                    </Typography>
+                    <Box clone pl={2}>
+                      <Typography variant="body2" color="error">
+                        oSQTH
+                      </Typography>
+                    </Box>
+                  </Box>
+                </Grid>
 
-              <Grid item md={4}>
-                <LabelWithTooltip labelVariant="caption" label="Collateral Amount" tooltip="ETH" />
-                <Typography variant="body1">
-                  {toTokenAmount(new BigNumber(vault.collateralAmount), 18).toFixed(4)}
-                </Typography>
+                <Grid item md={4}>
+                  <LabelWithTooltip labelVariant="caption" label="Collateral Amount" />
+                  <Box display="flex" alignItems="flex-end">
+                    <Typography variant="body1">
+                      {toTokenAmount(new BigNumber(vault.collateralAmount), 18).toFixed(4)}
+                    </Typography>
+                    <Box clone pl={2}>
+                      <Typography variant="body2" color="error">
+                        ETH
+                      </Typography>
+                    </Box>
+                  </Box>
+                </Grid>
               </Grid>
-            </Grid>
-          </SqueethCard>
+            </SqueethCard>
+          </a>
         </Link>
       ))}
     </>
