10564a20bd86e360f8d30bf72b7116017e0e30a8
Haythem Sellami <sellamihaythem96@gmail.com>
2022-03-06 23:27:22 +0100

    fix

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -73,10 +73,7 @@ const config: HardhatUserConfig = {
       hardfork: "berlin",
       accounts: {
         accountsBalance: '1000000000000000000000000000'
-      },
-      forking: {
-        url: "https://mainnet.infura.io/v3/${InfuraKey}",
-      }  
+      }
     },
     localhost: {
       url: "http://localhost:8545",
