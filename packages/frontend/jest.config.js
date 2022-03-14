346c6588959aecefa3b7101f7555306ebeb02ef3
EJ Liu <jacvd7@gmail.com>
2022-03-14 12:05:12 -0700

    feat: add e2e test configs and basic components with synpress

--- packages/frontend/jest.config.js
+++ packages/frontend/jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/cypress/'],
+  moduleNameMapper: {
+    '^@constants(.*)$': '<rootDir>/src/constants/$1',
+    '^@utils(.*)$': '<rootDir>/src/utils/$1',
+    '^@hooks(.*)$': '<rootDir>/src/hooks/$1',
+  },
 }
