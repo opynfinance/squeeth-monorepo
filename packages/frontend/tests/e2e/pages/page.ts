17437f686b7e3c3c33fb64ee68e6a742f9245d23
EJ Liu <jacvd7@gmail.com>
2022-03-14 12:05:12 -0700

    feat: add e2e test configs and basic components with synpress

---
+++ packages/frontend/tests/e2e/pages/page.ts
@@ -0,0 +1,54 @@
+export default class Page {
+  getTitle() {
+    return cy.title()
+  }
+
+  getMetamaskWalletAddress() {
+    return cy.fetchMetamaskWalletAddress()
+  }
+
+  acceptMetamaskAccessRequest() {
+    cy.acceptMetamaskAccess()
+  }
+
+  confirmMetamaskTransaction() {
+    // Currently without supplying a gas configuration results in failing transactions
+    // Possibly caused by wrong default behaviour within Synpress
+    cy.confirmMetamaskTransaction({ gasFee: 1, gasLimit: 5000000 })
+  }
+
+  snxExchangerSettle(asset) {
+    return cy.snxExchangerSettle(asset)
+  }
+
+  snxCheckWaitingPeriod(asset) {
+    cy.snxCheckWaitingPeriod(asset)
+  }
+
+  waitUntilAvailableOnEtherscan(urlOrTx, alias) {
+    if (!urlOrTx.includes('http')) {
+      cy.getNetwork().then((network) => {
+        const etherscanUrl =
+          network.networkName === 'mainnet'
+            ? `https://etherscan.io/tx/${urlOrTx}`
+            : `https://${network.networkName}.etherscan.io/tx/${urlOrTx}`
+        waitForTxSuccess(etherscanUrl, alias)
+      })
+    } else {
+      waitForTxSuccess(urlOrTx, alias)
+    }
+  }
+}
+
+function waitForTxSuccess(url, alias) {
+  cy.request(url).as(alias)
+  cy.get(`@${alias}`).then((response) => {
+    if (
+      response.body.includes('This transaction has been included into Block No') ||
+      response.body.includes('</i> Pending</span>')
+    ) {
+      cy.wait(5000)
+      waitForTxSuccess(url, alias)
+    }
+  })
+}
