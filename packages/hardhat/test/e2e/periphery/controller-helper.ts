36ee80babee24b434a6e657c66ab1b6a4fd38fec
Haythem Sellami <sellamihaythem96@gmail.com>
2022-03-15 23:17:18 +0100

    fix e2e

--- packages/hardhat/test/e2e/periphery/controller-helper.ts
+++ packages/hardhat/test/e2e/periphery/controller-helper.ts
@@ -134,9 +134,6 @@ describe("ControllerHelper: mainnet fork", function () {
       const tokenId = await (positionManager as INonfungiblePositionManager).tokenByIndex(tokenIndexAfter.sub(1));
       const position = await (positionManager as INonfungiblePositionManager).positions(tokenId)
 
-      console.log("vaultAfter.shortAmount", vaultAfter.shortAmount.toString())
-      console.log("mintWSqueethAmount", mintWSqueethAmount.toString())
-      
       expect(BigNumber.from(vaultAfter.NftCollateralId).eq(tokenId)).to.be.true;
       expect(position.tickLower === -887220).to.be.true
       expect(position.tickUpper === 887220).to.be.true
@@ -262,9 +259,6 @@ describe("ControllerHelper: mainnet fork", function () {
       const tokenIndexAfter = await (positionManager as INonfungiblePositionManager).totalSupply();
       const tokenId = await (positionManager as INonfungiblePositionManager).tokenByIndex(tokenIndexAfter.sub(1));
       const position = await (positionManager as INonfungiblePositionManager).positions(tokenId)
-
-      console.log("vaultAfter.collateralAmount", vaultAfter.collateralAmount.toString())
-      console.log("collateralToMint.div(2)", collateralToMint.div(2).toString())
 
       expect(BigNumber.from(vaultAfter.NftCollateralId).eq(tokenId)).to.be.true;
       expect(position.tickLower === -887220).to.be.true
