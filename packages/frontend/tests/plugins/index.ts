32593caf3b0c874407c186ebce106feb31970598
EJ Liu <jacvd7@gmail.com>
2022-03-19 16:46:05 +0800

    config: update synpress config setting for firebase access

---
+++ packages/frontend/tests/plugins/index.ts
@@ -0,0 +1,7 @@
+import admin = require('firebase-admin')
+import { plugin as cypressFirebasePlugin } from 'cypress-firebase'
+
+module.exports = (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) => {
+  const extendedConfig = cypressFirebasePlugin(on, config, admin)
+  return extendedConfig
+}
