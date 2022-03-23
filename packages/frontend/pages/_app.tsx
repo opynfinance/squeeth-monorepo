fa6351dce76e859f70f52360c0c1fa72a7cf066e
rootial <brightliu77@gmail.com>
2022-03-23 09:43:52 +0800

    update

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -97,10 +97,7 @@ const TradeApp = ({ Component, pageProps }: any) => {
           name="twitter:description"
           content="Squeeth is a new financial primitive in DeFi that gives traders exposure to ETH²"
         />
-        <meta
-          name="twitter:image"
-          content="https://squeeth.opyn.co/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FSqueethLogo.f8b5e88c.svg&w=128&q=75"
-        />
+        <meta name="twitter:image" content="https://squeeth.opyn.co/images/SqueethLogoMedium.png" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
