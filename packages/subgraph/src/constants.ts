c39ae7d92fb5184c1cfb8972e5ad2ac1927aeb97
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-03-30 21:33:43 +0100

    feat: adds controller helper address

--- packages/subgraph/src/constants.ts
+++ packages/subgraph/src/constants.ts
@@ -1,5 +1,5 @@
 import { Address, BigInt, dataSource } from "@graphprotocol/graph-ts";
-import { getShortHelperAddr } from "./util";
+import { getAddress } from "./util";
 
 export const BIGINT_ONE = BigInt.fromI32(1);
 export const BIGINT_ZERO = BigInt.fromI32(0);
@@ -16,4 +16,23 @@ export const RA_SHORT_HELPER_ADDR = Address.fromString(
   "0x5A30a1E3873A2B5Fc9DB9b2b52491C4b6086FAe0"
 );
 
-export const SHORT_HELPER_ADDR = getShortHelperAddr(dataSource.network());
+export const SHORT_HELPER_ADDR = getAddress(dataSource.network(), {
+  mainnet: MAINNET_SHORT_HELPER_ADDR,
+  ropsten: ROPSTEN_SHORT_HELPER_ADDR,
+  localhost: LOCALHOST_SHORT_HELPER_ADDR,
+  ra: RA_SHORT_HELPER_ADDR,
+});
+
+export const MAINNET_CONTROLLER_HELPER_ADDR = Address.fromString("");
+export const ROPSTEN_CONTROLLER_HELPER_ADDR = Address.fromString(
+  "0x75500ff751789b5753DF14c047e61BC5c4324CF8"
+);
+export const LOCALHOST_CONTROLLER_HELPER_ADDR = Address.fromString("");
+export const RA_CONTROLLER_HELPER_ADDR = Address.fromString("");
+
+export const CONTROLLER_HELPER_ADDR = getAddress(dataSource.network(), {
+  mainnet: MAINNET_CONTROLLER_HELPER_ADDR,
+  ropsten: ROPSTEN_CONTROLLER_HELPER_ADDR,
+  localhost: LOCALHOST_CONTROLLER_HELPER_ADDR,
+  ra: RA_CONTROLLER_HELPER_ADDR,
+});
