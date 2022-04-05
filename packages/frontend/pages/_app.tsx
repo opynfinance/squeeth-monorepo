b29b4a2415983d0c228e27cee3790760a8f54dbd
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-04-05 08:09:26 +0100

    fix: replaces removed code due to rebasing

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -19,6 +19,7 @@ import { useOnboard } from 'src/state/wallet/hooks'
 import { networkIdAtom } from 'src/state/wallet/atoms'
 import { useUpdateSqueethPrices, useUpdateSqueethPoolData } from 'src/state/squeethPool/hooks'
 import { useInitController } from 'src/state/controller/hooks'
+import { useSwaps } from 'src/state/positions/hooks'
 
 const queryClient = new QueryClient({ defaultOptions: { queries: { refetchOnWindowFocus: false } } })
 
@@ -76,6 +77,7 @@ const Init = () => {
   useUpdateSqueethPrices()
   useUpdateSqueethPoolData()
   useInitController()
+  useSwaps()
   return null
 }
 const MemoizedInit = memo(Init)
