e63e945aabbec30cdc1e341597cc65690068909e
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-04-13 10:24:24 +0100

    fix: updates useVaultManager hook replacing subcriptions with polling

--- packages/frontend/src/hooks/contracts/useVaultManager.ts
+++ packages/frontend/src/hooks/contracts/useVaultManager.ts
@@ -1,14 +1,17 @@
 import { useQuery } from '@apollo/client'
 import BigNumber from 'bignumber.js'
 import { useEffect, useState } from 'react'
+import { usePrevious } from 'react-use'
+import { useAtomValue } from 'jotai'
+import { useUpdateAtom } from 'jotai/utils'
 
 import { toTokenAmount } from '@utils/calculations'
 import { OSQUEETH_DECIMALS } from '../../constants/'
 import { VAULTS_QUERY, VAULTS_SUBSCRIPTION } from '../../queries/squeeth/vaultsQuery'
 import { Vaults } from '../../queries/squeeth/__generated__/Vaults'
 import { squeethClient } from '../../utils/apollo-client'
-import { useAtomValue } from 'jotai'
 import { addressAtom, networkIdAtom } from 'src/state/wallet/atoms'
+import { vaultManagerPollingAtom } from 'src/state/positions/atoms'
 
 /**
  * get user vaults.
@@ -16,51 +19,44 @@ import { addressAtom, networkIdAtom } from 'src/state/wallet/atoms'
  * @param refetchIntervalSec refetch interval in seconds
  * @returns {Vault[]}
  */
-export const useVaultManager = () => {
-  const [vaults, setVaults] = useState<Array<any>>([])
+export const useVaultManager = (poll = false) => {
   const address = useAtomValue(addressAtom)
   const networkId = useAtomValue(networkIdAtom)
+  const setVaultManagerPolling = useUpdateAtom(vaultManagerPollingAtom)
 
-  const { data, loading, subscribeToMore } = useQuery<Vaults>(VAULTS_QUERY, {
+  const { data, loading, startPolling, stopPolling } = useQuery<Vaults>(VAULTS_QUERY, {
     client: squeethClient[networkId],
     fetchPolicy: 'cache-and-network',
     variables: {
       ownerId: address ?? '',
     },
   })
+
+  const currentVault = data?.vaults.find((vault) => new BigNumber(vault.collateralAmount).isGreaterThan(0))
+  const prevVault = usePrevious(currentVault)
+
   useEffect(() => {
-    subscribeToMore({
-      document: VAULTS_SUBSCRIPTION,
-      variables: {
-        ownerId: address ?? '',
-      },
-      updateQuery(prev, { subscriptionData }) {
-        if (!subscriptionData.data || subscriptionData.data.vaults.length === data?.vaults.length) return prev
-        const newVaults = subscriptionData.data.vaults
-        return { vaults: newVaults }
-      },
+    if (poll && !prevVault && !currentVault) {
+      startPolling(500)
+    } else if (poll && new BigNumber(prevVault?.shortAmount).isEqualTo(new BigNumber(currentVault?.shortAmount))) {
+      startPolling(500)
+    } else {
+      stopPolling()
+      setVaultManagerPolling(false)
+    }
+  }, [currentVault?.shortAmount.toString(), poll, prevVault?.shortAmount.toString(), startPolling, stopPolling])
+
+  const vaults = data?.vaults
+    .filter((v) => {
+      return new BigNumber(v?.collateralAmount ?? 0).gt(0)
     })
-  }, [address, subscribeToMore])
-
-  useEffect(() => {
-    ;(async () => {
-      if (!data?.vaults?.length) return
-
-      const _vaults = data?.vaults
-        .filter((v) => {
-          return new BigNumber(v?.collateralAmount ?? 0).gt(0)
-        })
-        .map(({ id, NftCollateralId, collateralAmount, shortAmount, operator }) => ({
-          id,
-          NFTCollateralId: NftCollateralId,
-          collateralAmount: toTokenAmount(new BigNumber(collateralAmount), 18),
-          shortAmount: toTokenAmount(new BigNumber(shortAmount), OSQUEETH_DECIMALS),
-          operator,
-        }))
-
-      setVaults(_vaults)
-    })()
-  }, [data?.vaults?.length])
+    .map(({ id, NftCollateralId, collateralAmount, shortAmount, operator }) => ({
+      id,
+      NFTCollateralId: NftCollateralId,
+      collateralAmount: toTokenAmount(new BigNumber(collateralAmount), 18),
+      shortAmount: toTokenAmount(new BigNumber(shortAmount), OSQUEETH_DECIMALS),
+      operator,
+    }))
 
   return { vaults, loading }
 }
