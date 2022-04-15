6190c9631c84805df7c0ae29431b699703fab80f
Haythem Sellami <haythem96@users.noreply.github.com>
2022-04-15 14:28:04 +0200

    feat: add increasing liquidity within same ranges and rename sellAll() (#348)
    * change sellAll() to allow for increaseing liquidity
    
    * add checkClosedLpc call

--- packages/hardhat/contracts/periphery/ControllerHelper.sol
+++ packages/hardhat/contracts/periphery/ControllerHelper.sol
@@ -269,9 +269,9 @@ contract ControllerHelper is UniswapControllerHelper, EulerControllerHelper, IER
 
     /**
      * @notice sell all LP wPowerPerp amounts to WETH and send back to user
-     * @param _params ControllerHelperDataType.SellAll struct
+     * @param _params ControllerHelperDataType.ReduceLiquidityAndSell struct
      */
-    function sellAll(ControllerHelperDataType.SellAll calldata _params) external {
+    function reduceLiquidityAndSell(ControllerHelperDataType.ReduceLiquidityAndSell calldata _params) external {
         INonfungiblePositionManager(ControllerHelperDiamondStorage.getAddressAtSlot(6)).safeTransferFrom(
             msg.sender,
             address(this),
@@ -284,11 +284,20 @@ contract ControllerHelper is UniswapControllerHelper, EulerControllerHelper, IER
             ControllerHelperDataType.closeUniLpParams({
                 tokenId: _params.tokenId,
                 liquidity: _params.liquidity,
-                liquidityPercentage: 1e18,
+                liquidityPercentage: _params.liquidityPercentage,
                 amount0Min: uint128(_params.amount0Min),
                 amount1Min: uint128(_params.amount1Min)
             }),
             isWethToken0
+        );
+
+        ControllerHelperUtil.checkClosedLp(
+            msg.sender,
+            ControllerHelperDiamondStorage.getAddressAtSlot(0),
+            ControllerHelperDiamondStorage.getAddressAtSlot(6),
+            0,
+            _params.tokenId,
+            _params.liquidityPercentage
         );
 
         if (wPowerPerpAmountInLp > 0) {
