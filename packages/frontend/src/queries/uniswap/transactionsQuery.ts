8164757176038c3e63c4217a68eb3c5ed8db413a
EJ Liu <jacvd7@gmail.com>
2022-04-18 20:24:07 +0800

    fix: add liquidity txs query and subscription

--- packages/frontend/src/queries/uniswap/transactionsQuery.ts
+++ packages/frontend/src/queries/uniswap/transactionsQuery.ts
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client'
 
-const TRANSACTIONS_QUERY = gql`
+export const TRANSACTIONS_QUERY = gql`
   query transactions(
     $poolAddress: Bytes!
     $owner: String!
@@ -28,4 +28,32 @@ const TRANSACTIONS_QUERY = gql`
     }
   }
 `
-export default TRANSACTIONS_QUERY
+
+export const TRANSACTIONS_SUBSCRIPTION = gql`
+  subscription subscriptionTransactions(
+    $poolAddress: Bytes!
+    $owner: String!
+    $origin: String!
+    $recipients: [String!]!
+    $orderDirection: String!
+  ) {
+    positionSnapshots(
+      orderBy: id
+      orderDirection: $orderDirection
+      where: { pool: $poolAddress, owner: $owner }
+      subgraphError: allow
+    ) {
+      id
+      owner
+      liquidity
+      depositedToken0
+      depositedToken1
+      withdrawnToken0
+      withdrawnToken1
+      transaction {
+        id
+        timestamp
+      }
+    }
+  }
+`
