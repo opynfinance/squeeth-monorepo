99f0584d6c38e719c03f7dfae3a39ebe9e389b87
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-04-20 17:55:30 +0100

    fix: updates mock handlers

--- packages/frontend/mocks/handlers.ts
+++ packages/frontend/mocks/handlers.ts
@@ -1,5 +1,6 @@
 import { rest } from 'msw'
-import coingeckoData from './data/coingecko.json'
+import coingeckoData from './price_data/coingecko.json'
+import twelveData from './price_data/twelvedata.json'
 
 export const handlers = [
   rest.get('https://api.coingecko.com/api/v3/coins/ethereum/market_chart', (req, res, ctx) => {
@@ -7,13 +8,12 @@ export const handlers = [
     const currency = query.get('vs_currency')
     const days = query.get('days')
 
-    console.log({ query })
     if (currency && days) {
       return res(ctx.status(200), ctx.json(coingeckoData))
     }
   }),
 
-  rest.get(`/api/twelvedata`, (req, res, ctx) => {
+  rest.get(/\/api\/twelvedata/, (req, res, ctx) => {
     const query = req.url.searchParams
     const path = query.get('path')
     const start_date = query.get('start_date')
@@ -22,8 +22,9 @@ export const handlers = [
     const interval = query.get('interval')
 
     if (path && path === 'time_series' && start_date && end_date && symbol && interval) {
-      console.log('TWELVE DATA')
-      return res(ctx.status(200), ctx.json({ message: true }))
+      const price = twelveData.values.find((price) => price.datetime === start_date) ?? twelveData.values[0]
+
+      return res(ctx.status(200), ctx.json({ meta: twelveData.meta, values: [price], status: twelveData.status }))
     }
   }),
 ]
