176a6445c39cd8d185c4fd36c2c2181d3cf9c491
Adam <59410529+adam5909099@users.noreply.github.com>
2022-04-25 01:16:30 -0400

    Configure atom test and add pnl tests (#323)
    * Configure atom test and add pnl tests
    
    * Update yarn lock
    
    * Remove unsed values
    
    * Make crab position hook testing more readable
    
    * Make it more readable
    
    * test: fix test failure in in your vaults
    
    * chore: update jest config and update ci
    
    * Removed duplicated jest config
    
    * Update test script
    
    * chore: update jest again
    
    * chore: update ci test scripts
    
    * chore: wrap up ci config
    
    * Add comment for getting values

---
+++ packages/frontend/src/hooks/useCrabPosition/mocks.ts
@@ -0,0 +1,23 @@
+import { atom } from 'jotai'
+import BigNumber from 'bignumber.js'
+
+export const mockedCrabLoadingAtom = atom(true)
+
+export const mockedCurrentEthValueAtom = atom(new BigNumber(31.502381757505464))
+
+export const mockedIndexAtom = atom(new BigNumber(1.096035041905758e25))
+
+export const mockedUserCrabTxHistoryData = [
+  {
+    __typename: 'CrabStrategyTx' as const,
+    ethAmount: new BigNumber(29.686306442273224),
+    id: '0x468ab39dedabebb84e8245dd4a5b2e74a7f7866bad58b2cf9abf7410baa01438',
+    lpAmount: new BigNumber(167.1950754262323),
+    timestamp: '1646839085',
+    type: 'FLASH_DEPOSIT',
+    wSqueethAmount: '93257202421547735264',
+    oSqueethAmount: new BigNumber(93.25720242154773),
+    ethUsdValue: new BigNumber(81181.4599647523),
+    txTitle: 'Flash Deposit',
+  },
+]
