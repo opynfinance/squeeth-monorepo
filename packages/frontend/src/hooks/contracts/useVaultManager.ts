a33e37caa7d52bc4958f9863cf8657bc0c7e761f
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-04-27 18:44:00 +0100

    fix: updates useVaultManager hook to update vault data to be used across app

--- packages/frontend/src/hooks/contracts/useVaultManager.ts
+++ packages/frontend/src/hooks/contracts/useVaultManager.ts
@@ -1,15 +1,17 @@
+import { useCallback, useState } from 'react'
 import { useQuery } from '@apollo/client'
 import BigNumber from 'bignumber.js'
-import { useState } from 'react'
+import { usePrevious } from 'react-use'
+import { useAtomValue } from 'jotai'
 
 import { toTokenAmount } from '@utils/calculations'
 import { OSQUEETH_DECIMALS } from '../../constants/'
 import { VAULTS_QUERY, VAULTS_SUBSCRIPTION } from '../../queries/squeeth/vaultsQuery'
 import { Vaults } from '../../queries/squeeth/__generated__/Vaults'
 import { squeethClient } from '../../utils/apollo-client'
-import { useAtomValue } from 'jotai'
 import { addressAtom, networkIdAtom } from 'src/state/wallet/atoms'
 import useAppEffect from '@hooks/useAppEffect'
+import useAppMemo from '@hooks/useAppMemo'
 
 /**
  * get user vaults.
@@ -18,17 +20,46 @@ import useAppEffect from '@hooks/useAppEffect'
  * @returns {Vault[]}
  */
 export const useVaultManager = () => {
-  const [vaults, setVaults] = useState<Array<any>>([])
   const address = useAtomValue(addressAtom)
   const networkId = useAtomValue(networkIdAtom)
+  const [poll, setPoll] = useState(false)
 
-  const { data, loading, subscribeToMore } = useQuery<Vaults>(VAULTS_QUERY, {
+  const { data, loading, subscribeToMore, startPolling, stopPolling } = useQuery<Vaults>(VAULTS_QUERY, {
     client: squeethClient[networkId],
     fetchPolicy: 'cache-and-network',
     variables: {
       ownerId: address ?? '',
     },
   })
+
+  const beginVaultUpdate = useCallback(() => setPoll(true), [])
+
+  const currentVault = data?.vaults.find((vault) => new BigNumber(vault.collateralAmount).isGreaterThan(0))
+  const prevVault = usePrevious(currentVault)
+
+  useAppEffect(() => {
+    if (poll && !prevVault && !currentVault) {
+      startPolling(500)
+    } else if (
+      poll &&
+      (new BigNumber(prevVault?.shortAmount).isEqualTo(new BigNumber(currentVault?.shortAmount)) ||
+        new BigNumber(prevVault?.collateralAmount).isEqualTo(new BigNumber(currentVault?.collateralAmount)))
+    ) {
+      startPolling(500)
+    } else {
+      stopPolling()
+      setPoll(false)
+    }
+  }, [
+    currentVault?.shortAmount.toString(),
+    currentVault?.collateralAmount.toString(),
+    poll,
+    prevVault?.shortAmount.toString(),
+    prevVault?.collateralAmount.toString(),
+    startPolling,
+    stopPolling,
+  ])
+
   useAppEffect(() => {
     subscribeToMore({
       document: VAULTS_SUBSCRIPTION,
@@ -43,25 +74,20 @@ export const useVaultManager = () => {
     })
   }, [address, subscribeToMore, data?.vaults.length])
 
-  useAppEffect(() => {
-    ;(async () => {
-      if (!data?.vaults?.length) return
+  const vaultsData = data?.vaults
+    .filter((v) => {
+      return new BigNumber(v?.collateralAmount ?? 0).gt(0)
+    })
+    .map(({ id, NftCollateralId, collateralAmount, shortAmount, operator }) => ({
+      id,
+      NFTCollateralId: NftCollateralId,
+      collateralAmount: toTokenAmount(new BigNumber(collateralAmount), 18),
+      shortAmount: toTokenAmount(new BigNumber(shortAmount), OSQUEETH_DECIMALS),
+      operator,
+    }))
 
-      const _vaults = data?.vaults
-        .filter((v) => {
-          return new BigNumber(v?.collateralAmount ?? 0).gt(0)
-        })
-        .map(({ id, NftCollateralId, collateralAmount, shortAmount, operator }) => ({
-          id,
-          NFTCollateralId: NftCollateralId,
-          collateralAmount: toTokenAmount(new BigNumber(collateralAmount), 18),
-          shortAmount: toTokenAmount(new BigNumber(shortAmount), OSQUEETH_DECIMALS),
-          operator,
-        }))
-
-      setVaults(_vaults)
-    })()
-  }, [data?.vaults])
-
-  return { vaults, loading }
+  return useAppMemo(
+    () => ({ vaults: vaultsData, loading, beginVaultUpdate }),
+    [beginVaultUpdate, loading, data?.vaults],
+  )
 }
