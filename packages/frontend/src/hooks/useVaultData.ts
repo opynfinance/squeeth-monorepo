850086e6afccd2cb6b94ebf4a43774398c93687d
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-04-27 18:38:37 +0100

    fix: update useVaultData fixing issue of vault data not getting updated after a transaction

--- packages/frontend/src/hooks/useVaultData.ts
+++ packages/frontend/src/hooks/useVaultData.ts
@@ -1,82 +1,62 @@
 import BigNumber from 'bignumber.js'
 import { useState } from 'react'
-import { useAtom, useAtomValue } from 'jotai'
+import { useAtom } from 'jotai'
 
-import { addressAtom, connectedWalletAtom } from 'src/state/wallet/atoms'
-import { readyAtom } from 'src/state/squeethPool/atoms'
-import { useGetCollatRatioAndLiqPrice, useGetVault } from 'src/state/controller/hooks'
-import {
-  collatPercentAtom,
-  existingCollatAtom,
-  existingCollatPercentAtom,
-  existingLiqPriceAtom,
-  vaultAtom,
-} from 'src/state/positions/atoms'
-// import { normFactorAtom } from 'src/state/controller/atoms'
+import { useGetCollatRatioAndLiqPrice } from 'src/state/controller/hooks'
+import { collatPercentAtom, existingCollatPercentAtom, existingLiqPriceAtom } from 'src/state/positions/atoms'
 import useAppEffect from './useAppEffect'
-import useAppCallback from './useAppCallback'
 
-export const useVaultData = (vid: number) => {
-  const [vault, setVault] = useAtom(vaultAtom)
+interface IVault {
+  id: string
+  NFTCollateralId: any
+  collateralAmount: BigNumber
+  shortAmount: BigNumber
+  operator: any
+}
+
+export const useVaultData = (vault: IVault | undefined) => {
   const [existingCollatPercent, setExistingCollatPercent] = useAtom(existingCollatPercentAtom)
-  const [existingCollat, setExistingCollat] = useAtom(existingCollatAtom)
+  const [existingCollat, setExistingCollat] = useState(new BigNumber(0))
   const [existingLiqPrice, setExistingLiqPrice] = useAtom(existingLiqPriceAtom)
   const [collatPercent, setCollatPercent] = useAtom(collatPercentAtom)
-  const [isVaultLoading, setVaultLoading] = useState(true)
 
-  // const normFactor = useAtomValue(normFactorAtom)
-  const getVault = useGetVault()
   const getCollatRatioAndLiqPrice = useGetCollatRatioAndLiqPrice()
-  const ready = useAtomValue(readyAtom)
-  const connected = useAtomValue(connectedWalletAtom)
-  // const address = useAtomValue(addressAtom)
 
-  const updateVault = useAppCallback(async () => {
-    if (!connected || !ready) return
+  useAppEffect(() => {
+    if (!vault) {
+      setExistingCollat(new BigNumber(0))
+      setExistingCollatPercent(0)
+      setCollatPercent(0)
+      setExistingLiqPrice(new BigNumber(0))
+      return
+    }
 
-    const _vault = await getVault(vid)
-
-    if (!_vault) return
-
-    setVault(_vault)
-    setExistingCollat(_vault.collateralAmount)
+    setExistingCollat(vault.collateralAmount)
 
     getCollatRatioAndLiqPrice(
-      _vault.collateralAmount,
-      _vault.shortAmount,
-      _vault.NFTCollateralId ? Number(_vault.NFTCollateralId) : undefined,
+      vault.collateralAmount,
+      vault.shortAmount,
+      vault.NFTCollateralId ? Number(vault.NFTCollateralId) : undefined,
     ).then(({ collateralPercent, liquidationPrice }) => {
       setExistingCollatPercent(collateralPercent)
       setCollatPercent(collateralPercent)
       setExistingLiqPrice(new BigNumber(liquidationPrice))
-      setVaultLoading(false)
     })
   }, [
-    connected,
     getCollatRatioAndLiqPrice,
-    getVault,
-    ready,
     setCollatPercent,
     setExistingCollat,
     setExistingCollatPercent,
     setExistingLiqPrice,
-    setVault,
-    vid,
+    vault,
   ])
-
-  useAppEffect(() => {
-    updateVault()
-  }, [updateVault])
 
   return {
     vault,
     existingCollatPercent,
     existingLiqPrice,
-    updateVault,
     setCollatPercent,
     collatPercent,
-    isVaultLoading: isVaultLoading,
-    setVaultLoading,
     existingCollat,
   }
 }
