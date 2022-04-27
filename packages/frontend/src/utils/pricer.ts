6dd8b93b245ea5dae4e743ba015594e3772bc7eb
Adam <59410529+adam5909099@users.noreply.github.com>
2022-04-27 19:35:43 -0400

    Update app hooks and dependency of atoms and pricer (#389)
    * Update app hooks and dependency of atoms and pricer
    
    * Recover commented code

--- packages/frontend/src/utils/pricer.ts
+++ packages/frontend/src/utils/pricer.ts
@@ -120,77 +120,81 @@ export function useETHSqueethPNLCompounding(
   // const [timesToFetchNorm, setTimesToFetchNorm] = useState<number[]>([])
   // const normUpdated = useNormHistoryFromTime(timesToFetchNorm)
 
-  useAppEffect(() => {
-    ;(async () => {
-      let cumulativeSqueethLongReturn = 0
-      let cumulativeSqueethCrabReturn = 1
-      const volsMap = await getVolMap()
-      const liveVolsMap = await getLiveVolMap()
+  useAppEffect(
+    () => {
+      ;(async () => {
+        let cumulativeSqueethLongReturn = 0
+        let cumulativeSqueethCrabReturn = 1
+        const volsMap = await getVolMap()
+        const liveVolsMap = await getLiveVolMap()
 
-      // const normTimestamps = timestamps.filter((timestamp, index) => {
-      //   const utcDate = new Date(timestamp * 1000).toISOString().split('T')[0]
-      //   return (
-      //     (timestamp >= 1641772800 && !Object.keys(liveVolsMap ?? {}).includes(utcDate)) ||
-      //     index === timestamps.length - 1
-      //   ) // 1641772800 is UTC timestamp for Jan 10(the first date of live VOL) and exclude timestamp of current day as there will be data added
-      // })
-      // setTimesToFetchNorm(normTimestamps)
+        // const normTimestamps = timestamps.filter((timestamp, index) => {
+        //   const utcDate = new Date(timestamp * 1000).toISOString().split('T')[0]
+        //   return (
+        //     (timestamp >= 1641772800 && !Object.keys(liveVolsMap ?? {}).includes(utcDate)) ||
+        //     index === timestamps.length - 1
+        //   ) // 1641772800 is UTC timestamp for Jan 10(the first date of live VOL) and exclude timestamp of current day as there will be data added
+        // })
+        // setTimesToFetchNorm(normTimestamps)
 
-      if (timestamps.length > 0) {
-        const annualVolData = await Promise.all(
-          timestamps.map(async (timestamp, index) => {
+        if (timestamps.length > 0) {
+          const annualVolData = await Promise.all(
+            timestamps.map(async (timestamp, index) => {
+              const { value: price, time } = ethPrices[index > 0 ? index : 0]
+              const utcDate = new Date(timestamp * 1000).toISOString().split('T')[0]
+              let annualVol = liveVolsMap[utcDate]
+              let isLive = true
+              if (!annualVol) {
+                annualVol = await getVolForTimestampOrDefault(volsMap, time, price)
+                isLive = false
+              }
+
+              return { annualVol, isLive }
+            }),
+          )
+
+          let cumulativeEthLongShortReturn = 0
+
+          const ethCharts = ethPrices.map((ethItem, index) => {
+            const { value: price, time } = ethItem
+            const preEthPrice = ethPrices[index > 0 ? index - 1 : 0].value
+            cumulativeEthLongShortReturn += Math.log(price / preEthPrice)
+
+            const longPNL = Math.round((Math.exp(cumulativeEthLongShortReturn) - 1) * 10000) / 100
+            const shortPNL = Math.round((Math.exp(-cumulativeEthLongShortReturn) - 1) * 10000) / 100
+            return { shortPNL, longPNL, time }
+          })
+          setETHChartData(ethCharts)
+
+          const charts = annualVolData.map((item, index) => {
+            const { annualVol, isLive } = item
             const { value: price, time } = ethPrices[index > 0 ? index : 0]
-            const utcDate = new Date(timestamp * 1000).toISOString().split('T')[0]
-            let annualVol = liveVolsMap[utcDate]
-            let isLive = true
-            if (!annualVol) {
-              annualVol = await getVolForTimestampOrDefault(volsMap, time, price)
-              isLive = false
-            }
 
-            return { annualVol, isLive }
-          }),
-        )
+            const fundingPeriodMultiplier = days > 90 ? 365 : days > 1 ? 365 * 24 : 356 * 24 * 12
 
-        let cumulativeEthLongShortReturn = 0
+            let vol = annualVol * volMultiplier
+            const preEthPrice = ethPrices[index > 0 ? index - 1 : 0].value
+            let fundingCost = index === 0 ? 0 : (vol / Math.sqrt(fundingPeriodMultiplier)) ** 2
+            cumulativeSqueethLongReturn +=
+              2 * Math.log(price / preEthPrice) + Math.log(price / preEthPrice) ** 2 - fundingCost
+            // crab return
+            const crabVolMultiplier = 0.9
+            vol = annualVol * crabVolMultiplier
+            fundingCost = index === 0 ? 0 : (vol / Math.sqrt(fundingPeriodMultiplier)) ** 2
+            const simR = price / preEthPrice - 1
+            cumulativeSqueethCrabReturn *= 1 + -(simR ** 2) + fundingCost
+            const longPNL = Math.round((Math.exp(cumulativeSqueethLongReturn) - 1) * 10000) / 100
+            const shortPNL = Math.round(Math.log(cumulativeSqueethCrabReturn) * 10000) / 100
 
-        const ethCharts = ethPrices.map((ethItem, index) => {
-          const { value: price, time } = ethItem
-          const preEthPrice = ethPrices[index > 0 ? index - 1 : 0].value
-          cumulativeEthLongShortReturn += Math.log(price / preEthPrice)
-
-          const longPNL = Math.round((Math.exp(cumulativeEthLongShortReturn) - 1) * 10000) / 100
-          const shortPNL = Math.round((Math.exp(-cumulativeEthLongShortReturn) - 1) * 10000) / 100
-          return { shortPNL, longPNL, time }
-        })
-        setETHChartData(ethCharts)
-
-        const charts = annualVolData.map((item, index) => {
-          const { annualVol, isLive } = item
-          const { value: price, time } = ethPrices[index > 0 ? index : 0]
-
-          const fundingPeriodMultiplier = days > 90 ? 365 : days > 1 ? 365 * 24 : 356 * 24 * 12
-
-          let vol = annualVol * volMultiplier
-          const preEthPrice = ethPrices[index > 0 ? index - 1 : 0].value
-          let fundingCost = index === 0 ? 0 : (vol / Math.sqrt(fundingPeriodMultiplier)) ** 2
-          cumulativeSqueethLongReturn +=
-            2 * Math.log(price / preEthPrice) + Math.log(price / preEthPrice) ** 2 - fundingCost
-          // crab return
-          const crabVolMultiplier = 0.9
-          vol = annualVol * crabVolMultiplier
-          fundingCost = index === 0 ? 0 : (vol / Math.sqrt(fundingPeriodMultiplier)) ** 2
-          const simR = price / preEthPrice - 1
-          cumulativeSqueethCrabReturn *= 1 + -(simR ** 2) + fundingCost
-          const longPNL = Math.round((Math.exp(cumulativeSqueethLongReturn) - 1) * 10000) / 100
-          const shortPNL = Math.round(Math.log(cumulativeSqueethCrabReturn) * 10000) / 100
-
-          return { shortPNL, longPNL, time, isLive }
-        })
-        setSqueethChartData(charts)
-      }
-    })()
-  }, [timestamps, days, ethPrices, volMultiplier])
+            return { shortPNL, longPNL, time, isLive }
+          })
+          setSqueethChartData(charts)
+        }
+      })()
+    },
+    [timestamps, days, ethPrices, volMultiplier],
+    true,
+  )
   return { ethPNL: ethChartData, squeethPNL: squeethChartData }
 }
 
