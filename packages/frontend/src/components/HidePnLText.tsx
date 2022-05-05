7b7c7f4e6ff7f8038a55caca8d815add278a921d
EJ Liu <jacvd7@gmail.com>
2022-05-05 20:44:57 +0800

    feat: add HidePnLText component

---
+++ packages/frontend/src/components/HidePnLText.tsx
@@ -0,0 +1,26 @@
+import Typography from '@material-ui/core/Typography'
+import { LinkWrapper } from './LinkWrapper'
+import { positionTypeAtom } from 'src/state/positions/atoms'
+import { useAtomValue } from 'jotai'
+import { PositionType } from '@types/index'
+
+export const HidePnLText: React.FC = () => {
+  const positionType = useAtomValue(positionTypeAtom)
+
+  return (
+    <Typography variant="caption" color="textSecondary">
+      <br />
+      To calculate your PnL because you have{' '}
+      {positionType === PositionType.LONG ? (
+        <span>LPed, participated in crab auctions, or wallet transferred oSQTH</span>
+      ) : (
+        <span>short position</span>
+      )}{' '}
+      , please use{' '}
+      <LinkWrapper href="https://docs.google.com/spreadsheets/d/1iy5N3qy6g2xd2_BcsY_Hv0pKdyceC1h7y269KssOG8s/edit#gid=1267496112">
+        SqueethLab.
+      </LinkWrapper>
+      &nbsp; We are currently working on adding this into the frontend.
+    </Typography>
+  )
+}
