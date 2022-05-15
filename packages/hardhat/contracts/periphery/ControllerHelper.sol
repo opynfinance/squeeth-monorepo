aad1bc2f165d7c87cdef184964f6a84c5fc60d11
alpinechicken <joecmail@gmail.com>
2022-05-15 00:37:36 -0700

    rebalance with decrease liquidity, mint, and withdraw

--- packages/hardhat/contracts/periphery/ControllerHelper.sol
+++ packages/hardhat/contracts/periphery/ControllerHelper.sol
@@ -550,9 +550,6 @@ contract ControllerHelper is UniswapControllerHelper, EulerControllerHelper, IER
                     );
                     console.log('increaseLiquidityParam.wPowerPerpAmountToLp', increaseLiquidityParam.wPowerPerpAmountToLp);
                     console.log('increaseLiquidityParam.wethAmountToLp', increaseLiquidityParam.wethAmountToLp);
-                    console.log('increaseLiquidityParam.collateralToDeposit', increaseLiquidityParam.collateralToDeposit);
-                    console.log('increaseLiquidityParam.amount0Min', increaseLiquidityParam.amount0Min);
-                    console.log('increaseLiquidityParam.amount1Min', increaseLiquidityParam.amount1Min);
                     console.log('weth balance',IWETH9(ControllerHelperDiamondStorage.getAddressAtSlot(5)).balanceOf(address(this)));
                     console.log('wPowerPerp balance', IWPowerPerp(ControllerHelperDiamondStorage.getAddressAtSlot(4)).balanceOf(address(this)));
                     ControllerHelperUtil.increaseLpLiquidity(
@@ -569,6 +566,7 @@ contract ControllerHelper is UniswapControllerHelper, EulerControllerHelper, IER
                 } else if (
                     data[i].rebalanceVaultNftType == ControllerHelperDataType.RebalanceVaultNftType.DecreaseLpLiquidity
                 ) {
+                    console.log('decrease lp liquidity');
                     // decrease liquidity in LP
                     ControllerHelperDataType.DecreaseLpLiquidityParams memory decreaseLiquidityParam = abi.decode(
                         data[i].data,
@@ -603,7 +601,7 @@ contract ControllerHelper is UniswapControllerHelper, EulerControllerHelper, IER
                         data[i].data,
                         (ControllerHelperDataType.DepositIntoVault)
                     );
-
+                    console.log('deposit into vault');
                     ControllerHelperUtil.mintIntoVault(
                         controller,
                         weth,
@@ -619,7 +617,7 @@ contract ControllerHelper is UniswapControllerHelper, EulerControllerHelper, IER
                         data[i].data,
                         (ControllerHelperDataType.withdrawFromVault)
                     );
-
+                    console.log('withdraw from vault');
                     if (withdrawFromVaultParams.burnExactRemoved) {
                         ControllerHelperUtil.withdrawFromVault(
                             controller,
