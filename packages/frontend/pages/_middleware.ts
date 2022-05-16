b2966e0274a5d2018a43b12261127c92ce2de8fe
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-05-16 13:00:37 +0100

    fix: update middleware

--- packages/frontend/pages/_middleware.ts
+++ packages/frontend/pages/_middleware.ts
@@ -4,9 +4,9 @@ const BLOCKED_COUNTRIES = ['US', 'BY', 'CU', 'IR', 'IQ', 'CI', 'LR', 'KP', 'SD',
 
 export default function middleware(request: NextRequest) {
   const country = request.geo?.country
-  const url = request.url + `?country=${country}`
+  request.nextUrl.searchParams.append('country', 'Nigeria')
 
-  const response = NextResponse.rewrite(url)
+  const response = NextResponse.rewrite(request.nextUrl)
 
   if (country && BLOCKED_COUNTRIES.includes(country)) {
     response.cookie('restricted', `true,${country}`)
