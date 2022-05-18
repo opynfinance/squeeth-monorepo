7f1ad79613309600bca6dbf7fd5fab224cd0a4be
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-05-18 16:58:16 +0100

    fix: updates popup

--- packages/frontend/src/components/CookieConsentPopup.tsx
+++ packages/frontend/src/components/CookieConsentPopup.tsx
@@ -2,13 +2,11 @@ import { createStyles, makeStyles } from '@material-ui/core/styles'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useCookies } from 'react-cookie'
-import { useState } from 'react'
-import Button from '@material-ui/core/Button'
-import Dialog from '@material-ui/core/Dialog'
 import { useAtomValue } from 'jotai'
 
 import { networkIdAtom } from 'src/state/wallet/atoms'
 import { Networks } from '../types/index'
+import CookieConsent from 'react-cookie-consent'
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -17,6 +15,7 @@ const useStyles = makeStyles(() =>
       justifyContent: 'center',
       width: '100%',
       paddingBottom: '1em',
+      flexWrap: 'wrap',
     },
 
     link: {
@@ -45,61 +44,63 @@ const useStyles = makeStyles(() =>
     },
     dialog: {
       background: '#283944',
+      color: '#ffffff',
     },
+    title: { margin: '.5em 0' },
   }),
 )
 const CookieConsentPopup = () => {
   const classes = useStyles()
   const router = useRouter()
-  const [cookies, setCookie] = useCookies(['opyn_geo'])
+  const [, setCookie] = useCookies(['opyn_geo'])
   const path = router.pathname
   const networkId = useAtomValue(networkIdAtom)
 
-  const [open, setOpen] = useState(!router.query?.restricted?.includes('true'))
-
-  return !cookies?.opyn_geo && networkId !== Networks.ROPSTEN && path !== '/cookie-policy' ? (
-    <Dialog
-      open={open}
-      onClose={() => {}}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
+  return networkId !== Networks.ROPSTEN && path !== '/cookie-policy' ? (
+    <CookieConsent
+      location="bottom"
+      buttonText="I Accept"
+      cookieName="opyn_geo"
+      style={{
+        background: '#283944',
+        color: '#ffffff',
+        borderRadius: '5px',
+        boxShadow: '5px 3px 32px -6px rgb(14 103 112 / 56%)',
+        width: '50%',
+      }}
+      buttonStyle={{
+        backgroundColor: '#2b8e99',
+        color: '#ffffff',
+        padding: '.75em 2em',
+        borderRadius: 10,
+      }}
+      setDeclineCookie={false}
+      enableDeclineButton
+      flipButtons
+      declineButtonStyle={{
+        border: '1px solid #2b8e99',
+        color: '#2ce6f9',
+        borderRadius: 10,
+        backgroundColor: '#283944',
+        padding: '.75em 2em',
+      }}
+      onAccept={() => {
+        router.push(path, undefined, { shallow: true })
+        setCookie('opyn_geo', router.query?.restricted === 'true' ? `true,${router.query?.country}` : 'false')
+      }}
+      contentStyle={{ flex: '1 0 0' }}
+      buttonWrapperClasses={classes.buttonWrapper}
+      expires={Infinity}
     >
-      <div className={classes.dialog}>
-        <div className={classes.textContainer}>
-          <p>
-            We use cookies to recognize visitors and analyze front end traffic. To learn more about these methods,
-            including how to disable them, view our{' '}
-            <Link href="/cookie-policy">
-              <a rel="noreferrer" target="_blank" className={classes.link}>
-                Cookie Policy
-              </a>
-            </Link>
-            .
-          </p>
-        </div>
-
-        <div className={classes.buttonWrapper}>
-          <Button
-            className={classes.button}
-            onClick={() => {
-              router.push(path, undefined, { shallow: true })
-              setCookie('opyn_geo', router.query?.restricted === 'true' ? `true,${router.query?.country}` : 'false')
-              setOpen(false)
-            }}
-          >
-            I Accept
-          </Button>
-          <Button
-            className={classes.declineButton}
-            onClick={() => {
-              setOpen(false)
-            }}
-          >
-            Decline
-          </Button>
-        </div>
-      </div>
-    </Dialog>
+      <h2 className={classes.title}>Your Cookie Settings</h2>
+      We use cookies to recognize visitors and analyze front end traffic. To learn more about these methods, including
+      how to disable them, view our{' '}
+      <Link href="/cookie-policy">
+        <a rel="noreferrer" target="_blank" className={classes.link}>
+          Cookie Policy
+        </a>
+      </Link>
+    </CookieConsent>
   ) : null
 }
 
