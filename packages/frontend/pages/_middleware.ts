540dc1b8b177d23ba604a5420324b76c42099c77
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-05-23 11:43:33 +0100

    fix: removes comments

--- packages/frontend/pages/_middleware.ts
+++ packages/frontend/pages/_middleware.ts
@@ -1,11 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-// const BLOCKED_COUNTRIES = ['US', 'BY', 'CU', 'IR', 'IQ', 'CI', 'LR', 'KP', 'SD', 'SY', 'ZW']
-
 export default function middleware(request: NextRequest) {
   const country = request.geo?.country
   const url = request.nextUrl
-  // const isRestricted = BLOCKED_COUNTRIES.includes(country ?? '')
 
   if (request.cookies.opyn_geo) {
     const cachedCountry = request.cookies.opyn_geo.split(',')[1]
@@ -21,7 +18,6 @@ export default function middleware(request: NextRequest) {
   }
 
   url.searchParams.set('country', country!)
-  // url.searchParams.set('restricted', String(isRestricted))
 
   return NextResponse.redirect(url)
 }
