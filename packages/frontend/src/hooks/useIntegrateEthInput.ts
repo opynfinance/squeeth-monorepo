e4533aa62c63fb0f372e510538672c6b60e086ae
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-05-30 17:23:28 +0100

    fix: adds description comment to integrate eth input function

--- packages/frontend/src/hooks/useIntegrateEthInput.ts
+++ packages/frontend/src/hooks/useIntegrateEthInput.ts
@@ -13,6 +13,11 @@ export const useIntergrateEthInput = () => {
   const getCollatRatioAndLiqPrice = useGetCollatRatioAndLiqPrice()
   const getSellQuote = useGetSellQuote()
 
+  /**
+   * @param ethDeposited - eth amount inputted by user
+   * @param desiredCollatRatio - collateral ratio chosen by user
+   * @param slippage
+   */
   const integrateETHInput = useAppCallback(
     async (ethDeposited: BigNumber, desiredCollatRatio: number, slippage: BigNumber) => {
       const emptyState = {
@@ -27,19 +32,24 @@ export const useIntergrateEthInput = () => {
         },
       }
 
+      // sst up upper and lower multiplier to calculate eth amount out.
       let start = 0
       let end = 3
 
       let prevState = { ...emptyState }
 
+      // to not loop if no eth input or collateral ratio is below 1.5
       if (ethDeposited.isZero() || desiredCollatRatio <= 1.5) return emptyState
 
       while (start <= end) {
         const middle = (start + end) / 2
+
+        //Get estimated oSqth price based on multiplier
         const estimatedOsqthPrice = new BigNumber(middle)
           .multipliedBy(normFactor)
           .times(ethPrice.div(new BigNumber(10000)))
 
+        // calculate oSqth mint guess from estimatedOsqthPrice above
         const oSQTH_mint_guess = ethDeposited.div(
           new BigNumber(desiredCollatRatio)
             .times(normFactor)
@@ -47,9 +57,12 @@ export const useIntergrateEthInput = () => {
             .minus(estimatedOsqthPrice),
         )
 
+        // calculate actual proceeds from oSQTH_mint_guess sale
         const quote = await getSellQuote(oSQTH_mint_guess, slippage)
         const ethBorrow = quote.minimumAmountOut
         const totalCollat = ethDeposited.plus(ethBorrow)
+
+        // Get collat ratio based on proceeds
         const { collateralPercent: collatRatio, liquidationPrice: liqPrice } = await getCollatRatioAndLiqPrice(
           totalCollat,
           oSQTH_mint_guess,
@@ -57,6 +70,7 @@ export const useIntergrateEthInput = () => {
 
         prevState = { ethBorrow, collatRatio, squeethAmount: oSQTH_mint_guess, quote, liqPrice }
 
+        // stop when collat ratio based on proceeds is equal to collat rato chosen by the user
         if ((collatRatio / 100).toFixed(2) === desiredCollatRatio.toFixed(2)) {
           break
         }
