199bef49d04ab37ddcd78f95a6e8b232dec3bbdc
EJ Liu <jacvd7@gmail.com>
2022-06-06 04:39:02 +0800

    fix: update functions

--- packages/subgraph/src/mappings/core.ts
+++ packages/subgraph/src/mappings/core.ts
@@ -1,77 +1,33 @@
 /* eslint-disable prefer-const */
-import {
-  Bundle,
-  Factory,
-  Pool,
-  Swap,
-  Tick,
-  Token,
-} from "../../generated/schema";
-import { Pool as PoolABI } from "../../generated/Factory/Pool";
-import { BigDecimal, BigInt, ethereum, store } from "@graphprotocol/graph-ts";
-import {
-  Initialize,
-  Swap as SwapEvent,
-} from "../../generated/templates/Pool/Pool";
-import { convertTokenToDecimal, loadTransaction, safeDiv } from "../utils";
-import { FACTORY_ADDRESS, ONE_BI, ZERO_BD, ZERO_BI } from "../utils/constants";
-import {
-  findEthPerToken,
-  getEthPriceInUSD,
-  getTrackedAmountUSD,
-  sqrtPriceX96ToTokenPrices,
-} from "../utils/pricing";
+import { TransactionHistory } from "../../generated/schema";
+import { Swap as SwapEvent } from "../../generated/Pool/Pool";
+import { sqrtPriceX96ToOSQTHTokenPrices } from "../utils/pricing";
+import { BIGINT_ZERO } from "../constants";
 
 export function handleSwap(event: SwapEvent): void {
-  let bundle = Bundle.load("1");
-  let factory = Factory.load(FACTORY_ADDRESS);
-  let pool = Pool.load(event.address.toHexString());
+  let transactionHistory = new TransactionHistory(
+    `${event.transaction.hash.toHex()}-${event.logIndex}`
+  );
 
-  // hot fix for bad pricing
-  if (pool.id == "0x9663f2ca0454accad3e094448ea6f77443880454") {
-    return;
+  // token0 osqth
+  // token1 weth
+  // token0 per token1
+  let prices = sqrtPriceX96ToOSQTHTokenPrices(event.params.sqrtPriceX96);
+  transactionHistory.oSqthPrice = prices[0];
+
+  // selling
+  if (event.params.amount0.gt(BIGINT_ZERO)) {
+    transactionHistory.transactionType = "SELL_OSQTH";
+  }
+  // buying
+  if (event.params.amount0.lt(BIGINT_ZERO)) {
+    transactionHistory.transactionType = "BUY_OSQTH";
   }
 
-  let token0 = Token.load(pool.token0);
-  let token1 = Token.load(pool.token1);
+  transactionHistory.owner = event.transaction.from.toString();
+  transactionHistory.oSqthAmount = event.params.amount0;
+  transactionHistory.ethAmount = event.params.amount1;
+  transactionHistory.timestamp = event.block.timestamp;
 
-  // updated pool ratess
-  let prices = sqrtPriceX96ToTokenPrices(
-    pool.sqrtPrice,
-    token0 as Token,
-    token1 as Token
-  );
-  pool.token0Price = prices[0];
-  pool.token1Price = prices[1];
-  pool.save();
-
-  // update USD pricing
-  bundle.ethPriceUSD = getEthPriceInUSD();
-  bundle.save();
-  token0.derivedETH = findEthPerToken(token0 as Token);
-  token1.derivedETH = findEthPerToken(token1 as Token);
-
-  // create Swap event
-  let transaction = loadTransaction(event);
-  let swap = new Swap(transaction.id + "#" + pool.txCount.toString());
-  swap.transaction = transaction.id;
-  swap.timestamp = transaction.timestamp;
-  swap.pool = pool.id;
-  swap.token0 = pool.token0;
-  swap.token1 = pool.token1;
-  swap.sender = event.params.sender;
-  swap.origin = event.transaction.from;
-  swap.recipient = event.params.recipient;
-  swap.amount0 = amount0;
-  swap.amount1 = amount1;
-  swap.amountUSD = amountTotalUSDTracked;
-  swap.tick = BigInt.fromI32(event.params.tick as i32);
-  swap.sqrtPriceX96 = event.params.sqrtPriceX96;
-  swap.logIndex = event.logIndex;
-
-  swap.save();
-  factory.save();
-  pool.save();
-  token0.save();
-  token1.save();
+  transactionHistory.save();
 }
--- packages/subgraph/src/mappings/position-manager.ts
+++ packages/subgraph/src/mappings/position-manager.ts
@@ -3,104 +3,52 @@ import {
   Collect,
   DecreaseLiquidity,
   IncreaseLiquidity,
-  NonfungiblePositionManager,
-  Transfer,
 } from "../../generated/NonfungiblePositionManager/NonfungiblePositionManager";
-import {
-  Position,
-  PositionSnapshot,
-  Token,
-  TransactionHistory,
-} from "../../generated/schema";
+import { TransactionHistory } from "../../generated/schema";
 
-import { Address, BigInt, ethereum } from "@graphprotocol/graph-ts";
-
-function getPosition(event: ethereum.Event, tokenId: BigInt): Position | null {
-  let position = Position.load(tokenId.toString());
-}
+import { BigDecimal } from "@graphprotocol/graph-ts";
 
 export function handleIncreaseLiquidity(event: IncreaseLiquidity): void {
-  let position = getPosition(event, event.params.tokenId);
-
-  // position was not able to be fetched
-  if (position == null) {
-    return;
-  }
-
-  // temp fix
-  if (
-    Address.fromString(position.pool).equals(
-      Address.fromHexString("0x8fe8d9bb8eeba3ed688069c3d6b556c9ca258248")
-    )
-  ) {
-    return;
-  }
   let transactionHistory = new TransactionHistory(
     `${event.transaction.hash.toHex()}-${event.logIndex}`
   );
 
+  transactionHistory.transactionType = "ADD_LIQUIDITY";
+  transactionHistory.owner = event.transaction.from.toString();
+  transactionHistory.oSqthAmount = event.params.amount0;
+  transactionHistory.ethAmount = event.params.amount1;
   transactionHistory.timestamp = event.block.timestamp;
-  transactionHistory.oSqthAmount = BigInt.zero();
-  transactionHistory.ethAmount = BigInt.zero();
-  transactionHistory.oSqthPrice = BigInt.zero();
+  transactionHistory.oSqthPrice = BigDecimal.zero();
 
   transactionHistory.save();
 }
 
 export function handleDecreaseLiquidity(event: DecreaseLiquidity): void {
-  let position = getPosition(event, event.params.tokenId);
-
-  // position was not able to be fetched
-  if (position == null) {
-    return;
-  }
-
-  // temp fix
-  if (
-    Address.fromString(position.pool).equals(
-      Address.fromHexString("0x8fe8d9bb8eeba3ed688069c3d6b556c9ca258248")
-    )
-  ) {
-    return;
-  }
-
   let transactionHistory = new TransactionHistory(
     `${event.transaction.hash.toHex()}-${event.logIndex}`
   );
 
+  transactionHistory.transactionType = "REMOVE_LIQUIDITY";
+  transactionHistory.owner = event.transaction.from.toString();
+  transactionHistory.oSqthAmount = event.params.amount0;
+  transactionHistory.ethAmount = event.params.amount1;
   transactionHistory.timestamp = event.block.timestamp;
-  transactionHistory.oSqthAmount = BigInt.zero();
-  transactionHistory.ethAmount = BigInt.zero();
-  transactionHistory.oSqthPrice = BigInt.zero();
+  transactionHistory.oSqthPrice = BigDecimal.zero();
 
   transactionHistory.save();
 }
 
 export function handleCollect(event: Collect): void {
-  let position = getPosition(event, event.params.tokenId);
-
-  // position was not able to be fetched
-  if (position == null) {
-    return;
-  }
-
-  // temp fix
-  if (
-    Address.fromString(position.pool).equals(
-      Address.fromHexString("0x8fe8d9bb8eeba3ed688069c3d6b556c9ca258248")
-    )
-  ) {
-    return;
-  }
-
   let transactionHistory = new TransactionHistory(
     `${event.transaction.hash.toHex()}-${event.logIndex}`
   );
 
+  transactionHistory.transactionType = "COLLECT_FEE";
+  transactionHistory.owner = event.transaction.from.toString();
+  transactionHistory.oSqthAmount = event.params.amount0;
+  transactionHistory.ethAmount = event.params.amount1;
   transactionHistory.timestamp = event.block.timestamp;
-  transactionHistory.oSqthAmount = BigInt.zero();
-  transactionHistory.ethAmount = BigInt.zero();
-  transactionHistory.oSqthPrice = BigInt.zero();
+  transactionHistory.oSqthPrice = BigDecimal.zero();
 
   transactionHistory.save();
 }
