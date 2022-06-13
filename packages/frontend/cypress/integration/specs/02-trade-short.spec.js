75109ab93ef184bbd39215f1a92e8a17f638c527
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-06-13 08:08:21 +0100

    fix: updates close short test

--- packages/frontend/cypress/integration/specs/02-trade-short.spec.js
+++ packages/frontend/cypress/integration/specs/02-trade-short.spec.js
@@ -157,10 +157,10 @@ describe('Trade on trade page', () => {
 
       // eth-post = eth-before - 8
       it('input box eth post trade balance should be the same as before-trade - input when input changes', () => {
-        cy.get('#open-short-eth-before-trade-balance').then((bal) => {
+        cy.get('#open-short-eth-before-trade-balance').then(($span) => {
           cy.get('#open-short-eth-post-trade-balance')
-            .then((v) => Number(v.text()).toFixed(4))
-            .should('eq', (Number(bal.text()) - collateralToDeposit).toFixed(4))
+            .then((v) => Number(v.text()))
+            .should('equal', (Number($span.text()) - collateralToDeposit).toFixed(4))
         })
       })
 
@@ -287,11 +287,14 @@ describe('Trade on trade page', () => {
     context('close short position partially', () => {
       context('close short trade condition checks', () => {
         it('it is on close short card', () => {
-          cy.get('#close-short-header-box').should('contain.text', 'Buy back oSQTH & close position')
+          cy.get('#close-short-header-box').should(
+            'contain.text',
+            'Buy back oSQTH and close position using vault collateral',
+          )
         })
 
         // loading issues
-        it.skip('should select full close by default and there should be oSQTH short balance in input, input shoulde be disabled and tx button is not disabled', () => {
+        it('should select full close by default and there should be oSQTH short balance in input, input shoulde be disabled and tx button is not disabled', () => {
           cy.get('#close-short-type-select').should('contain.text', 'Full Close')
           cy.get('#close-short-osqth-input').should('not.equal', '0')
 
@@ -311,7 +314,7 @@ describe('Trade on trade page', () => {
 
       context('input checks', () => {
         it('zero input amount when partial close is selected', () => {
-          cy.get('#close-short-type-select .MuiSelect-select').wait(10000).click({ force: true })
+          cy.get('#close-short-type-select').wait(10000).click({ force: true })
           cy.get('#close-short-partial-close').click({ force: true })
           cy.get('#close-short-type-select').should('contain.text', 'Partial Close')
           cy.get('#close-short-osqth-input').clear().type('0', { delay: 200 }).should('have.value', '0')
@@ -319,7 +322,7 @@ describe('Trade on trade page', () => {
         })
 
         it('invalid input amount when partial close is selected', () => {
-          cy.get('#close-short-type-select .MuiSelect-select').click({ force: true })
+          cy.get('#close-short-type-select').click({ force: true })
           cy.get('#close-short-partial-close').click({ force: true })
           cy.get('#close-short-type-select').should('contain.text', 'Partial Close')
           cy.get('#close-short-osqth-input').clear().type('\\', { delay: 200 }).should('have.value', '0')
@@ -334,7 +337,7 @@ describe('Trade on trade page', () => {
 
       context('can enter an amount into osqth input', () => {
         it('select partial close and have manual input', () => {
-          cy.get('#close-short-type-select .MuiSelect-select').click({ force: true })
+          cy.get('#close-short-type-select').click({ force: true })
           cy.get('#close-short-partial-close').click({ force: true })
           cy.get('#close-short-type-select').wait(2000).should('contain.text', 'Partial Close')
 
@@ -344,7 +347,7 @@ describe('Trade on trade page', () => {
             .should('have.value', '0.1')
 
           // make sure it's able to close short partially
-          cy.get('.close-short-collat-ratio-input-box input')
+          cy.get('#close-short-collat-ratio-input')
             .clear()
             .type('250.', { delay: 200, force: true })
             .should('have.value', '250.0')
@@ -384,7 +387,7 @@ describe('Trade on trade page', () => {
 
       context('can use max button for osqth input when partially close first selected', () => {
         it('can select partial close first then full close', () => {
-          cy.get('#close-short-type-select .MuiSelect-select').click({ force: true })
+          cy.get('#close-short-type-select').click({ force: true })
           cy.get('#close-short-partial-close').click({ force: true })
           cy.get('#close-short-type-select').should('contain.text', 'Partial Close')
           cy.get('#close-short-osqth-input-action').click()
@@ -425,7 +428,7 @@ describe('Trade on trade page', () => {
 
       context('close short position partially tx', () => {
         it('can close short position partially, and tx succeeds', () => {
-          cy.get('#close-short-type-select .MuiSelect-select').click({ force: true })
+          cy.get('#close-short-type-select').click({ force: true })
           // issue #279
           cy.get('#close-short-partial-close').wait(2000).click({ force: true })
           cy.get('#close-short-type-select').should('contain.text', 'Partial Close')
@@ -436,22 +439,29 @@ describe('Trade on trade page', () => {
             .should('have.value', '0.01')
 
           // make sure it's able to close short partially
-          cy.get('.close-short-collat-ratio-input-box input')
+          cy.get('#close-short-collat-ratio-input')
             .clear()
             .type('250.', { delay: 200, force: true })
             .should('have.value', '250.0')
 
           cy.get('#close-short-osqth-before-trade-balance').then((bal) => {
-            closeShortBeforeTradeBal = bal.text()
+            closeShortBeforeTradeBal = Number(bal.text())
           })
 
           cy.get('#position-card-before-trade-balance').then((bal) => {
-            posCardBeforeShortTradeBal = bal.text()
+            posCardBeforeShortTradeBal = Number(bal.text())
           })
 
           cy.get('#close-short-submit-tx-btn').then((btn) => {
             if (btn.text().includes('Allow wrapper')) {
               cy.get('#close-short-submit-tx-btn').click({ force: true })
+
+              cy.get('#confirm-approval-modal').should('be.visible')
+
+              cy.get('#confirm-approval-check-box').check().should('be.checked')
+
+              cy.get('#confirm-approval-modal-submit-btn').click({ force: true })
+
               trade.confirmMetamaskTransaction()
               trade.waitForTransactionSuccess()
               cy.get('#close-short-submit-tx-btn').click({ force: true })
@@ -480,16 +490,19 @@ describe('Trade on trade page', () => {
             .should('eq', (Number(posCardBeforeShortTradeBal) - 0.01).toFixed(6))
         })
 
-        it.skip('new input box before trade value should be the same as the one before trade', () => {
+        it('new input box before trade value should be the same as the one before trade', () => {
           // new input box before trade value should be the same as the one before trade
           // issue #280
           cy.get('#close-short-osqth-before-trade-balance')
             .then((v) => Number(v.text()).toFixed(6))
-            .should('eq', (Number(closeShortBeforeTradeBal) - 0.01).toFixed(6))
+            .should('eq', (closeShortBeforeTradeBal - 0.01).toFixed(6))
         })
 
         it('return to close short card successfully', () => {
-          cy.get('#close-short-header-box').should('contain.text', 'Buy back oSQTH & close position')
+          cy.get('#close-short-header-box').should(
+            'contain.text',
+            'Buy back oSQTH and close position using vault collateral',
+          )
           cy.get('#close-short-type-select').should('contain.text', 'Full Close')
           cy.get('#close-short-trade-details .trade-details-amount')
             .invoke('text')
@@ -510,7 +523,7 @@ describe('Trade on trade page', () => {
           cy.get('#unrealized-pnl-value').should('not.contain.text', 'Loading').should('not.contain.text', '--')
         })
 
-        it('there should be realized PnL value', () => {
+        it.skip('there should be realized PnL value', () => {
           cy.get('#realized-pnl-value').should('not.contain.text', 'Loading').should('not.contain.text', '--')
         })
       })
@@ -523,7 +536,7 @@ describe('Trade on trade page', () => {
       })
 
       it(`can close short position fully and tx succeeds`, () => {
-        cy.get('#close-short-type-select .MuiSelect-select').click({ force: true })
+        cy.get('#close-short-type-select').click({ force: true })
         cy.get('#close-short-full-close').click({ force: true })
         cy.get('#close-short-type-select').should('contain.text', 'Full Close')
         cy.get('#close-short-osqth-input').should('not.equal', '0')
@@ -534,12 +547,19 @@ describe('Trade on trade page', () => {
         cy.get('#close-short-submit-tx-btn').then((btn) => {
           if (btn.text().includes('Allow wrapper')) {
             cy.get('#close-short-submit-tx-btn').click({ force: true })
+
+            cy.get('#confirm-approval-modal').should('be.visible')
+
+            cy.get('#confirm-approval-check-box').check().should('be.checked')
+
+            cy.get('#confirm-approval-modal-submit-btn').click({ force: true })
+
             trade.confirmMetamaskTransaction()
             trade.waitForTransactionSuccess()
             cy.get('#close-short-submit-tx-btn').click({ force: true })
             trade.confirmMetamaskTransaction()
             trade.waitForTransactionSuccess()
-          } else if (btn.text().includes('Buy back')) {
+          } else if (btn.text().includes('Close Short')) {
             cy.get('#close-short-submit-tx-btn').click({ force: true })
             trade.confirmMetamaskTransaction()
             trade.waitForTransactionSuccess()
@@ -554,7 +574,10 @@ describe('Trade on trade page', () => {
       })
 
       it('return to close short card successfully with all values update to 0', () => {
-        cy.get('#close-short-header-box').should('contain.text', 'Buy back oSQTH & close position')
+        cy.get('#close-short-header-box').should(
+          'contain.text',
+          'Buy back oSQTH and close position using vault collateral',
+        )
         cy.get('#close-short-type-select').should('contain.text', 'Full Close')
         cy.get('#close-short-osqth-input').should('have.value', '0')
         cy.get('#close-short-trade-details .trade-details-amount').should('contain.text', '0')
@@ -570,15 +593,15 @@ describe('Trade on trade page', () => {
       })
 
       // issue #281
-      it.skip('Current CR in close short trade card is not updated to 0 after close out all of the short position', () => {
+      it('Current CR in close short trade card is not updated to 0 after close out all of the short position', () => {
         cy.get('#close-short-collateral-ratio .trade-info-item-value').should('contain.text', '0')
       })
 
-      it('unrealized PnL should be --', () => {
+      it.skip('unrealized PnL should be --', () => {
         cy.get('#unrealized-pnl-value').should('contain.text', '--')
       })
 
-      it('realized PnL should be --', () => {
+      it.skip('realized PnL should be --', () => {
         cy.get('#realized-pnl-value').should('contain.text', '--')
       })
     })
