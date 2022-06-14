fe74c7715e12289999391dcd3a0432143a3ca304
EJ Liu <jacvd7@gmail.com>
2022-06-15 03:11:13 +0800

    feat: add useCurrentPrices hook to pull prices

--- packages/frontend/src/hooks/useCurrentPrices.ts
+++ packages/frontend/src/hooks/useCurrentPrices.ts
@@ -1,5 +1,23 @@
-import BigNumber from 'bignumber.js'
+import { useQuery } from '@apollo/client'
+import { POOLS_QUERY } from '@queries/squeeth/poolsQuery'
+import { pools } from '@queries/squeeth/__generated__/pools'
+import { squeethClient } from '@utils/apollo-client'
+import { BigNumber } from 'bignumber.js'
+import { useAtomValue } from 'jotai'
+import { addressAtom, networkIdAtom } from 'src/state/wallet/atoms'
 
 export default function useCurrentPrices() {
-  return { ethPrice: new BigNumber(0), oSqthPrice: new BigNumber(0) }
+  const address = useAtomValue(addressAtom)
+  const networkId = useAtomValue(networkIdAtom)
+
+  const { data: { pools } = {} } = useQuery<pools>(POOLS_QUERY, {
+    client: squeethClient[networkId],
+    skip: !address,
+  })
+
+  const ethPrice = pools ? new BigNumber(pools[0]?.token0Price) : new BigNumber(0)
+  return {
+    ethPrice: ethPrice,
+    oSqthPrice: pools ? new BigNumber(pools[1]?.token1Price).times(ethPrice) : new BigNumber(0),
+  }
 }
