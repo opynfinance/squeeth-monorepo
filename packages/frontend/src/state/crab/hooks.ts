16627c73e97f3a984b9e583086f0535972c5dab7
adam5909099 <adam5909099@gmail.com>
2022-06-14 22:39:19 -0400

    Revert "Update loading"
    This reverts commit 0d3345de8a518fc51c6aa00d2f151f6b65491e96.

--- packages/frontend/src/state/crab/hooks.ts
+++ packages/frontend/src/state/crab/hooks.ts
@@ -153,7 +153,6 @@ export const useCurrentCrabPositionValue = () => {
         return
       }
 
-      setCurrentEthLoading(false)
       setCurrentCrabPositionValue(getWSqueethPositionValue(net))
       setCurrentCrabPositionValueInETH(getWSqueethPositionValueInETH(net))
     })()
