d61e3806e9ffbdbff8f9ff7a6f86b3f58e38fb3d
EJ Liu <jacvd7@gmail.com>
2022-06-16 20:59:20 +0800

    fix: add position type check condition

--- packages/frontend/src/hooks/usePositionNPnL.ts
+++ packages/frontend/src/hooks/usePositionNPnL.ts
@@ -1,3 +1,4 @@
+import { PositionType } from '../types/index'
 import { calculatePnL } from 'src/lib'
 import useAccounts from './useAccounts'
 import useCurrentPrices from './useCurrentPrices'
@@ -5,6 +6,7 @@ import useCurrentPrices from './useCurrentPrices'
 export default function usePositionNPnL() {
   const { ethPrice, oSqthPrice, loading: isCurrentPriceLoading } = useCurrentPrices()
   const { positions, lpPosition, loading: isPositionsLoading, accShortAmount } = useAccounts()
+  let positionType = PositionType.NONE
 
   const {
     currentPositionValue,
@@ -24,10 +26,19 @@ export default function usePositionNPnL() {
     realizedPnLInPerct: lpRealizedPnLInPerct,
   } = calculatePnL(lpPosition, oSqthPrice, ethPrice)
 
+  if (currentOSQTHAmount.gt(accShortAmount)) {
+    positionType = PositionType.LONG
+  } else if (currentOSQTHAmount.lt(accShortAmount)) {
+    positionType = PositionType.SHORT
+  } else {
+    positionType = PositionType.NONE
+  }
+
   return {
     currentPositionValue,
     currentETHAmount,
     currentOSQTHAmount,
+    positionType,
     loading: isCurrentPriceLoading || isPositionsLoading,
     unrealizedPnL,
     unrealizedPnLInPerct,
