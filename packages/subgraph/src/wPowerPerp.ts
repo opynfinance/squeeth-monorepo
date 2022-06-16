96ecb2437c845910dad6884dfd2136327a560a22
EJ Liu <jacvd7@gmail.com>
2022-06-17 04:50:12 +0800

    fix: add address check to see if it's EOA otc transfer

--- packages/subgraph/src/wPowerPerp.ts
+++ packages/subgraph/src/wPowerPerp.ts
@@ -1,13 +1,16 @@
-import { Address, BigDecimal } from "@graphprotocol/graph-ts";
+import { BigDecimal, log } from "@graphprotocol/graph-ts";
 import { Pool } from "../generated/schema";
 import { Transfer } from "../generated/WPowerPerp/WPowerPerp";
+import { CONTROLLER_ADDR, SHORT_HELPER_ADDR } from "./constants";
 import { buyOrSellSQTH, createTransactionHistory } from "./util";
 
 export function handleTransfer(event: Transfer): void {
   let pool = Pool.load(event.address.toHexString());
-
-  // If it's from pool, it means buying/selling and shouldn't handle PnL and history logic
-  if (pool != null) {
+  if (
+    pool != null ||
+    event.transaction.to == SHORT_HELPER_ADDR ||
+    event.transaction.to == CONTROLLER_ADDR
+  ) {
     return;
   }
 
@@ -26,7 +29,7 @@ export function handleTransfer(event: Transfer): void {
     receiverTransactionHistory.owner = receipt;
     receiverTransactionHistory.oSqthAmount = event.params.value;
     receiverTransactionHistory.save();
-    buyOrSellSQTH(receipt.toHexString(), amount);
+    buyOrSellSQTH(receipt.toHex(), amount);
   }
 
   buyOrSellSQTH(event.transaction.from.toHex(), amount.neg());
