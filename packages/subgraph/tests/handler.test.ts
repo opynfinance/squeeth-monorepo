dd95046c8d8bbb45ba71dfa7d89388c7b9d00894
adam5909099 <adam5909099@gmail.com>
2022-06-20 10:55:34 -0400

    Add test cases for long

--- packages/subgraph/tests/handler.test.ts
+++ packages/subgraph/tests/handler.test.ts
@@ -1,21 +1,49 @@
 import { BigDecimal } from "@graphprotocol/graph-ts";
-import { assert, clearStore, test } from "matchstick-as";
+import { assert, clearStore, test, describe, beforeEach } from "matchstick-as";
 import { handleOSQTHChange } from "../src/utils/handler";
-// import { handleOSQTHChange } from "../src/utils/handler";
 import { loadOrCreatePosition } from "../src/utils/loadInit";
 
-test("handleOSQTHChange handles long position", () => {
-  let userAddr = "0x02a248876233fd78b97ecf1e18114b8eb22ba603";
+describe("handleOSQTHChange", () => {
+  beforeEach(() => {
+    clearStore();
+  });
 
-  // Open long position with 5 sqth (osqthPriceInUSD: $1000)
-  let position = loadOrCreatePosition(userAddr);
-  handleOSQTHChange(
-    userAddr,
-    BigDecimal.fromString("5"),
-    BigDecimal.fromString("1000")
-  );
+  test("opens long (5 sqth, sqthPrice: $10)", () => {
+    let userAddr = "0x02a248876233fd78b97ecf1e18114b8eb22ba603";
+    let position = loadOrCreatePosition(userAddr);
 
-  assert.fieldEquals("Position", position.id, "currentOSQTHAmount", "5");
+    handleOSQTHChange(
+      userAddr,
+      BigDecimal.fromString("5"),
+      BigDecimal.fromString("10")
+    );
 
-  clearStore();
+    assert.fieldEquals("Position", position.id, "currentOSQTHAmount", "5");
+    assert.fieldEquals(
+      "Position",
+      position.id,
+      "unrealizedOSQTHUnitCost",
+      "10"
+    );
+  });
+
+  test("opens long (5 sqth, sqthPrice: $10) and partically close long (2 sqth, sqthPrice: $11)", () => {
+    let userAddr = "0x02a248876233fd78b97ecf1e18114b8eb22ba603";
+    let position = loadOrCreatePosition(userAddr);
+
+    handleOSQTHChange(
+      userAddr,
+      BigDecimal.fromString("5"),
+      BigDecimal.fromString("10")
+    );
+
+    handleOSQTHChange(
+      userAddr,
+      BigDecimal.fromString("-3"),
+      BigDecimal.fromString("12")
+    );
+
+    assert.fieldEquals("Position", position.id, "currentOSQTHAmount", "2");
+    assert.fieldEquals("Position", position.id, "unrealizedOSQTHUnitCost", "7");
+  });
 });
