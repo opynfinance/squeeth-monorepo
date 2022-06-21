32e9441995bd6388ec01ea362c82e7ddca306d2f
adam5909099 <adam5909099@gmail.com>
2022-06-20 23:42:23 -0400

    Add test cases for short

--- packages/subgraph/tests/handler.test.ts
+++ packages/subgraph/tests/handler.test.ts
@@ -106,4 +106,86 @@ describe("handleOSQTHChange", () => {
     assert.fieldEquals("Position", position.id, "realizedOSQTHUnitGain", "0");
     assert.fieldEquals("Position", position.id, "realizedOSQTHAmount", "0");
   });
+
+  test("opens short (5 sqth, sqthPrice: $10)", () => {
+    let position = loadOrCreatePosition(userAddr);
+
+    handleOSQTHChange(
+      userAddr,
+      BigDecimal.fromString("-5"),
+      BigDecimal.fromString("10")
+    );
+
+    assert.fieldEquals("Position", position.id, "currentOSQTHAmount", "-5");
+    assert.fieldEquals(
+      "Position",
+      position.id,
+      "unrealizedOSQTHUnitCost",
+      "10"
+    );
+    assert.fieldEquals("Position", position.id, "realizedOSQTHUnitCost", "10");
+  });
+
+  test("opens short (5 sqth, sqthPrice: $10) and partially close short (2 sqth, $8)", () => {
+    let position = loadOrCreatePosition(userAddr);
+
+    handleOSQTHChange(
+      userAddr,
+      BigDecimal.fromString("-5"),
+      BigDecimal.fromString("10")
+    );
+
+    handleOSQTHChange(
+      userAddr,
+      BigDecimal.fromString("2"),
+      BigDecimal.fromString("8")
+    );
+
+    assert.fieldEquals("Position", position.id, "currentOSQTHAmount", "-3");
+    assert.fieldEquals(
+      "Position",
+      position.id,
+      "unrealizedOSQTHUnitCost",
+      "11.33333333333333333333333333333333"
+    ); // -34/-3
+    assert.fieldEquals("Position", position.id, "realizedOSQTHUnitGain", "8");
+    assert.fieldEquals("Position", position.id, "realizedOSQTHAmount", "2");
+  });
+
+  test("opens short (5 sqth, sqthPrice: $10), partially close short (2 sqth, $8), and partially close short (1 sqth, $12)", () => {
+    let position = loadOrCreatePosition(userAddr);
+
+    handleOSQTHChange(
+      userAddr,
+      BigDecimal.fromString("-5"),
+      BigDecimal.fromString("10")
+    );
+
+    handleOSQTHChange(
+      userAddr,
+      BigDecimal.fromString("2"),
+      BigDecimal.fromString("8")
+    );
+
+    handleOSQTHChange(
+      userAddr,
+      BigDecimal.fromString("1"),
+      BigDecimal.fromString("12")
+    );
+
+    assert.fieldEquals("Position", position.id, "currentOSQTHAmount", "-2");
+    assert.fieldEquals(
+      "Position",
+      position.id,
+      "unrealizedOSQTHUnitCost",
+      "11"
+    ); // -22/-2
+    assert.fieldEquals(
+      "Position",
+      position.id,
+      "realizedOSQTHUnitGain",
+      "-9.333333333333333333333333333333333"
+    ); // 28/3
+    assert.fieldEquals("Position", position.id, "realizedOSQTHAmount", "3");
+  });
 });
