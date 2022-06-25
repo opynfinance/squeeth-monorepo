50770c09ac5c61527ac860f748f9b8ec09ed3cf1
Darya Kaviani <daryakaviani@daryas-mbp.lan>
2022-06-25 11:36:24 -0700

    add apollo client subgraph framework (need links)

--- packages/frontend/src/utils/apollo-client.ts
+++ packages/frontend/src/utils/apollo-client.ts
@@ -10,12 +10,20 @@ const httpLinkRP = new HttpLink({
   uri: 'https://api.thegraph.com/subgraphs/name/kmkoushik/uniswap-v3-ropsten',
 })
 
+const httpLinkGL = new HttpLink({
+  uri: '',
+})
+
 const httpLinkRPSqueeth = new HttpLink({
   uri: 'https://api.thegraph.com/subgraphs/name/opynfinance/squeeth-ropsten',
 })
 
 const httpLinkMNSqueeth = new HttpLink({
   uri: 'https://api.thegraph.com/subgraphs/name/opynfinance/squeeth',
+})
+
+const httpLinkGLSqueeth = new HttpLink({
+  uri: '',
 })
 
 const wsLinkRP =
@@ -38,6 +46,16 @@ const wsLinkMN =
       })
     : null
 
+  const wsLinkGL =
+    typeof window !== 'undefined'
+      ? new WebSocketLink({
+          uri: '',
+          options: {
+            reconnect: true,
+          },
+        })
+      : null
+
 const wsLinkRPSqueeth =
   typeof window !== 'undefined'
     ? new WebSocketLink({
@@ -52,6 +70,17 @@ const wsLinkMNSqueeth =
   typeof window !== 'undefined'
     ? new WebSocketLink({
         uri: 'wss://api.thegraph.com/subgraphs/name/opynfinance/squeeth',
+        options: {
+          reconnect: true,
+        },
+      })
+    : null
+
+
+const wsLinkGLSqueeth =
+  typeof window !== 'undefined'
+    ? new WebSocketLink({
+        uri: '',
         options: {
           reconnect: true,
         },
@@ -79,9 +108,15 @@ const ropsten = new ApolloClient({
   cache: new InMemoryCache(),
 })
 
+const goerli = new ApolloClient({
+  link: typeof window !== 'undefined' ? splitLink(wsLinkGL, httpLinkGL) : undefined,
+  cache: new InMemoryCache(),
+})
+
 export const uniswapClient = {
   1: mainnet,
   3: ropsten,
+  5: goerli,
   31337: mainnet, // Can be replaced with local graph node if needed
   421611: mainnet, // Should be replaced with arbitrum subgraph
 }
@@ -96,9 +131,15 @@ const squeethRopsten = new ApolloClient({
   cache: new InMemoryCache(),
 })
 
+const squeethGoerli = new ApolloClient({
+  link: typeof window !== 'undefined' ? splitLink(wsLinkGLSqueeth, httpLinkGLSqueeth) : undefined,
+  cache: new InMemoryCache(),
+})
+
 export const squeethClient = {
   1: squeethMainnet,
   3: squeethRopsten,
+  5: squeethGoerli,
   31337: squeethMainnet, // Can be replaced with local graph node if needed
   421611: squeethMainnet, // Should be replaced with arbitrum subgraph
 }
