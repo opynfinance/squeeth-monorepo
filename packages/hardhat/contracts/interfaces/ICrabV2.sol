d6d02476f5fc7ca927dcdb2cf67712b81cffdc3a
alpinechicken <joecmail@gmail.com>
2022-06-26 16:05:11 -0700

    lint

--- packages/hardhat/contracts/interfaces/ICrabV2.sol
+++ packages/hardhat/contracts/interfaces/ICrabV2.sol
@@ -4,47 +4,14 @@ pragma solidity >=0.7.0 <0.9.0;
 pragma experimental ABIEncoderV2;
 
 interface MyInterface {
-    event Approval(
-        address indexed owner,
-        address indexed spender,
-        uint256 value
-    );
-    event Deposit(
-        address indexed depositor,
-        uint256 wSqueethAmount,
-        uint256 lpAmount
-    );
-    event FlashDeposit(
-        address indexed depositor,
-        uint256 depositedAmount,
-        uint256 tradedAmountOut
-    );
-    event FlashDepositCallback(
-        address indexed depositor,
-        uint256 flashswapDebt,
-        uint256 excess
-    );
-    event FlashWithdraw(
-        address indexed withdrawer,
-        uint256 crabAmount,
-        uint256 wSqueethAmount
-    );
-    event FlashWithdrawCallback(
-        address indexed withdrawer,
-        uint256 flashswapDebt,
-        uint256 excess
-    );
-    event HedgeOTC(
-        address trader,
-        uint256 quantity,
-        uint256 price,
-        bool isBuying,
-        uint256 clearingPrice
-    );
-    event OwnershipTransferred(
-        address indexed previousOwner,
-        address indexed newOwner
-    );
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Deposit(address indexed depositor, uint256 wSqueethAmount, uint256 lpAmount);
+    event FlashDeposit(address indexed depositor, uint256 depositedAmount, uint256 tradedAmountOut);
+    event FlashDepositCallback(address indexed depositor, uint256 flashswapDebt, uint256 excess);
+    event FlashWithdraw(address indexed withdrawer, uint256 crabAmount, uint256 wSqueethAmount);
+    event FlashWithdrawCallback(address indexed withdrawer, uint256 flashswapDebt, uint256 excess);
+    event HedgeOTC(address trader, uint256 quantity, uint256 price, bool isBuying, uint256 clearingPrice);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
     event SetHedgePriceThreshold(uint256 newHedgePriceThreshold);
     event SetHedgeTimeThreshold(uint256 newHedgeTimeThreshold);
     event SetHedgingTwapPeriod(uint32 newHedgingTwapPeriod);
@@ -52,26 +19,14 @@ interface MyInterface {
     event SetStrategyCap(uint256 newCapAmount);
     event Transfer(address indexed from, address indexed to, uint256 value);
     event VaultTransferred(address indexed newStrategy, uint256 vaultId);
-    event Withdraw(
-        address indexed withdrawer,
-        uint256 crabAmount,
-        uint256 wSqueethAmount,
-        uint256 ethWithdrawn
-    );
-    event WithdrawShutdown(
-        address indexed withdrawer,
-        uint256 crabAmount,
-        uint256 ethWithdrawn
-    );
+    event Withdraw(address indexed withdrawer, uint256 crabAmount, uint256 wSqueethAmount, uint256 ethWithdrawn);
+    event WithdrawShutdown(address indexed withdrawer, uint256 crabAmount, uint256 ethWithdrawn);
 
     function DOMAIN_SEPARATOR() external view returns (bytes32);
 
     function POWER_PERP_PERIOD() external view returns (uint32);
 
-    function allowance(address owner, address spender)
-        external
-        view
-        returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
 
     function approve(address spender, uint256 amount) external returns (bool);
 
@@ -85,9 +40,7 @@ interface MyInterface {
 
     function decimals() external view returns (uint8);
 
-    function decreaseAllowance(address spender, uint256 subtractedValue)
-        external
-        returns (bool);
+    function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool);
 
     function deposit() external payable;
 
@@ -113,10 +66,7 @@ interface MyInterface {
             uint256
         );
 
-    function getWsqueethFromCrabAmount(uint256 _crabAmount)
-        external
-        view
-        returns (uint256);
+    function getWsqueethFromCrabAmount(uint256 _crabAmount) external view returns (uint256);
 
     function hedgeOTC(
         uint256 _totalQuantity,
@@ -131,9 +81,7 @@ interface MyInterface {
 
     function hedgingTwapPeriod() external view returns (uint32);
 
-    function increaseAllowance(address spender, uint256 addedValue)
-        external
-        returns (bool);
+    function increaseAllowance(address spender, uint256 addedValue) external returns (bool);
 
     function incrementNonce() external returns (uint256 current);
 
@@ -188,9 +136,7 @@ interface MyInterface {
 
     function totalSupply() external view returns (uint256);
 
-    function transfer(address recipient, uint256 amount)
-        external
-        returns (bool);
+    function transfer(address recipient, uint256 amount) external returns (bool);
 
     function transferFrom(
         address sender,
--- packages/hardhat/contracts/periphery/CrabHelper.sol
+++ packages/hardhat/contracts/periphery/CrabHelper.sol
@@ -5,7 +5,6 @@ pragma abicoder v2;
 
 // interface
 import {ICrabV2} from "../interfaces/ICrabV2.sol";
-import {IWPowerPerp} from "../interfaces/IWPowerPerp.sol";
 import {IOracle} from "../interfaces/IOracle.sol";
 import {IWETH9} from "../interfaces/IWETH9.sol";
 // lib
@@ -19,7 +18,7 @@ import {ECDSA} from "@openzeppelin/contracts/cryptography/ECDSA.sol";
  * @notice Contract for Crab strategy
  * @author Opyn team
  */
-contract CrabHelper  {
+contract CrabHelper {
     using StrategyMath for uint256;
     using Address for address payable;
 
@@ -43,7 +42,6 @@ contract CrabHelper  {
     address public immutable weth;
     address public immutable wPowerPerp;
 
-
     struct Order {
         uint256 bidId;
         address trader;
@@ -59,10 +57,10 @@ contract CrabHelper  {
 
     /**
      * @notice constructor
-     * @dev uses crabV2 public params 
+     * @dev uses crabV2 public params
      * @param _crabV2 crabV2 address
      */
-    constructor(address _crabV2){
+    constructor(address _crabV2) {
         ICrabV2 crabv2 = ICrabV2(_crabV2);
         // get addresses from crabv2 contract
         ethWSqueethPool = crabv2.ethWSqueethPool;
@@ -72,8 +70,7 @@ contract CrabHelper  {
         hedgingTwapPeriod = crabV2.hedgingFrequency;
         weth = crabV2.weth;
         wPowerPerp = crabV2.wPowerPerp;
-
-}
+    }
 
     /**
      * @dev get current nonce of the address
@@ -95,7 +92,7 @@ contract CrabHelper  {
         return (syncedStrategyDebt, syncedStrategyCollateral);
     }
 
-       /// @dev typehash for signed orders
+    /// @dev typehash for signed orders
     bytes32 private constant _CRAB_BALANCE_TYPEHASH =
         keccak256(
             "Order(uint256 bidId,address trader,uint256 quantity,uint256 price,bool isBuying,uint256 expiry,uint256 nonce)"
@@ -106,10 +103,7 @@ contract CrabHelper  {
      * @param _order crab ratio
      * @return isValid true if order is good
      */
-    function _verifyOrder(
-        Order memory _order
-    ) external view returns (bool) {
-
+    function _verifyOrder(Order memory _order) external view returns (bool) {
         bytes32 structHash = keccak256(
             abi.encode(
                 _CRAB_BALANCE_TYPEHASH,
@@ -132,38 +126,41 @@ contract CrabHelper  {
         uint256 wethAmount = _order.quantity.mul(_order.price).div(1e18);
 
         if (_order.isBuying) {
-            // check weth balance and allowance 
+            // check weth balance and allowance
             require(IWETH9(weth).balanceOf(_order.trader) >= wethAmount, "Not enough weth balance for trade");
-            require(IWETH9(weth).allowance(_order.trader, address(this)) >= wethAmount, "Not enough weth allowance for trade");
+            require(
+                IWETH9(weth).allowance(_order.trader, address(this)) >= wethAmount,
+                "Not enough weth allowance for trade"
+            );
             // check allowance
         } else {
             // check wPowerPerp balance and allowance
-            require(IWETH9(wPowerPerp).balanceOf(_order.trader) >= _order.quantity, "Not enough wPowerPerp balance for trade");
-            require(IWETH9(wPowerPerp).allowance(_order.trader, address(this)) >= _order.quantity, "Not enough wPowerPerp balance for trade");
+            require(
+                IWETH9(wPowerPerp).balanceOf(_order.trader) >= _order.quantity,
+                "Not enough wPowerPerp balance for trade"
+            );
+            require(
+                IWETH9(wPowerPerp).allowance(_order.trader, address(this)) >= _order.quantity,
+                "Not enough wPowerPerp balance for trade"
+            );
         }
-       return true;
+        return true;
     }
 
     /**
      * @notice view function for hedge size based on current state
      * @return hedge amount, isSellingSqueeth
      */
-    function getHedgeSize() external view returns (uint256, bool){
+    function getHedgeSize() external view returns (uint256, bool) {
         // Get state and calculate hedge
         (uint256 strategyDebt, uint256 ethDelta) = _syncStrategyState();
-        uint256 wSqueethEthPrice = IOracle(oracle).getTwap(
-            ethWSqueethPool,
-            wPowerPerp,
-            weth,
-            hedgingTwapPeriod,
-            true
-        );
+        uint256 wSqueethEthPrice = IOracle(oracle).getTwap(ethWSqueethPool, wPowerPerp, weth, hedgingTwapPeriod, true);
 
         uint256 wSqueethDelta = strategyDebt.wmul(2e18).wmul(wSqueethEthPrice);
 
-        return (wSqueethDelta > ethDelta)
+        return
+            (wSqueethDelta > ethDelta)
                 ? ((wSqueethDelta.sub(ethDelta)).wdiv(wSqueethEthPrice), false)
                 : ((ethDelta.sub(wSqueethDelta)).wdiv(wSqueethEthPrice), true);
     }
-
 }
