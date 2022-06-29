b4e2fe0c053cb44e890887979b18294e8a72cb9a
KMKoushik <koushikmohan1996@gmail.com>
2022-06-30 09:57:55 +1000

    Add more migration code

--- packages/frontend/src/components/Strategies/Crab/CrabMigrate.tsx
+++ packages/frontend/src/components/Strategies/Crab/CrabMigrate.tsx
@@ -13,6 +13,9 @@ import { useTokenBalance } from '@hooks/contracts/useTokenBalance'
 import useAppEffect from '@hooks/useAppEffect'
 import BigNumber from 'bignumber.js'
 import useAppCallback from '@hooks/useAppCallback'
+import { useInitCrabMigration, useQueueMigrate } from 'src/state/crabMigration/hooks'
+import { currentCrabPositionValueAtom, currentCrabPositionValueInETHAtom } from 'src/state/crab/atoms'
+import TradeInfoItem from '@components/Trade/TradeInfoItem'
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -33,6 +36,9 @@ const useStyles = makeStyles((theme) =>
       fontSize: '14px',
       marginTop: theme.spacing(0.5),
     },
+    position: {
+      marginTop: theme.spacing(2),
+    },
   }),
 )
 
@@ -48,34 +54,43 @@ const getAction = (action: MIGRATION_STEP) => {
 }
 
 const CrabMigration: React.FC = () => {
+  useInitCrabMigration()
   const classes = useStyles()
   const { crabMigration, crabStrategy } = useAtomValue(addressesAtom)
+  const positionInEth = useAtomValue(currentCrabPositionValueInETHAtom)
+  const positionInUsd = useAtomValue(currentCrabPositionValueAtom)
   const { value: userCrabBalance } = useTokenBalance(crabStrategy, 15, 18)
   const { allowance, isLoadingAllowance, approve } = useUserAllowance(crabStrategy, crabMigration)
+  const queueMigrate = useQueueMigrate()
 
-  const [amount, setAmount] = useState(userCrabBalance.toString())
   const [action, setAction] = useState(MIGRATION_STEP.APPROVE)
   const [loadingTx, setLoadingTx] = useState(false)
 
   useAppEffect(() => {
-    if (allowance.gte(new BigNumber(amount))) {
+    if (allowance.gte(userCrabBalance)) {
       setAction(MIGRATION_STEP.MIGRATE)
     }
-  }, [allowance, amount])
+  }, [allowance, userCrabBalance])
 
   const executeAction = useCallback(
     async (action: MIGRATION_STEP) => {
+      setLoadingTx(true)
       if (action === MIGRATION_STEP.APPROVE) {
-        setLoadingTx(true)
         try {
           await approve(() => null)
         } catch (e) {
           console.log(e)
         }
-        setLoadingTx(false)
+      } else if (action === MIGRATION_STEP.MIGRATE) {
+        try {
+          await queueMigrate(userCrabBalance)
+        } catch (e) {
+          console.log(e)
+        }
       }
+      setLoadingTx(false)
     },
-    [approve, setLoadingTx],
+    [userCrabBalance, approve, queueMigrate],
   )
 
   const loading = useMemo(() => {
@@ -84,32 +99,32 @@ const CrabMigration: React.FC = () => {
 
   return (
     <div className={classes.container}>
-      <PrimaryInput
-        id="crab-deposit-eth-input"
-        value={amount}
-        onChange={(v) => setAmount(v)}
-        label="Amount"
-        tooltip="CRAB token to queue"
-        actionTxt="Max"
-        unit="CRAB"
-        hint="Balance"
-        onActionClicked={() => console.log('none')}
-      />
       <div className={classes.infoBox}>
         <InfoIcon className={classes.infoIcon} />
-
         <Typography color="textSecondary" variant="caption" style={{ marginLeft: '4px' }}>
-          Your funds are still participating in crab v1 and will be deposited in crab v2 upon launch.{' '}
+          Your position is still participating in crab v1 and will be migrated upon crab v2 launch. The migrated ETH
+          amount may be different than your current ETH amount since crab v1 will be active until then.{' '}
           <LinkWrapper href="https://www.notion.so/opynopyn/Crab-Migration-FAQ-Draft-1b79e3c2b98641b08745d5cc72c94a5c">
             {' '}
             Learn more.
           </LinkWrapper>
         </Typography>
       </div>
+      <div className={classes.position}>
+        <TradeInfoItem
+          label="Crab Position"
+          value={`${positionInEth.toFixed(4)} ETH`}
+          unit={`$${positionInUsd.toFixed(1)}`}
+        />
+        {/* <Typography variant="body2" color="textSecondary">
+          Crab Position
+        </Typography>
+        <Typography variant="body2">{positionInEth.toFixed(6)} ETH</Typography> */}
+      </div>
       <PrimaryButton
         variant="contained"
         style={{ marginTop: '16px' }}
-        disabled={loading}
+        disabled={loading || userCrabBalance.isZero()}
         onClick={() => executeAction(action)}
       >
         {loading ? <CircularProgress color="primary" size="1.5rem" /> : getAction(action)}
