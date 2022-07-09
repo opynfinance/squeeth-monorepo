c06aea3ad6f8e496659af484a1079b7eb4161542
Alexis Gauba <alexisgauba@gmail.com>
2022-07-08 19:06:52 -0700

    fix: copy

--- packages/frontend/src/components/Strategies/Crab/MigrationNotice.tsx
+++ packages/frontend/src/components/Strategies/Crab/MigrationNotice.tsx
@@ -25,7 +25,7 @@ const MigrationNotice: React.FC = () => {
       <div style={{ marginLeft: '8px' }}>
         <Typography variant="body1">Crab V2 Early Access: Gaurantee your spot </Typography>
         <Typography component="p" variant="caption" style={{ marginTop: '4px', lineHeight: 1.5 }} color="textSecondary">
-          As OG Day 1 crabbers, you get to commit to v2 before anyone else. Gaurantee your spot in crab v2 right when
+          As OG Day 1 crabbers, you get to commit to v2 before anyone else. Gaurantee your spot in crab v2 right when it
           drops!
         </Typography>
       </div>
