2609b27ac89d6464c0ad338cf9d19803e2546f59
KM Koushik <koushikmohan1996@gmail.com>
2022-07-12 09:16:35 +1000

    Update address.ts

--- packages/frontend/src/constants/address.ts
+++ packages/frontend/src/constants/address.ts
@@ -111,7 +111,7 @@ export const CRAB_STRATEGY: Address = {
 
 export const CRAB_MIGRATION: Address = {
   1: '0x1d1e0838d97996eD3f46b24f52533E3d09F02BcB',
-  3: '0x1d1e0838d97996eD3f46b24f52533E3d09F02BcB',
+  3: '0x380B43a55d672951b8aa193780134af03EBeF39E',
   421611: '',
   31337: '',
 }
