27b0146b6f0c3609672c7185061bdb675169da13
Darya Kaviani <daryakaviani@Daryas-MacBook-Pro.local>
2022-07-12 23:12:11 -0700

    tick space

--- packages/frontend/src/state/lp/hooks.ts
+++ packages/frontend/src/state/lp/hooks.ts
@@ -208,6 +208,9 @@ export const useOpenPositionDeposit = () => {
   return openPositionDeposit
 }
 
+/*** CONSTANTS ***/
+const TICK_SPACE = 60
+
 /*** GETTERS ***/
 
 export const useGetPosition = () => {
