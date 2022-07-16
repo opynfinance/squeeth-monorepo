3829b01cedcb4825a51643623174696c6bd413f5
Darya Kaviani <daryakaviani@Daryas-MacBook-Pro.local>
2022-07-15 22:19:57 -0700

    variable vault id

--- packages/frontend/src/state/lp/hooks.ts
+++ packages/frontend/src/state/lp/hooks.ts
@@ -34,7 +34,7 @@ export const useOpenPositionDeposit = () => {
   const getTwapSqueethPrice = useGetTwapSqueethPrice()
   const getDebtAmount = useGetDebtAmount()
   const openPositionDeposit = useAppCallback(
-    async (squeethToMint: BigNumber, lowerTickInput: number, upperTickInput: number, onTxConfirmed?: () => void) => {
+    async (squeethToMint: BigNumber, lowerTickInput: number, upperTickInput: number, vaultId: number, onTxConfirmed?: () => void) => {
       if (!contract || !address) return null
       
       const squeethPrice = await getTwapSqueethPrice()
@@ -49,7 +49,7 @@ export const useOpenPositionDeposit = () => {
 
       const flashloanWMintDepositNftParams = {
         wPowerPerpPool: squeethPool,
-        vaultId: 0,
+        vaultId: vaultId,
         wPowerPerpAmount: mintWSqueethAmount.toFixed(0),
         collateralToDeposit: collateralToMint.toFixed(0),
         collateralToFlashloan: collateralToMint.toFixed(0),
