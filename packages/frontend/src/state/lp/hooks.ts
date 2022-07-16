d73e710bbe5d97de4e137a0153e7ad5c7ec0bcf5
Darya Kaviani <daryakaviani@Daryas-MacBook-Pro.local>
2022-07-15 23:11:49 -0700

    null unitokenid

--- packages/frontend/src/state/lp/hooks.ts
+++ packages/frontend/src/state/lp/hooks.ts
@@ -142,7 +142,8 @@ export const useCollectFees = () => {
       !controllerHelperContract ||
       !address ||
       !vaultBefore ||
-      !vaultBefore.shortAmount
+      !vaultBefore.shortAmount ||
+      !uniTokenId
     )
       return
 
