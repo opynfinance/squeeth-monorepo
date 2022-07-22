4070631795274397198579490060c3694b38b0d9
adam5909099 <adam5909099@gmail.com>
2022-07-22 05:34:13 -0400

    Revert "Working on handling swap"
    This reverts commit 5f7ebffe4467bda20dfd0e71433e90c8d375fdd3.

--- packages/subgraph/src/wPowerPerp.ts
+++ packages/subgraph/src/wPowerPerp.ts
@@ -1,4 +1,4 @@
-import { Address, log } from "@graphprotocol/graph-ts";
+import { log } from "@graphprotocol/graph-ts";
 import { Transfer } from "../generated/WPowerPerp/WPowerPerp";
 import { TOKEN_DECIMALS_18 } from "./constants";
 import { convertTokenToDecimal, sqthChange } from "./util";
@@ -6,16 +6,15 @@ import { convertTokenToDecimal, sqthChange } from "./util";
 export function handleTransfer(event: Transfer): void {
   let amount = convertTokenToDecimal(event.params.value, TOKEN_DECIMALS_18);
 
-  // If it's for minting/burning, ignore PnL calc
-  if (
-    event.transaction.from === Address.empty() ||
-    event.transaction.to === Address.empty()
-  ) {
-    return;
-  }
+  log.warning(
+    amount.toString() +
+      ", " +
+      event.params.from.toHex() +
+      ", " +
+      event.params.to.toHex(),
+    []
+  );
 
-  sqthChange(event.transaction.from.toHex(), amount.neg());
-  if (event.transaction.to) {
-    sqthChange(event.transaction.to.toHex(), amount);
-  }
+  sqthChange(event.params.to.toHex(), amount);
+  sqthChange(event.params.from.toHex(), amount.neg());
 }
