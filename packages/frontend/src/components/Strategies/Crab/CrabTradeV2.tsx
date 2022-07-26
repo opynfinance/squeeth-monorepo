3a488411b8d645b863f9811578f7adb5f0724f3d
Darya Kaviani <daryakaviani@Daryas-MacBook-Pro.local>
2022-07-26 12:58:50 -0700

    fix withdraw

--- packages/frontend/src/components/Strategies/Crab/CrabTradeV2.tsx
+++ packages/frontend/src/components/Strategies/Crab/CrabTradeV2.tsx
@@ -157,7 +157,7 @@ const CrabTradeV2: React.FC<CrabTradeV2Type> = ({ maxCap, depositedAmount }) => 
     const threshold = BigNumber.max(dailyHistoricalImpliedVol.times(new BigNumber(1).plus(impliedVolDiff)),
                                     dailyHistoricalImpliedVol.plus(impliedVolDiffLowVol))
     depositFundingWarning = (depositOption === 0 && new BigNumber(impliedVol).lt(threshold)) ? true : false
-    withdrawFundingWarning = true
+    withdrawFundingWarning = (depositOption != 0 && new BigNumber(impliedVol).gt(threshold)) ? true : false
     return { depositFundingWarning, withdrawFundingWarning }
     }, [
         dailyHistoricalFunding.funding,
