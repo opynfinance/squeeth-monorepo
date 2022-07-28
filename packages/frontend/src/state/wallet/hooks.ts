c67dd9f9de033bd5ecd7cab95a8dc3737a080f1a
KMKoushik <koushikmohan1996@gmail.com>
2022-07-29 09:26:46 +1000

    Fix tx confirmation showing early

--- packages/frontend/src/state/wallet/hooks.ts
+++ packages/frontend/src/state/wallet/hooks.ts
@@ -72,13 +72,15 @@ export const useHandleTransaction = () => {
 
         emitter.on('all', (transaction) => {
           if (networkId === Networks.LOCAL) return
-          setTransactionData(transaction)
 
           if (transaction.status === 'confirmed') {
-            if (onTxConfirmed) {
-              onTxConfirmed()
-            }
-            refetch()
+            tx.then(() => {
+              setTransactionData(transaction)
+              if (onTxConfirmed) {
+                onTxConfirmed()
+              }
+              refetch()
+            })
           }
 
           return {
@@ -218,12 +220,12 @@ export function initOnboard(subscriptions: any, networkId: Networks) {
     networkId === Networks.LOCAL
       ? 'http://127.0.0.1:8545/'
       : networkId === Networks.ARBITRUM_RINKEBY
-      ? 'https://rinkeby.arbitrum.io/rpc'
-      : useAlchemy === 'true'
-      ? `https://eth-${network}.alchemyapi.io/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`
-      : usePokt === 'true'
-      ? `https://eth-${network}.gateway.pokt.network/v1/lb/${process.env.NEXT_PUBLIC_POKT_ID}`
-      : `https://${network}.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_API_KEY}`
+        ? 'https://rinkeby.arbitrum.io/rpc'
+        : useAlchemy === 'true'
+          ? `https://eth-${network}.alchemyapi.io/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`
+          : usePokt === 'true'
+            ? `https://eth-${network}.gateway.pokt.network/v1/lb/${process.env.NEXT_PUBLIC_POKT_ID}`
+            : `https://${network}.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_API_KEY}`
   return Onboard({
     dappId: process.env.NEXT_PUBLIC_BLOCKNATIVE_DAPP_ID,
     networkId: networkId,
