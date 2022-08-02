eb128cc58ab07caa6a7a16f6da40dc6fe8ae534c
KMKoushik <koushikmohan1996@gmail.com>
2022-08-02 11:02:12 +1000

    Remove if check

--- packages/frontend/src/state/squeethPool/hooks.ts
+++ packages/frontend/src/state/squeethPool/hooks.ts
@@ -94,19 +94,17 @@ export const useUpdateSqueethPoolData = () => {
         isWethToken0 ? 'oSqueeth' : 'Wrapped Ether',
       )
 
-      if (ticks?.length) {
-        const pool = new Pool(
-          TokenA,
-          TokenB,
-          Number(fee),
-          state.sqrtPriceX96.toString(),
-          state.liquidity.toString(),
-          Number(state.tick),
-          ticks || [],
-        )
-  
-        setPool(pool)
-      }
+      const pool = new Pool(
+        TokenA,
+        TokenB,
+        Number(fee),
+        state.sqrtPriceX96.toString(),
+        state.liquidity.toString(),
+        Number(state.tick),
+        ticks || [],
+      )
+
+      setPool(pool)
       setWethToken(isWethToken0 ? TokenA : TokenB)
       setSqueethToken(isWethToken0 ? TokenB : TokenA)
     })()
