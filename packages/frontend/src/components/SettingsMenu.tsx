ead35901d9796504ffd22c09b6b59e222c6e4a0c
adam5909099 <adam5909099@gmail.com>
2022-08-24 13:31:15 -0400

    Update link to be more local

--- packages/frontend/src/components/SettingsMenu.tsx
+++ packages/frontend/src/components/SettingsMenu.tsx
@@ -187,7 +187,15 @@ const SettingMenu = () => {
               <ListItemText>
                 The app securely reviews your wallet address using the Chainalysis, Inc. API for risk and compliance
                 reasons.{' '}
-                <MatLink href="https://squeeth.opyn.co/terms-of-service" target="_blank">
+                <Link href="/terms-of-service">
+                  <a
+                    target="_blank"
+                    style={{ textDecoration: isOver && currentlyOver === 'tos' ? 'underline' : 'none' }}
+                  >
+                    Squeeth User Terms of Service
+                  </a>
+                </Link>
+                <MatLink href={`${location.host}/terms-of-service`} target="_blank">
                   Learn More
                 </MatLink>
               </ListItemText>
