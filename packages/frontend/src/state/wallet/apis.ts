9b4b282cfab5d42d173e69aa2f2af737991f9dc4
adam5909099 <adam5909099@gmail.com>
2022-08-28 11:00:22 -0400

    Fix wallet connect on refresh

--- packages/frontend/src/state/wallet/apis.ts
+++ packages/frontend/src/state/wallet/apis.ts
@@ -4,6 +4,8 @@ import * as Sentry from '@sentry/nextjs'
 export const checkIsValidAddress = async (address: string) => {
   const { data } = await axios.get<{ valid: boolean }>(`/api/isValidAddress?address=${address}`)
 
+  console.log('valid address check')
+
   if (!data.valid) {
     Sentry.captureMessage(`Risk address ${address} is blocked.`)
   }
