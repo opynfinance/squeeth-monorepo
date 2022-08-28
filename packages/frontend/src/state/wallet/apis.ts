f1d766e65a802e6a0ecb0ea4ca619edf133422c6
adam5909099 <adam5909099@gmail.com>
2022-08-28 11:07:03 -0400

    Remove console log

--- packages/frontend/src/state/wallet/apis.ts
+++ packages/frontend/src/state/wallet/apis.ts
@@ -4,8 +4,6 @@ import * as Sentry from '@sentry/nextjs'
 export const checkIsValidAddress = async (address: string) => {
   const { data } = await axios.get<{ valid: boolean }>(`/api/isValidAddress?address=${address}`)
 
-  console.log('valid address check')
-
   if (!data.valid) {
     Sentry.captureMessage(`Risk address ${address} is blocked.`)
   }
