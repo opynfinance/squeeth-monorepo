691fe6d0a42e40e22902a89545f4d58c948db041
Haythem Sellami <sellamihaythem96@gmail.com>
2022-08-29 13:30:17 +0200

    add goerli addresses to subgraph

--- packages/subgraph/src/util.ts
+++ packages/subgraph/src/util.ts
@@ -5,7 +5,17 @@ import {
   ROPSTEN_SHORT_HELPER_ADDR,
   LOCALHOST_SHORT_HELPER_ADDR,
   RA_SHORT_HELPER_ADDR,
+  GOERLI_SHORT_HELPER_ADDR,
   BIGINT_ZERO,
+  MAINNET_CRAB_V2_ADDR,
+  ROPSTEN_CRAB_V2_ADDR,
+  MAINNET_CRAB_MIGRATION_ADDR,
+  ROPSTEN_CRAB_MIGRATION_ADDR,
+  MAINNET_CRAB_V1_ADDR,
+  ROPSTEN_CRAB_V1_ADDR,
+  GOERLI_CRAB_V2_ADDR,
+  GOERLI_CRAB_MIGRATION_ADDR,
+  GOERLI_CRAB_V1_ADDR
 } from "./constants";
 
 export function getShortHelperAddr(networkName: string): Address {
@@ -16,7 +26,42 @@ export function getShortHelperAddr(networkName: string): Address {
     addr = LOCALHOST_SHORT_HELPER_ADDR;
   } else if (networkName == "rinkebyArbitrum") {
     addr = RA_SHORT_HELPER_ADDR;
+  } else if (networkName == "goerli") {
+    addr = GOERLI_SHORT_HELPER_ADDR;
+  } 
+  return addr;
+}
+
+export function getCrabV2Addr(networkName: string): Address {
+  let addr = MAINNET_CRAB_V2_ADDR;
+  if (networkName == "ropsten") {
+    addr = ROPSTEN_CRAB_V2_ADDR;
+  } else if (networkName == "goerli") {
+    addr = GOERLI_CRAB_V2_ADDR
   }
+
+  return addr;
+}
+
+export function getCrabMigrationAddr(networkName: string): Address {
+  let addr = MAINNET_CRAB_MIGRATION_ADDR;
+  if (networkName == "ropsten") {
+    addr = ROPSTEN_CRAB_MIGRATION_ADDR;
+  } else if (networkName == "goerli") {
+    addr = GOERLI_CRAB_MIGRATION_ADDR;
+  }
+
+  return addr;
+}
+
+export function getCrabV1Addr(networkName: string): Address {
+  let addr = MAINNET_CRAB_V1_ADDR;
+  if (networkName == "ropsten") {
+    addr = ROPSTEN_CRAB_V1_ADDR
+  } else if (networkName == "goerli") {
+    addr = GOERLI_CRAB_V1_ADDR
+  }
+
   return addr;
 }
 
