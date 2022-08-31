7a73ce7f846a1c0d6501fa6121e87dc73c64f821
Haythem Sellami <sellamihaythem96@gmail.com>
2022-08-31 10:44:53 +0200

    updatE

--- packages/bull-vault/src/SqueethUtil.sol
+++ packages/bull-vault/src/SqueethUtil.sol
@@ -1,5 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity =0.8.13;
+pragma solidity =0.7.6;
+
+import {IController} from "squeeth-monorepo/interfaces/IController.sol";
+import {IWPowerPerp} from "squeeth-monorepo/interfaces/IWPowerPerp.sol";
+import {IOracle} from "squeeth-monorepo/interfaces/IOracle.sol";
+import {IWETH9} from "squeeth-monorepo/interfaces/IWETH9.sol";
+// import {IUniswapV3Pool} from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
 
 contract Contract {
     
