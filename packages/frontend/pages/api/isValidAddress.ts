36636055409f0e1155df02b79747596f7244b55b
Demola <66267188+DemolaJames@users.noreply.github.com>
2022-09-02 17:34:53 +0100

    add sentry logging to aml check

--- packages/frontend/pages/api/isValidAddress.ts
+++ packages/frontend/pages/api/isValidAddress.ts
@@ -1,10 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios'
+import * as Sentry from '@sentry/nextjs'
 
 const handleRequest = async (req: NextApiRequest, res: NextApiResponse) => {
   const { address } = req.query
 
   try {
+
     const { data } = await axios.post(
       `https://api.chainalysis.com/api/kyt/v1/users/${address}/withdrawaladdresses`,
       [
@@ -18,8 +20,16 @@ const handleRequest = async (req: NextApiRequest, res: NextApiResponse) => {
     )
 
     res.status(200).json({ valid: (data?.[0]?.rating ?? 'highRisk') !== 'highRisk' })
-  } catch (e) {
-    res.status(200).json({ valid: false })
+
+  } catch (error) {
+    // catches all reponses not 2XX from source
+    if (error instanceof Error) {
+      Sentry.captureMessage(`AML Check: Error occured when checking for address:${address}. Error: ${error.message} `)
+    } else {
+      Sentry.captureMessage(`AML Check: Error occured when checking for address:${address}. Error: ${JSON.stringify(error)}`)
+    }
+
+    res.status(200).json({ valid: true })
   }
 }
 
