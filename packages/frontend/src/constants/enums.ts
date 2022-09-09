9ac65b78fdb166d5591bf3e4dccef968e131abc2
Haythem Sellami <sellamihaythem96@gmail.com>
2022-09-09 12:27:55 +0200

    Add UniswapIFrameOpen for Goerli

--- packages/frontend/src/constants/enums.ts
+++ packages/frontend/src/constants/enums.ts
@@ -93,7 +93,7 @@ export enum Links {
 export const UniswapIFrameOpen = {
   1: `https://app.uniswap.org/#/add/ETH/${OSQUEETH[1]}/3000`,
   3: `https://squeeth-uniswap.netlify.app/#/add/ETH/${OSQUEETH[3]}/3000`,
-  5: '',
+  5: `https://squeeth-uniswap.netlify.app/#/add/ETH/${OSQUEETH[5]}/3000`,
   31337: 'https://app.uniswap.org/#/add/ETH/0xf1B99e3E573A1a9C5E6B2Ce818b617F0E664E86B/3000', // Should be replaced with arbitrum subgraph
   421611: 'https://app.uniswap.org/#/add/ETH/0xf1B99e3E573A1a9C5E6B2Ce818b617F0E664E86B/3000', // Should be replaced with arbitrum subgraph
 }
