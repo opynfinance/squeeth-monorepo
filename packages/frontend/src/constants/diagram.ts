69993a1af9ae1e069353c027104dda3b0268416d
Demola <66267188+DemolaJames@users.noreply.github.com>
2022-09-14 15:40:24 +0100

    Make axes clearer

--- packages/frontend/src/constants/diagram.ts
+++ packages/frontend/src/constants/diagram.ts
@@ -41,3 +41,50 @@ export const graphOptions = {
     fontFamily: 'Open Sans',
   },
 }
+
+
+export const crabV2graphOptions = {
+  alignLabels: true,
+  priceScale: {
+    scaleMargins: {
+      top: 0.2,
+      bottom: 0.15,
+    },
+    mode: 0,
+    borderVisible: true,
+    borderColor: '#878686',
+  },
+  timeScale: {
+    rightOffset: 5,
+    barSpacing: 3,
+    fixLeftEdge: false, // false: can move before starting point
+    lockVisibleTimeRangeOnResize: true,
+    rightBarStaysOnScroll: true,
+    borderVisible: true,
+    borderColor: '#878686',
+    visible: true,
+    timeVisible: true,
+    secondsVisible: false,
+  },
+  localization: {
+    priceFormatter: function (price: any) {
+      return '$' + Number(price.toFixed(2)).toLocaleString()
+    },
+    locale: 'en-US',
+  },
+  grid: {
+    vertLines: {
+      color: 'rgba(255, 255, 255, 0.12)',
+      visible: false,
+    },
+    horzLines: {
+      color: 'rgba(255, 255, 255, 0.12)',
+      visible: false,
+    },
+  },
+  layout: {
+    fontSize: 12,
+    backgroundColor: '#181B1C00',
+    fontFamily: 'Open Sans',
+  },
+}
