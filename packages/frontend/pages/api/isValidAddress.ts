a6e125e1677009cc455e413614116eff0e636ff6
Demola <66267188+DemolaJames@users.noreply.github.com>
2022-09-14 18:19:48 +0100

    AML check update (#651)
    * add sentry logging to aml check
    
    * add tests to aml check
    
    * handle cases for response body change
    
    * Update tests to mock values
    
    * final fix for test for when reponse value is null
    
    * add indentation for better readability

--- packages/frontend/pages/api/isValidAddress.ts
+++ packages/frontend/pages/api/isValidAddress.ts
@@ -1,8 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios'
+import * as Sentry from '@sentry/nextjs'
 
 const handleRequest = async (req: NextApiRequest, res: NextApiResponse) => {
   const { address } = req.query
+  let isValidAddress = true
 
   try {
     const { data } = await axios.post(
@@ -14,12 +16,23 @@ const handleRequest = async (req: NextApiRequest, res: NextApiResponse) => {
           address,
         },
       ],
-      { headers: { Token: process.env.NEXT_PUBLIC_AML_API_KEY ?? '' } },
+      { headers: { Token: process.env.AML_API_KEY ?? '' } },
     )
+   
+    if (data && data?.[0]?.rating)
+      isValidAddress = (data?.[0].rating === 'highRisk') ? false : true 
 
-    res.status(200).json({ valid: (data?.[0]?.rating ?? 'highRisk') !== 'highRisk' })
-  } catch (e) {
-    res.status(200).json({ valid: false })
+    res.status(200).json({ valid: isValidAddress, madeThirdPartyConnection: true })
+
+  } catch (error) {
+    // catches all reponses not 2XX from source
+    if (error instanceof Error) {
+      Sentry.captureMessage(`AML Check: Error occured when checking for address:${address}. Error: ${error.message} `)
+    } else {
+      Sentry.captureMessage(`AML Check: Error occured when checking for address:${address}. Error: ${JSON.stringify(error)}`)
+    }
+
+    res.status(200).json({ valid: true, madeThirdPartyConnection: false })
   }
 }
 
