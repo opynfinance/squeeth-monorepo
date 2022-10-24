cf0474064f2380b3be6924f5c26f40438b1b91ff
Haythem Sellami <haythem96@users.noreply.github.com>
2022-10-24 15:15:07 +0200

    feat: Withdraw (#678)
    * init contracts
    
    * init bull contracts
    
    * example of testing
    
    * Bull Variable Names (#674)
    
    * Adding variable names
    
    * Crab ETH total
    
    Co-authored-by: Darya Kaviani <daryakaviani@daryas-mbp.lan>
    
    * lint
    
    * crabby deposit
    
    * add .env.example
    
    * add fuzz testing
    
    * add fuzz testing
    
    * fix
    
    * add CI for build and fuzzing
    
    * add CI for build and fuzzing
    
    * add env var to github actions
    
    * add env var to github actions
    
    * add env var to github actions
    
    * add README
    
    * withdraw
    
    * fix build
    
    * fix build
    
    * fix build
    
    * withdraw
    
    * clean
    
    * add github action for forge coverage
    
    Co-authored-by: Darya Kaviani <daryakavi@gmail.com>
    Co-authored-by: Darya Kaviani <daryakaviani@daryas-mbp.lan>

--- packages/bull-vault/src/BullStrategy.sol
+++ packages/bull-vault/src/BullStrategy.sol
@@ -12,6 +12,7 @@ import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
 import {LeverageBull} from "./LeverageBull.sol";
 import {UniBull} from "./UniBull.sol";
 // lib
+import {Address} from "openzeppelin/utils/Address.sol";
 import {StrategyMath} from "squeeth-monorepo/strategy/base/StrategyMath.sol"; // StrategyMath licensed under AGPL-3.0-only
 import {VaultLib} from "squeeth-monorepo/libs/VaultLib.sol";
 
@@ -22,6 +23,7 @@ import {VaultLib} from "squeeth-monorepo/libs/VaultLib.sol";
  */
 contract BullStrategy is ERC20, LeverageBull, UniBull {
     using StrategyMath for uint256;
+    using Address for address payable;
 
     uint256 private constant ONE = 1e18;
     uint32 private constant TWAP = 420;
@@ -55,6 +57,8 @@ contract BullStrategy is ERC20, LeverageBull, UniBull {
     /// @dev target CR for our ETH collateral
     uint256 public crTarget;
 
+    event Withdraw(address from, uint256 bullAmount, uint256 wPowerPerpToRedeem, uint256 ethToWithdrawFromCrab);
+
     /**
      * @notice constructor for BullStrategy
      * @dev this will open a vault in the power token contract and store the vault ID
@@ -78,6 +82,10 @@ contract BullStrategy is ERC20, LeverageBull, UniBull {
         wPowerPerp = IController(_powerTokenController).wPowerPerp();
         ethWSqueethPool = IController(_powerTokenController).wPowerPerpPool();
         ethUSDCPool = IController(_powerTokenController).ethQuoteCurrencyPool();
+    }
+
+    receive() external payable {
+        require(msg.sender == weth || msg.sender == address(crab));
     }
 
     /**
@@ -111,7 +119,29 @@ contract BullStrategy is ERC20, LeverageBull, UniBull {
         IERC20(usdc).transfer(msg.sender, usdcBorrowed);
     }
 
-    function withdraw() external {}
+    /**
+     * @notice withdraw ETH from crab and euler by providing wPowerPerp, bull token and USDC to repay debt
+     * @param _bullAmount amount of bull token to redeem
+     */
+    function withdraw(uint256 _bullAmount) external {
+        uint256 share = _bullAmount.wdiv(totalSupply());
+        uint256 crabToRedeem = share.wmul(IERC20(crab).balanceOf(address(this)));
+        uint256 crabTotalSupply = IERC20(crab).totalSupply();
+        (uint256 ethInCrab, uint256 squeethInCrab) = _getCrabVaultDetails();
+        uint256 wPowerPerpToRedeem = crabToRedeem.wmul(squeethInCrab).wdiv(crabTotalSupply);
+        uint256 ethToWithdrawFromCrab = crabToRedeem.wmul(ethInCrab).wdiv(crabTotalSupply);
+        
+        IERC20(wPowerPerp).transferFrom(msg.sender, address(this), wPowerPerpToRedeem);
+        IERC20(wPowerPerp).approve(crab, wPowerPerpToRedeem);
+        _burn(msg.sender, _bullAmount);
+        ICrabStrategyV2(crab).withdraw(crabToRedeem);
+        
+        _repayAndWithdrawFromLeverage(share);
+
+        payable(msg.sender).sendValue(address(this).balance);
+
+        emit Withdraw(msg.sender, _bullAmount, wPowerPerpToRedeem, ethToWithdrawFromCrab);
+    }
 
     function _uniFlashSwap(
         address, /*_caller*/
