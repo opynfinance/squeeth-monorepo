a3fff2578299ca270af64d468c5dff528b087a61
Anand Narayan <i@codeanand.com>
2022-10-24 13:50:54 -0700

    deposit for crab netting

--- packages/crab-netting/src/CrabNetting.sol
+++ packages/crab-netting/src/CrabNetting.sol
@@ -5,20 +5,30 @@ import "forge-std/console.sol";
 
 contract CrabNetting {
     address usdc;
+<<<<<<< HEAD
     address crab;
 
     mapping(address => uint256) usd_balance;
     mapping(address => uint256) crab_balance;
+=======
+    mapping(address => uint256) usd_balance;
+>>>>>>> 3342065f (deposit for crab netting)
     struct Receipt {
         address depositor;
         uint256 amount;
     }
     Receipt[] deposits;
+<<<<<<< HEAD
     Receipt[] crab_deposits;
 
     constructor(address _usdc, address _crab) {
         usdc = _usdc;
         crab = _crab;
+=======
+
+    constructor(address _usdc) {
+        usdc = _usdc;
+>>>>>>> 3342065f (deposit for crab netting)
     }
 
     function depositUSDC(uint256 amount) public {
@@ -33,6 +43,7 @@ contract CrabNetting {
         IERC20(usdc).transfer(msg.sender, amount);
     }
 
+<<<<<<< HEAD
     function depositCrab(uint256 amount) public {
         IERC20(crab).transferFrom(msg.sender, address(this), amount);
         crab_balance[msg.sender] = crab_balance[msg.sender] + amount;
@@ -52,4 +63,9 @@ contract CrabNetting {
     function crabBalanceOf(address account) public view returns (uint256) {
         return crab_balance[account];
     }
+=======
+    function balanceOf(address account) public view returns (uint256) {
+        return usd_balance[account];
+    }
+>>>>>>> 3342065f (deposit for crab netting)
 }
--- packages/crab-netting/test/Deposit.t.sol
+++ packages/crab-netting/test/Deposit.t.sol
@@ -6,15 +6,23 @@ import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
 
 import {CrabNetting} from "../src/CrabNetting.sol";
 
+<<<<<<< HEAD
 contract FixedERC20 is ERC20 {
+=======
+contract USDC is ERC20 {
+>>>>>>> 3342065f (deposit for crab netting)
     constructor(uint256 initialSupply) ERC20("USDC", "USDC") {
         _mint(msg.sender, initialSupply);
     }
 }
 
 contract DepositTest is Test {
+<<<<<<< HEAD
     FixedERC20 usdc;
     FixedERC20 crab;
+=======
+    USDC usdc;
+>>>>>>> 3342065f (deposit for crab netting)
     CrabNetting netting;
 
     uint256 internal ownerPrivateKey;
@@ -25,9 +33,14 @@ contract DepositTest is Test {
     address internal withdrawer;
 
     function setUp() public {
+<<<<<<< HEAD
         usdc = new FixedERC20(10000 * 1e18);
         crab = new FixedERC20(10000 * 1e18);
         netting = new CrabNetting(address(usdc), address(crab));
+=======
+        usdc = new USDC(10000 * 1e18);
+        netting = new CrabNetting(address(usdc));
+>>>>>>> 3342065f (deposit for crab netting)
 
         ownerPrivateKey = 0xA11CE;
         owner = vm.addr(ownerPrivateKey);
@@ -39,7 +52,10 @@ contract DepositTest is Test {
         withdrawer = vm.addr(withdrawerPk);
 
         usdc.transfer(depositor, 2 * 1e18);
+<<<<<<< HEAD
         crab.transfer(withdrawer, 2 * 1e18);
+=======
+>>>>>>> 3342065f (deposit for crab netting)
     }
 
     function testDepositAndWithdraw() public {
@@ -51,6 +67,7 @@ contract DepositTest is Test {
         netting.withdrawUSDC(1 * 1e18);
         assertEq(netting.balanceOf(depositor), 1e18);
     }
+<<<<<<< HEAD
 
     function testCrabDepositAndWithdraw() public {
         vm.startPrank(withdrawer);
@@ -61,4 +78,6 @@ contract DepositTest is Test {
         netting.withdrawCrab(1 * 1e18);
         assertEq(netting.crabBalanceOf(withdrawer), 1e18);
     }
+=======
+>>>>>>> 3342065f (deposit for crab netting)
 }
