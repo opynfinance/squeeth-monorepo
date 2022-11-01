453395b73c11d6c4d562d58e24bb949b21f77d8c
Anand Narayan <i@codeanand.com>
2022-11-01 12:07:34 -0700

    withdraw auction test refactored

--- packages/crab-netting/test/WithdrawAuction.t.sol
+++ packages/crab-netting/test/WithdrawAuction.t.sol
@@ -3,58 +3,23 @@ pragma solidity ^0.8.17;
 
 import "forge-std/Test.sol";
 
-import {IWETH} from "../src/interfaces/IWETH.sol";
-
-import {CrabNetting, Order} from "../src/CrabNetting.sol";
-import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
-import {IQuoter} from "@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol";
+import {Order} from "../src/CrabNetting.sol";
 import {ICrabStrategyV2} from "../src/interfaces/ICrabStrategyV2.sol";
-import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 
 import {UniswapQuote} from "./utils/UniswapQuote.sol";
+import {BaseForkSetup} from "./BaseForkSetup.t.sol";
+
+import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
+import {IWETH} from "../src/interfaces/IWETH.sol";
 
 struct TimeBalances {
     uint256 start;
     uint256 end;
 }
 
-contract TestWithdrawAuction is Test {
-    ISwapRouter public immutable swapRouter =
-        ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
-    uint256 internal withdrawerPk;
-    address internal withdrawer;
-
-    uint256 internal mm1Pk;
-    address internal mm1;
-
-    uint256 internal mm2Pk;
-    address internal mm2;
-
-    ICrabStrategyV2 crab;
-    IWETH weth;
-    ERC20 usdc;
-    ERC20 sqth;
-    CrabNetting netting;
-    Order[] orders;
-    IQuoter quoter;
-
-    function setUp() public {
-        string memory FORK_URL = vm.envString("FORK_URL");
-        vm.createSelectFork(FORK_URL, 15819213); // price of eth in this block is 1,343.83
-        quoter = IQuoter(0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6);
-        crab = ICrabStrategyV2(0x3B960E47784150F5a63777201ee2B15253D713e8);
-        weth = IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-        vm.label(address(weth), "weth");
-        usdc = ERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
-        sqth = ERC20(0xf1B99e3E573A1a9C5E6B2Ce818b617F0E664E86B);
-
-        withdrawerPk = 0xA11CE;
-        withdrawer = vm.addr(withdrawerPk);
-        mm1Pk = 0xA11CC;
-        mm1 = vm.addr(mm1Pk);
-        vm.label(mm1, "mm1");
-        mm2Pk = 0xA11CA;
-        mm2 = vm.addr(mm2Pk);
+contract TestWithdrawAuction is BaseForkSetup {
+    function setUp() public override {
+        BaseForkSetup.setUp();
 
         // this is a crab whale, get some crab token from
         vm.prank(0x06CECFbac34101aE41C88EbC2450f8602b3d164b);
@@ -66,14 +31,6 @@ contract TestWithdrawAuction is Test {
         sqth.transfer(mm2, 1000e18);
         vm.stopPrank();
 
-        netting = new CrabNetting(
-            address(usdc),
-            address(crab),
-            address(weth),
-            address(sqth),
-            address(swapRouter)
-        );
-
         // deposit crab for withdrawing
         vm.startPrank(withdrawer);
         crab.approve(address(netting), 13 * 1e18);
@@ -81,10 +38,10 @@ contract TestWithdrawAuction is Test {
         netting.queueCrabForWithdrawal(3 * 1e18);
         netting.queueCrabForWithdrawal(6 * 1e18);
         vm.stopPrank();
+        // 11 crabs queued for withdrawal
     }
 
     function testWithdrawAuction() public {
-        assert(true);
         // find the sqth to buy to make the trade
         uint256 crabToWithdraw = 10e18;
         uint256 sqthToBuy = crab.getWsqueethFromCrabAmount(crabToWithdraw);
