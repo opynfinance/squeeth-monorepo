6c3583201cc15ba9ba4232e98c8f870be9054587
Haythem Sellami <sellamihaythem96@gmail.com>
2022-11-04 02:58:23 +0000

    finally we have local setup

--- packages/bull-vault/test/unit-test/mock/EulerDtokenMock.t.sol
+++ packages/bull-vault/test/unit-test/mock/EulerDtokenMock.t.sol
@@ -3,13 +3,17 @@ pragma solidity =0.7.6;
 
 import "forge-std/Test.sol";
 import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
+import {EulerMock} from "./EulerMock.t.sol";
 
 contract EulerDtokenMock is ERC20, Test {
 
     // asset to borrow/repay
     address underlying;
 
-    constructor(address _underlying, string memory _name, string memory _symbol, uint8 _decimals) ERC20(_name, _symbol) {
+    EulerMock internal euler;
+
+    constructor(address _euler, address _underlying, string memory _name, string memory _symbol, uint8 _decimals) ERC20(_name, _symbol) {
+        euler = EulerMock(_euler);
         underlying = _underlying;
         _setupDecimals(_decimals);
     }
@@ -17,9 +21,11 @@ contract EulerDtokenMock is ERC20, Test {
     function borrow(uint256 subAccountId, uint256 amount) external {
         _mint(msg.sender, amount);
         ERC20(underlying).transfer(msg.sender, amount);
+        euler.executeTransfer(underlying, address(euler), msg.sender, amount);
     }
 
     function repay(uint256 subAccountId, uint256 amount) external {
+        euler.executeTransfer(underlying, msg.sender, address(euler), amount);
         ERC20(underlying).transferFrom(msg.sender, address(this), amount);
         _burn(msg.sender, amount);
     }
--- packages/bull-vault/test/unit-test/mock/EulerEtokenMock.t.sol
+++ packages/bull-vault/test/unit-test/mock/EulerEtokenMock.t.sol
@@ -3,6 +3,7 @@ pragma solidity =0.7.6;
 
 import "forge-std/Test.sol";
 import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
+import {EulerMock} from "./EulerMock.t.sol";
 import {StrategyMath} from "squeeth-monorepo/strategy/base/StrategyMath.sol"; // StrategyMath licensed under AGPL-3.0-only
 
 contract EulerEtokenMock is ERC20, Test {
@@ -10,22 +11,25 @@ contract EulerEtokenMock is ERC20, Test {
     // asset to deposit as collateral
     address public underlying;
 
+    EulerMock internal euler;
+
     mapping(address => uint256) internal underlyingBalance;
 
-    constructor(address _underlying, string memory _name, string memory _symbol, uint8 _decimals) ERC20(_name, _symbol) {
+    constructor(address _euler, address _underlying, string memory _name, string memory _symbol, uint8 _decimals) ERC20(_name, _symbol) {
+        euler = EulerMock(_euler);
         underlying = _underlying;
         _setupDecimals(_decimals);
     }
 
     function deposit(uint256 subAccountId, uint256 amount) external {
-        ERC20(underlying).transferFrom(msg.sender, address(this), amount);
+        euler.executeTransfer(underlying, msg.sender, address(euler), amount);
         underlyingBalance[msg.sender] = underlyingBalance[msg.sender].add(amount);
         _mint(msg.sender, amount);
     }
     function withdraw(uint256 subAccountId, uint256 amount) external {
         underlyingBalance[msg.sender] = underlyingBalance[msg.sender].sub(amount);
         _burn(msg.sender, amount);
-        ERC20(underlying).transfer(msg.sender, amount);
+        euler.executeTransfer(underlying, address(euler), msg.sender, amount);
     }
     function balanceOfUnderlying(address account) external view returns (uint256) {
         return underlyingBalance[account];
