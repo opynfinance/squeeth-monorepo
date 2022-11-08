6acc46adaac29ea635d559f3a2b237015f1a6799
Haythem Sellami <sellamihaythem96@gmail.com>
2022-11-08 01:00:52 +0000

    test

--- packages/bull-vault/src/AuctionBull.sol
+++ packages/bull-vault/src/AuctionBull.sol
@@ -14,7 +14,12 @@ import { UniOracle } from "./UniOracle.sol";
 // lib
 import { StrategyMath } from "squeeth-monorepo/strategy/base/StrategyMath.sol"; // StrategyMath licensed under AGPL-3.0-only
 
-import { console } from "forge-std/console.sol";
+/**
+ * Error code
+ * AB0: caller is not auction manager
+ * AB1: Invalid delta after rebalance
+ * AB2: Invalid CR after rebalance
+ */
 
 /**
  * @notice AuctionBull contract
@@ -54,8 +59,8 @@ contract AuctionBull is UniFlash, Ownable {
 
     /// @dev enum to differentiate between Uniswap swap callback function source
     enum FLASH_SOURCE {
-        SELLING_USDC,
-        BUYING_USDC
+        LEVERAGE_REBALANCE_DECREASE_DEBT,
+        LEVERAGE_REBALANCE_INCREASE_DEBT
     }
 
     constructor(
@@ -79,7 +84,9 @@ contract AuctionBull is UniFlash, Ownable {
         eToken = _eToken;
         dToken = _dToken;
 
-        IERC20(IController(IBullStrategy(_bull).powerTokenController()).weth()).approve(_bull, type(uint256).max);
+        IERC20(IController(IBullStrategy(_bull).powerTokenController()).weth()).approve(
+            _bull, type(uint256).max
+        );
         IERC20(IController(IBullStrategy(_bull).powerTokenController()).quoteCurrency()).approve(
             _bull, type(uint256).max
         );
@@ -88,67 +95,66 @@ contract AuctionBull is UniFlash, Ownable {
     }
 
     /**
-     * @dev changes the leverage component composition by buying or selling eth
+     * @dev changes the leverage component composition by buying or selling USDC
      */
     function leverageRebalance(
-        bool _isBuyingUsdc,
+        bool _isSellingUsdc,
         uint256 _usdcAmount,
         uint256 _wethThresholdAmount,
         uint24 _poolFee
     ) external {
-        require(msg.sender == auctionManager);
+        require(msg.sender == auctionManager, "AB0");
 
-        if (_isBuyingUsdc) {
-            // swap ETH to USDC
-            _exactOutFlashSwap(
-                weth,
-                usdc,
-                _poolFee,
-                _usdcAmount,
-                _wethThresholdAmount,
-                uint8(FLASH_SOURCE.BUYING_USDC),
-                abi.encodePacked(_usdcAmount)
-            );
-        } else {
-            // swap USDC to ETH
+        if (_isSellingUsdc) {
+            // swap USDC to WETH
             _exactInFlashSwap(
                 usdc,
                 weth,
                 _poolFee,
                 _usdcAmount,
                 _wethThresholdAmount,
-                uint8(FLASH_SOURCE.SELLING_USDC),
+                uint8(FLASH_SOURCE.LEVERAGE_REBALANCE_INCREASE_DEBT),
                 ""
+            );
+        } else {
+            // swap WETH to USDC
+            _exactOutFlashSwap(
+                weth,
+                usdc,
+                _poolFee,
+                _usdcAmount,
+                _wethThresholdAmount,
+                uint8(FLASH_SOURCE.LEVERAGE_REBALANCE_DECREASE_DEBT),
+                abi.encodePacked(_usdcAmount)
             );
         }
 
         _isValidLeverageRebalance();
     }
 
-    function getCurrentDeltaAndCollatRatio()
-        external
-        view
-        returns (uint256, uint256)
-    {
+    function getCurrentDeltaAndCollatRatio() external view returns (uint256, uint256) {
         return _getCurrentDeltaAndCollatRatio();
     }
 
-
     function _uniFlashSwap(UniFlashswapCallbackData memory _uniFlashSwapData) internal override {
-        if (FLASH_SOURCE(_uniFlashSwapData.callSource) == FLASH_SOURCE.SELLING_USDC) {
-            IBullStrategy(bullStrategy).depositAndBorrowFromLeverage(IERC20(weth).balanceOf(address(this)), _uniFlashSwapData.amountToPay);
-
-            IERC20(_uniFlashSwapData.tokenIn).transfer(
-                _uniFlashSwapData.pool, _uniFlashSwapData.amountToPay
+        if (
+            FLASH_SOURCE(_uniFlashSwapData.callSource)
+                == FLASH_SOURCE.LEVERAGE_REBALANCE_INCREASE_DEBT
+        ) {
+            IBullStrategy(bullStrategy).depositAndBorrowFromLeverage(
+                IERC20(weth).balanceOf(address(this)), _uniFlashSwapData.amountToPay
             );
 
             IERC20(usdc).transfer(_uniFlashSwapData.pool, _uniFlashSwapData.amountToPay);
-
-        } else if (FLASH_SOURCE(_uniFlashSwapData.callSource) == FLASH_SOURCE.BUYING_USDC) {
+        } else if (
+            FLASH_SOURCE(_uniFlashSwapData.callSource)
+                == FLASH_SOURCE.LEVERAGE_REBALANCE_DECREASE_DEBT
+        ) {
             uint256 usdcToRepay = abi.decode(_uniFlashSwapData.callData, (uint256));
             // Repay some USDC debt
-            IERC20(usdc).approve(address(bullStrategy), usdcToRepay);
-            IBullStrategy(bullStrategy).repayAndWithdrawFromLeverage(usdcToRepay, _uniFlashSwapData.amountToPay);
+            IBullStrategy(bullStrategy).repayAndWithdrawFromLeverage(
+                usdcToRepay, _uniFlashSwapData.amountToPay
+            );
 
             IERC20(weth).transfer(_uniFlashSwapData.pool, _uniFlashSwapData.amountToPay);
         }
@@ -156,18 +162,15 @@ contract AuctionBull is UniFlash, Ownable {
 
     function _isValidLeverageRebalance() internal view {
         (uint256 delta, uint256 cr) = _getCurrentDeltaAndCollatRatio();
-        require(delta <= DELTA_UPPER && delta >= DELTA_LOWER, "Invalid delta after rebalance");
-        require(cr <= CR_UPPER && cr >= CR_LOWER, "Invalid CR after rebalance");
+
+        require(delta <= DELTA_UPPER && delta >= DELTA_LOWER, "AB1");
+        require(cr <= CR_UPPER && cr >= CR_LOWER, "AB2");
     }
 
     /**
      * @notice get expected bull startegy delta and leverage collat ratio after leverage rebalance
      */
-    function _getCurrentDeltaAndCollatRatio()
-        internal
-        view
-        returns (uint256, uint256)
-    {
+    function _getCurrentDeltaAndCollatRatio() internal view returns (uint256, uint256) {
         (uint256 ethInCrab, uint256 squeethInCrab) =
             IBullStrategy(bullStrategy).getCrabVaultDetails();
         uint256 ethUsdPrice = UniOracle._getTwap(ethUSDCPool, weth, usdc, TWAP, false);
@@ -179,17 +182,11 @@ contract AuctionBull is UniFlash, Ownable {
         uint256 usdcDebt = IEulerDToken(dToken).balanceOf(address(bullStrategy));
         uint256 wethInCollateral = IEulerEToken(eToken).balanceOfUnderlying(address(bullStrategy));
 
-        console.log("here");
         uint256 delta = (wethInCollateral.wmul(ethUsdPrice)).wdiv(
             (IBullStrategy(bullStrategy).getCrabBalance().wmul(crabUsdPrice)).add(
                 wethInCollateral.wmul(ethUsdPrice)
             ).sub(usdcDebt.mul(1e12))
         );
-        console.log("here2");
-        console.log("delta", delta);
-        console.log("wethInCollateral", wethInCollateral);
-        console.log("ethUsdPrice", ethUsdPrice);
-        console.log("usdcDebt", usdcDebt);
 
         uint256 cr = wethInCollateral.wmul(ethUsdPrice).wdiv(usdcDebt.mul(1e12));
 
