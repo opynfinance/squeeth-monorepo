3c006e9b3f4f7975dac87f21b09edf5c86f49daa
Andrew Leone <5490766+aleone@users.noreply.github.com>
2022-11-09 11:23:12 -0800

    refactor shutdown logic to use external contract

--- packages/bull-vault/src/interface/IBullStrategy.sol
+++ packages/bull-vault/src/interface/IBullStrategy.sol
@@ -2,6 +2,8 @@
 
 pragma solidity =0.7.6;
 
+import { ILeverageBull } from "./ILeverageBull.sol";
+
 interface IBullStrategy {
     function deposit(uint256 _crabAmount) external payable;
     function withdraw(uint256 _bullAmount) external;
@@ -17,4 +19,5 @@ interface IBullStrategy {
     ) external view returns (uint256, uint256);
     function calcUsdcToRepay(uint256 _bullShare) external view returns (uint256);
     function getCrabBalance() external view returns (uint256);
+    function shutdownRepayAndWithdraw(uint256 wethToUniswap) external;
 }
