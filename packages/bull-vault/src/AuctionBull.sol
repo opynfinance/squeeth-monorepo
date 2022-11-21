35c9ccefa817f3136047c15c2ec712b2bccab932
alpinechicken <joecmail@gmail.com>
2022-11-21 11:38:15 -0800

    missing case

--- packages/bull-vault/src/AuctionBull.sol
+++ packages/bull-vault/src/AuctionBull.sol
@@ -2,6 +2,7 @@
 pragma solidity =0.7.6;
 
 pragma abicoder v2;
+import { console } from "forge-std/console.sol";
 
 import { console } from "forge-std/console.sol";
 
@@ -587,7 +588,7 @@ contract AuctionBull is UniFlash, Ownable, EIP712 {
     ) external {
         require(msg.sender == auctionManager, "AB0");
         require(_clearingPrice > 0, "AB5");
-
+        console.log('reached fullRebalance');
         _checkFullRebalanceClearingPrice(_clearingPrice, _isDepositingInCrab);
         _checkRebalanceLimitPrice(_wethLimitPrice);
 
@@ -803,6 +804,7 @@ contract AuctionBull is UniFlash, Ownable, EIP712 {
         uint256 ethNeededForCrab = totalEthNeededForCrab.sub(_params.wethBoughtFromAuction);
         // WETH collateral in Euler
         uint256 wethInCollateral = IEulerEToken(eToken).balanceOfUnderlying(address(bullStrategy));
+        console.log('reached _executeCrabDeposit with %s wethTargetInEuler and %s wethIncollatearl', _params.wethTargetInEuler, wethInCollateral);
         if (_params.wethTargetInEuler > wethInCollateral) {
             // crab deposit eth + collateral shortfall
             uint256 wethToGet =
