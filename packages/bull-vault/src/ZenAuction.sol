66e1c143fafcf2e02707362f35fa2ad6e9e2f537
Haythem Sellami <sellamihaythem96@gmail.com>
2022-11-22 23:49:46 +0000

    lint

--- packages/bull-vault/src/ZenAuction.sol
+++ packages/bull-vault/src/ZenAuction.sol
@@ -2,6 +2,7 @@
 pragma solidity =0.7.6;
 
 pragma abicoder v2;
+
 import { console } from "forge-std/console.sol";
 
 // interface
@@ -347,7 +348,7 @@ contract ZenAuction is UniFlash, Ownable, EIP712 {
     ) external {
         require(msg.sender == auctionManager, "AB0");
         require(_clearingPrice > 0, "AB5");
-        console.log('reached fullRebalance');
+        console.log("reached fullRebalance");
         _checkFullRebalanceClearingPrice(_clearingPrice, _isDepositingInCrab);
         _checkRebalanceLimitPrice(_wethLimitPrice);
 
@@ -564,7 +565,11 @@ contract ZenAuction is UniFlash, Ownable, EIP712 {
         uint256 ethNeededForCrab = totalEthNeededForCrab.sub(_params.wethBoughtFromAuction);
         // WETH collateral in Euler
         uint256 wethInCollateral = IEulerEToken(eToken).balanceOfUnderlying(address(bullStrategy));
-        console.log('reached _executeCrabDeposit with %s wethTargetInEuler and %s wethIncollatearl', _params.wethTargetInEuler, wethInCollateral);
+        console.log(
+            "reached _executeCrabDeposit with %s wethTargetInEuler and %s wethIncollatearl",
+            _params.wethTargetInEuler,
+            wethInCollateral
+        );
         if (_params.wethTargetInEuler > wethInCollateral) {
             // crab deposit eth + collateral shortfall
             uint256 wethToGet =
