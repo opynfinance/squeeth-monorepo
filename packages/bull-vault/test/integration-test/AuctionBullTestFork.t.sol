f3f3f63350b6b7eae5f259ccad368f6a99312772
alpinechicken <joecmail@gmail.com>
2022-11-21 23:08:16 -0800

    fix limit price for test

--- packages/bull-vault/test/integration-test/AuctionBullTestFork.t.sol
+++ packages/bull-vault/test/integration-test/AuctionBullTestFork.t.sol
@@ -12437,7 +12437,7 @@ contract AuctionBullTestFork is Test {
             crabAmount,
             squeethEthPrice,
             targetWethInLeverage,
-            ethUsdPrice.wmul(12e17),
+            ethUsdPrice.wmul(8e17),
             3000,
             isDepositingInCrab
         );
