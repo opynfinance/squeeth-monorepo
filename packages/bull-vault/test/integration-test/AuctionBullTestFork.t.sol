8ecb02e4457dcfbd105fdd1a9105ae064a833382
alpinechicken <joecmail@gmail.com>
2022-11-22 17:52:33 -0800

    withdraw weth borrow usdc deposit case

--- packages/bull-vault/test/integration-test/AuctionBullTestFork.t.sol
+++ packages/bull-vault/test/integration-test/AuctionBullTestFork.t.sol
@@ -7873,8 +7873,8 @@ contract AuctionBullTestFork is Test {
         currentWethInLeverage = IEulerEToken(eToken).balanceOfUnderlying(address(bullStrategy));  
         console.log('currentDebt', currentDebt);
         console.log('currentWethInLeverage', currentWethInLeverage);
-        uint256 sellUsdcBuyWethAmount = 30000000e6;
-        uint256 sellWethBuyWPowerPerpAmount = 1;
+        uint256 sellUsdcBuyWethAmount = 5000000e6;
+        uint256 sellWethBuyWPowerPerpAmount = 200e18;
         uint256 sellWethBuyUsdcAmount = 1;
         uint256 sellWPowerPerpAmountBuyWethAmount = 1;
         {
