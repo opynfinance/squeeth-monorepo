fb78c4dfcee4725a8a0e059eae570314a65c78dd
alpinechicken <joecmail@gmail.com>
2022-11-22 19:27:48 -0800

    skull and x bonez test

--- packages/bull-vault/src/AuctionBull.sol
+++ packages/bull-vault/src/AuctionBull.sol
@@ -647,6 +647,7 @@ contract AuctionBull is UniFlash, Ownable, EIP712 {
                     ethUsdcPoolFee: _ethUsdcPoolFee
                 })
             );
+            console.log('lev rebal done');
         }
 
         // check that rebalance does not breach collateral ratio or delta tolerance
@@ -915,7 +916,7 @@ contract AuctionBull is UniFlash, Ownable, EIP712 {
                 == FLASH_SOURCE.FULL_REBALANCE_REPAY_USDC_WITHDRAW_WETH
         ) {
             uint256 remainingWeth = abi.decode(_uniFlashSwapData.callData, (uint256));
-
+            console.log('mark 6');
             IBullStrategy(bullStrategy).auctionRepayAndWithdrawFromLeverage(
                 IERC20(usdc).balanceOf(address(this)),
                 _uniFlashSwapData.amountToPay.sub(remainingWeth)
