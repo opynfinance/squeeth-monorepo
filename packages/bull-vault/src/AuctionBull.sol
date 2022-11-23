b929c66bffa75801baeeaf439b9c6fafac1f3d89
Andrew Leone <5490766+aleone@users.noreply.github.com>
2022-11-23 13:12:53 -0800

    add case for withdraw crab, deposit leverage, repay debt'

--- packages/bull-vault/src/AuctionBull.sol
+++ packages/bull-vault/src/AuctionBull.sol
@@ -645,7 +645,7 @@ contract AuctionBull is UniFlash, Ownable, EIP712 {
                     ethUsdcPoolFee: _ethUsdcPoolFee
                 })
             );
-            console.log('lev rebal done');
+            console.log("lev rebal done");
         }
 
         // check that rebalance does not breach collateral ratio or delta tolerance
