153ea5c8a61bcbee8bb7f145d5e77f610dfeeea3
Demola <66267188+DemolaJames@users.noreply.github.com>
2022-11-27 10:44:01 +0100

    fix deposited amount value not updating after render

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -102,8 +102,7 @@ const Init = () => {
 
     checkIsValidAddress(onboardAddress).then((valid) => {
       if (valid) {
-        setAddress('0x0879749D42d1fc1b5d83e9081EF56fd1F5316A03')
-       // setAddress(onboardAddress)
+        setAddress(onboardAddress)
         setUserId(onboardAddress)
         track(EVENT_NAME.WALLET_CONNECTED)
       } else {
--- packages/frontend/src/hooks/useUserCrabV2TxHistory.ts
+++ packages/frontend/src/hooks/useUserCrabV2TxHistory.ts
@@ -15,13 +15,13 @@ import {
 } from '../constants'
 import { squeethClient } from '@utils/apollo-client'
 import { CrabStrategyV2TxType } from '../types/index'
-import { useUsdAmount } from './useUsdAmount'
 import { networkIdAtom } from 'src/state/wallet/atoms'
 import { useAtomValue } from 'jotai'
 import useAppMemo from './useAppMemo'
 import BigNumber from 'bignumber.js'
 import { addressesAtom } from 'src/state/positions/atoms'
 import { getHistoricEthPrices } from './useETHPrice'
+import { useEffect, useState } from 'react'
 
 const getTxTitle = (type: string) => {
   if (type === CrabStrategyV2TxType.DEPOSIT) return 'Deposit'
@@ -34,7 +34,8 @@ const getTxTitle = (type: string) => {
 export const useUserCrabV2TxHistory = (user: string, isDescending?: boolean) => {
   const networkId = useAtomValue(networkIdAtom)
   const { usdc } = useAtomValue(addressesAtom)
- // const { getUsdAmt } = useUsdAmount()
+  const [ ethUsdPriceMap, setEthUsdPriceMap]  = useState()
+  const [ ethUsdPriceMapLoading, setEthUsdPriceMapLoading]  = useState(true)
   const { data, loading, startPolling, stopPolling } = useQuery<userCrabV2Txes, userCrabV2TxesVariables>(
     USER_CRAB_V2_TX_QUERY,
     {
@@ -48,25 +49,27 @@ export const useUserCrabV2TxHistory = (user: string, isDescending?: boolean) => 
   )
  
   //get all timestamps found in the user's history once
-  const ethPriceMap = useAppMemo(async() => {
+  useEffect(() => {
+
     let timestampsArr : any[] = []
-    let prices : any
       data?.crabUserTxes.map(tx => { 
         timestampsArr.push(tx.timestamp *1000)
       })
       if(timestampsArr.length > 0){
-        prices = await getHistoricEthPrices(timestampsArr)
+          getHistoricEthPrices(timestampsArr).then(result => {
+            setEthUsdPriceMap(result ?? undefined)
+            setEthUsdPriceMapLoading(false)
+          })
       }
-    return prices
     }, [data?.crabUserTxes,usdc]
   )
+  
 
   const uiData = useAppMemo(
     () =>
-    
       data?.crabUserTxes.map((tx) => {
         let ethAmount = toTokenAmount(tx.ethAmount, WETH_DECIMALS)
-        let ethUsdValue = BIG_ZERO // ethPriceMap ?  ethAmount.multipliedBy(ethPriceMap[Number(tx.timestamp) * 1000]) : 0// getUsdAmt(ethAmount, tx.timestamp)
+        let ethUsdValue = ethUsdPriceMap ? ethAmount.multipliedBy(ethUsdPriceMap[Number(tx.timestamp) * 1000 ] ) : 0
 
         if (tx.type === CrabStrategyV2TxType.DEPOSIT_V1) {
           const ethMigrated = new BigNumber(V2_MIGRATION_ETH_AMOUNT)
@@ -85,8 +88,8 @@ export const useUserCrabV2TxHistory = (user: string, isDescending?: boolean) => 
           tx.type === CrabStrategyV2TxType.FLASH_DEPOSIT &&
           usdc.toLowerCase() === tx.erc20Token?.toLowerCase()
         ) {
-          ethUsdValue = toTokenAmount(tx.erc20Amount, USDC_DECIMALS).minus(BIG_ZERO
-           /* getUsdAmt(toTokenAmount(tx.excessEth, 18), tx.timestamp),*/
+          ethUsdValue = toTokenAmount(tx.erc20Amount, USDC_DECIMALS).minus(
+            ethUsdPriceMap ? toTokenAmount(tx.excessEth, 18).multipliedBy(ethUsdPriceMap[Number(tx.timestamp) *1000]) : 0
           )
         }
         const lpAmount = toTokenAmount(tx.lpAmount, WETH_DECIMALS)
@@ -101,11 +104,11 @@ export const useUserCrabV2TxHistory = (user: string, isDescending?: boolean) => 
           txTitle: getTxTitle(tx.type),
         }
       }),
-    [data?.crabUserTxes, usdc],
+    [data?.crabUserTxes, usdc, ethUsdPriceMap],
   )
 
   return {
-    loading,
+    loading: loading || ethUsdPriceMapLoading,
     data: uiData,
     startPolling,
     stopPolling,
