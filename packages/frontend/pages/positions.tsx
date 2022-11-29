84cdd1b9ff1cfe990b7e365ed0f49998a4b8723d
KMKoushik <koushikmohan1996@gmail.com>
2022-11-30 08:50:57 +1100

    Include migrated shares for crabv2

--- packages/frontend/pages/positions.tsx
+++ packages/frontend/pages/positions.tsx
@@ -1,11 +1,13 @@
 import ConnectWallet from '@pages/positions/ConnectWallet'
 import Positions from '@pages/positions/Positions'
 import { useAtomValue } from 'jotai'
+import { useInitCrabMigration } from 'src/state/crabMigration/hooks'
 import { addressAtom, supportedNetworkAtom } from 'src/state/wallet/atoms'
 
 const PositionsPage = () => {
   const address = useAtomValue(addressAtom)
   const supportedNetwork = useAtomValue(supportedNetworkAtom)
+  useInitCrabMigration()
 
   if (address && supportedNetwork)
     return (
