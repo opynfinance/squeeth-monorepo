0b578366eb1e5254e97fb8fd1c4c3c86d3ec761a
KMKoushik <koushikmohan1996@gmail.com>
2022-11-30 16:36:25 +1100

    Add unsubscribe function

--- packages/frontend/src/components/Nav.tsx
+++ packages/frontend/src/components/Nav.tsx
@@ -8,7 +8,7 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
 import { useAtomValue } from 'jotai'
-import useAmplitude from '@hooks/useAmplitude';
+import useAmplitude from '@hooks/useAmplitude'
 
 import logo from '../../public/images/SqueethLogo.svg'
 import useCopyClipboard from '@hooks/useCopyClipboard'
@@ -107,12 +107,16 @@ const Nav: React.FC = () => {
   const [isCopied, setCopied] = useCopyClipboard()
 
   const router = useRouter()
-  const {track} = useAmplitude();
-  useEffect(()=>{
-    router.events.on('routeChangeComplete', (url)=> {
-      let e:string = (url.split('?')[0].substring(1)).toUpperCase();
-      track(("NAV_" + e));
+  const { track } = useAmplitude()
+  useEffect(() => {
+    router.events.on('routeChangeComplete', (url) => {
+      const e: string = url.split('?')[0].substring(1).toUpperCase()
+      track('NAV_' + e)
     })
+
+    return () => {
+      router.events.off('routeChangeComplete', () => console.log('Unsubscribed'))
+    }
   }, [router])
 
   return (
