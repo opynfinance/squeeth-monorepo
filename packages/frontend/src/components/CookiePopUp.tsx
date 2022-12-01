fdd41c78a0bfdfaab0c9b9dac0b9b22f18d4f5ba
nikhil arora <narora200@gmail.com>
2022-12-01 16:40:17 +0530

    fix: add a higher zindex to cookie popup

--- packages/frontend/src/components/CookiePopUp.tsx
+++ packages/frontend/src/components/CookiePopUp.tsx
@@ -1,6 +1,7 @@
-import CookieConsent, { Cookies } from "react-cookie-consent";
-import { CookieNames } from "@utils/cookies";
-import { createStyles, makeStyles } from "@material-ui/core";
+import CookieConsent from 'react-cookie-consent'
+import { CookieNames } from '@utils/cookies'
+import { createStyles, makeStyles } from '@material-ui/core'
+import Link from 'next/link'
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -12,15 +13,15 @@ const useStyles = makeStyles((theme) =>
       top: '60%',
       right: '0px',
       bottom: 'auto',
+      zIndex: 2000,
       transform: 'translate3d(-10%, -50%, 9999990px)',
       width: '85%',
     },
   }),
 )
 
-
-const CookiePopUp : React.FC = () => {
-    const styles = useStyles()
+const CookiePopUp: React.FC = () => {
+  const styles = useStyles()
 
   return (
     <div className={styles.container}>
@@ -28,22 +29,42 @@ const CookiePopUp : React.FC = () => {
         location="none"
         buttonText="Accept"
         cookieName={CookieNames.Consent}
-        style={{  background: "#3F4243", borderRadius: "10px", textAlign:'left' }}
-        buttonStyle={{ backgroundColor:"#D9D9D9", borderRadius:"10px", padding:'12px 24px', color: "#525556", fontSize: "14px", fontWeight:'bold' }}
+        style={{ background: '#3F4243', borderRadius: '10px', textAlign: 'left' }}
+        buttonStyle={{
+          backgroundColor: '#D9D9D9',
+          borderRadius: '10px',
+          padding: '12px 24px',
+          color: '#525556',
+          fontSize: '14px',
+          fontWeight: 'bold',
+        }}
         expires={365}
         enableDeclineButton
-        declineButtonText= "Decline"
-        declineButtonStyle={{ backgroundColor:"transparent", border:'2px solid #D9D9D9', padding:'10px 20px', borderRadius:"10px", color: "#fff", fontSize: "14px", fontWeight:'bold' }}
+        declineButtonText="Decline"
+        declineButtonStyle={{
+          backgroundColor: 'transparent',
+          border: '2px solid #D9D9D9',
+          padding: '10px 20px',
+          borderRadius: '10px',
+          color: '#fff',
+          fontSize: '14px',
+          fontWeight: 'bold',
+        }}
         flipButtons
         overlay
       >
         <h3>Cookie Preferences</h3>
-        We use cookies to support technical features that enhance your user experience and analyze frontend traffic. 
-        To learn more about these methods, including how to disable them, view our {" "}
-        <span style={{ color:"#2CE6F9" }}> <a href="/privacy-policy" target="_blank">Privacy Policy.</a> </span>
+        We use cookies to support technical features that enhance your user experience and analyze frontend traffic. To
+        learn more about these methods, including how to disable them, view our{' '}
+        <span style={{ color: '#2CE6F9' }}>
+          {' '}
+          <Link href="/privacy-policy">
+            <a target="_blank">Privacy Policy.</a>
+          </Link>{' '}
+        </span>
       </CookieConsent>
     </div>
-  );
-};
+  )
+}
 
-export default CookiePopUp;
+export default CookiePopUp
