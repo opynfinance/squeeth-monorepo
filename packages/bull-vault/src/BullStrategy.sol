df3d34daf7cc2aef5b1e93487649a1f3cc776a81
Haythem Sellami <sellamihaythem96@gmail.com>
2022-12-06 14:58:37 +0100

    fix

--- packages/bull-vault/src/BullStrategy.sol
+++ packages/bull-vault/src/BullStrategy.sol
@@ -169,7 +169,7 @@ contract BullStrategy is ERC20, LeverageBull {
         (uint256 ethInCrab, uint256 wPowerPerpInCrab) = _getCrabVaultDetails();
         // deposit eth into leverage component and borrow USDC
         (uint256 wethLent, uint256 usdcBorrowed, uint256 _totalWethInEuler) = _leverageDeposit(
-            msg.value, _crabAmount, share, ethInCrab, wPowerPerpInCrab, IERC20(crab).totalSupply()
+            _crabAmount, share, ethInCrab, wPowerPerpInCrab, IERC20(crab).totalSupply()
         );
 
         require(_totalWethInEuler <= strategyCap, "BS2");
