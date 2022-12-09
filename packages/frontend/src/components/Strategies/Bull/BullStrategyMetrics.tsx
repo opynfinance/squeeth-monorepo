cdd847e93729b1495c6d6cbfd22f44d0118f36c2
rushikeshakhare <devolopdifferent@gmail.com>
2022-12-09 16:52:06 +0530

    update bull metrics ui to match with crab

--- packages/frontend/src/components/Strategies/Bull/BullStrategyMetrics.tsx
+++ packages/frontend/src/components/Strategies/Bull/BullStrategyMetrics.tsx
@@ -1,59 +1,103 @@
 import React from 'react'
-import { Typography, Box } from '@material-ui/core'
+import { Typography, Box, Tooltip } from '@material-ui/core'
+import { Tooltips } from '@constants/enums'
+import InfoIcon from '@material-ui/icons/InfoOutlined'
 import { createStyles, makeStyles } from '@material-ui/core/styles'
 
 import Metric from '@components/Metric'
 
-const useStyles = makeStyles(() =>
+const useLabelStyles = makeStyles((theme) =>
   createStyles({
-    metricLabel: {
+    labelContainer: {
+      display: 'flex',
+      alignItems: 'center',
+      color: 'rgba(255, 255, 255, 0.5)',
+    },
+    label: {
       fontSize: '15px',
       fontWeight: 500,
       width: 'max-content',
-      color: 'rgba(255, 255, 255, 0.5)',
+    },
+    infoIcon: {
+      fontSize: '15px',
+      marginLeft: theme.spacing(0.5),
     },
   }),
 )
 
+const Label: React.FC<{ label: string; tooltipTitle: string }> = ({ label, tooltipTitle }) => {
+  const classes = useLabelStyles()
+
+  return (
+    <div className={classes.labelContainer}>
+      <Typography className={classes.label}>{label}</Typography>
+      <Tooltip title={tooltipTitle}>
+        <InfoIcon fontSize="small" className={classes.infoIcon} />
+      </Tooltip>
+    </div>
+  )
+}
+
 const BullStrategyMetrics: React.FC = () => {
-  const classes = useStyles()
+  const dailyHistoricalFundingPeriod = 420
 
   return (
     <Box display="flex" alignItems="center" flexWrap="wrap" gridGap="12px">
       <Metric
-        isSmall
-        flexBasis="180px"
-        label={<Typography className={classes.metricLabel}>ETH Price</Typography>}
+        flexBasis="250px"
+        label={<Label label="ETH Price" tooltipTitle={Tooltips.SpotPrice} />}
         value={`$1,119.07`}
       />
       <Metric
-        isSmall
-        flexBasis="180px"
-        label={<Typography className={classes.metricLabel}>Current Implied Premium</Typography>}
+        flexBasis="250px"
+        label={
+          <Label
+            label="Current Implied Premium"
+            tooltipTitle={`${Tooltips.StrategyEarnFunding}. ${Tooltips.CurrentImplFunding}`}
+          />
+        }
         value={`0.22%`}
       />
       <Metric
-        isSmall
-        flexBasis="180px"
-        label={<Typography className={classes.metricLabel}>Historical Daily Premium</Typography>}
+        flexBasis="250px"
+        label={
+          <Label
+            label="Historical Daily Premium"
+            tooltipTitle={`${
+              Tooltips.StrategyEarnFunding
+            }. ${`Historical daily premium based on the last ${dailyHistoricalFundingPeriod} hours. Calculated using a ${dailyHistoricalFundingPeriod} hour TWAP of Mark - Index`}`}
+          />
+        }
         value={`0.21%`}
       />
       <Metric
-        isSmall
-        flexBasis="180px"
-        label={<Typography className={classes.metricLabel}>Last rebalance</Typography>}
+        flexBasis="250px"
+        label={
+          <Label
+            label="Last rebalance"
+            tooltipTitle={
+              'Last rebalanced at ' +
+              new Date().toLocaleString(undefined, {
+                day: 'numeric',
+                month: 'long',
+                hour: 'numeric',
+                minute: 'numeric',
+                timeZoneName: 'long',
+              }) +
+              '. Rebalances approximately 3 times a week (on MWF) or every 20% ETH price move'
+            }
+          />
+        }
         value={`11/18, 8:44 AM`}
       />
       <Metric
-        isSmall
-        flexBasis="180px"
-        label={<Typography className={classes.metricLabel}>Stacks ETH if between</Typography>}
+        flexBasis="250px"
+        label={<Label label="Stack ETH if between" tooltipTitle={`Stack ETH if between the given values`} />}
         value={`~$1,128 - $1,293`}
       />
       <Metric
-        isSmall
-        flexBasis="180px"
-        label={<Typography className={classes.metricLabel}>Collateralization Ratio</Typography>}
+        flexBasis="250px"
+        label={<Label label="Collateralization Ratio" tooltipTitle={Tooltips.StrategyCollRatio} />}
         value={`200%`}
       />
     </Box>
