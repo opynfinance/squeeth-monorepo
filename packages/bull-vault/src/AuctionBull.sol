e721a766d1fd9b59884a7c6c3e7e895522e6b6bf
Andrew <5490766+aleone@users.noreply.github.com>
2022-12-11 07:31:52 -0800

    [Audit Fix][N-09] Misleading parameter passing (#778)
    * update configs
    
    * update configs
    
    * update path
    
    * queue crab netting jobs
    
    * update report name
    
    * added coverage for flashdeposit with fee and auction setting
    
    * missing case
    
    * fix limit price for test
    
    * generalize rebal test with combinations of price movements
    
    * failing withdraw tests
    
    * added more cases
    
    * withdraw weth borrow usdc deposit case
    
    * lint
    
    * skull and x bonez test
    
    * add case for withdraw crab, deposit leverage, repay debt'
    
    * clean up + add fuzz for auction
    
    * missing case
    
    * lint
    
    * skull and x bonez test
    
    * clean up + add fuzz for auction
    
    * missing case
    
    * lint
    
    * clean up + add fuzz for auction
    
    * fix
    
    * missing case
    
    * lint
    
    * clean up + add fuzz for auction
    
    * update configs
    
    * fix
    
    * fix
    
    * fix
    
    * add delta test (#755)
    
    * add delta test
    
    * lint
    
    Co-authored-by: Haythem Sellami <sellamihaythem96@gmail.com>
    
    * change expect revert
    
    * missing case
    
    * merge + finish coverage
    
    * add case for withdraw crab, deposit leverage, repay debt'
    
    * clean up + add fuzz for auction
    
    * inverse tests
    
    * comment tests
    
    * Change limit price to fix test
    
    * added back testFullRebalanceWhenEthDownAndCrTooLow and changed limit price
    
    * lint
    
    * refactor
    
    * add case for withdraw crab, deposit leverage, repay debt'
    
    * clean up + add fuzz for auction
    
    * remove isShutdown() require statement
    
    * remove isShutdown() require statement
    
    * skull and x bonez test
    
    * add case for withdraw crab, deposit leverage, repay debt'
    
    * clean up + add fuzz for auction
    
    * skull and x bonez test
    
    * add case for withdraw crab, deposit leverage, repay debt'
    
    * clean up + add fuzz for auction
    
    * add docs
    
    * add extension
    
    * skull and x bonez test
    
    * add case for withdraw crab, deposit leverage, repay debt'
    
    * clean up + add fuzz for auction
    
    * add missing natspec and clean up and missing params, returns, etc
    
    * lintage
    
    * skull and x bonez test
    
    * add case for withdraw crab, deposit leverage, repay debt'
    
    * clean up + add fuzz for auction
    
    * add missing natspec and clean up and missing params, returns, etc
    
    * lintage
    
    * skull and x bonez test
    
    * add case for withdraw crab, deposit leverage, repay debt'
    
    * clean up + add fuzz for auction
    
    * skull and x bonez test
    
    * add case for withdraw crab, deposit leverage, repay debt'
    
    * clean up + add fuzz for auction
    
    * adjust params in deposit() to use correct crab vs bull for clarity
    
    * merge and lint
    
    * fix duplicated code
    
    * fix test, merge downstream
    
    Co-authored-by: Haythem Sellami <sellamihaythem96@gmail.com>
    Co-authored-by: alpinechicken <joecmail@gmail.com>

--- packages/bull-vault/src/AuctionBull.sol
+++ packages/bull-vault/src/AuctionBull.sol
@@ -75,6 +75,16 @@ import { console } from "forge-std/console.sol";
 
 import { console } from "forge-std/console.sol";
 
+import { console } from "forge-std/console.sol";
+
+import { console } from "forge-std/console.sol";
+
+import { console } from "forge-std/console.sol";
+
+import { console } from "forge-std/console.sol";
+
+import { console } from "forge-std/console.sol";
+
 // interface
 import { IController } from "squeeth-monorepo/interfaces/IController.sol";
 import { IBullStrategy } from "./interface/IBullStrategy.sol";
@@ -242,6 +252,26 @@ contract AuctionBull is UniFlash, Ownable, EIP712 {
         uint256 _oldWethLimitPriceTolerance, uint256 _newWethLimitPriceTolerance
     );
     event SetAuctionManager(address newAuctionManager, address oldAuctionManager);
+
+    /**
+     * @notice constructor for AuctionBull
+     * @param _auctionManager the address that can run auctions
+     * @param _bull bull strategy address
+     * @param _factory uniswap factory address
+     * @param _crab crab strategy address
+     * @param _eToken euler collateral token address for weth
+     * @param _dToken euler debt token address for usdc
+     */
+
+    /**
+     * @notice constructor for AuctionBull
+     * @param _auctionManager the address that can run auctions
+     * @param _bull bull strategy address
+     * @param _factory uniswap factory address
+     * @param _crab crab strategy address
+     * @param _eToken euler collateral token address for weth
+     * @param _dToken euler debt token address for usdc
+     */
 
     /**
      * @notice constructor for AuctionBull
