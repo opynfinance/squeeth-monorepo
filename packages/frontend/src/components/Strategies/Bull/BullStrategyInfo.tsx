7756ff87ced0b14735ce031e6bb544a90e0eac77
rushikeshakhare <devolopdifferent@gmail.com>
2022-12-13 13:15:01 +0530

    calm bull to zen bull

--- packages/frontend/src/components/Strategies/Bull/BullStrategyInfo.tsx
+++ packages/frontend/src/components/Strategies/Bull/BullStrategyInfo.tsx
@@ -58,7 +58,7 @@ export const BullStrategyInfo: React.FC = () => {
       </Typography>
       <div className={classes.container}>
         <Typography variant="subtitle1" className={clsx(classes.caption, classes.fontColorCaption)}>
-          Calm bull likes calm bull markets. It makes money when ETH goes up, slow and steady. It stacks ETH when ETH
+          Zen bull likes zen bull markets. It makes money when ETH goes up, slow and steady. It stacks ETH when ETH
           stays within around {ethPriceDown}-{ethPriceUp} before the next rebalance.
         </Typography>
 
@@ -71,24 +71,24 @@ export const BullStrategyInfo: React.FC = () => {
             Risk
           </Typography>
           <Typography variant="subtitle1" className={clsx(classes.content, classes.fontColorBody)}>
-            The Calm Bull Strategy contains a short squeeth and an ETH leverage position. If the strategy falls below
-            the safe collateralization threshold ({shortSqueethPosition} on the short squeeth position and{' '}
+            The Zen Bull Strategy contains a short squeeth and an ETH leverage position. If the strategy falls below the
+            safe collateralization threshold ({shortSqueethPosition} on the short squeeth position and{' '}
             {ethLeveragePosition} on the ETH leverage position), the strategy is at risk of liquidation. Rebalancing
             based on large ETH price changes helps prevent a liquidation from occurring.
             <br /> <br />
-            Based on current premiums, calm bull strategy would only be stacking ETH if ETH stays within ETH stays
-            within around {ethPriceDown}-{ethPriceUp} before the next rebalance. The implied premium which you deposit
-            at impacts your profitability. Depositing at a higher premium than expected increases likelihood of
+            Based on current premiums, zen bull strategy would only be stacking ETH if ETH stays within ETH stays within
+            around {ethPriceDown}-{ethPriceUp} before the next rebalance. The implied premium which you deposit at
+            impacts your profitability. Depositing at a higher premium than expected increases likelihood of
             profitability.
             <br /> <br />
-            If the Squeeth premium increases, it will be more expensive to close your position. Calm bull aims to be
+            If the Squeeth premium increases, it will be more expensive to close your position. Zen bull aims to be
             profitable in ETH terms.{' '}
             <a className={classes.link} href={Links.CrabFAQ} target="_blank" rel="noreferrer">
               {' '}
               Learn more.{' '}
             </a>
             <br /> <br />
-            Calm bull smart contracts have been audited by Open Zeppelin. However, smart contracts are experimental
+            Zen bull smart contracts have been audited by Open Zeppelin. However, smart contracts are experimental
             technology and we encourage caution only risking funds you can afford to lose.
           </Typography>
         </Box>
