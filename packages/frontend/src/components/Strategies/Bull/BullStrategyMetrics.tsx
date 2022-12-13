4d1d9c99705a32ae1ab033dc631d1a413393ff42
Rushikesh Akhare <52928941+rushikeshakhare@users.noreply.github.com>
2022-12-13 15:56:22 +0530

    fix: zen bull ui changes (#828)
    * graphic update
    
    * calm bull to zen bull
    
    * metrics ui update
    
    * load more with 3 + 3 increment pattern

--- packages/frontend/src/components/Strategies/Bull/BullStrategyMetrics.tsx
+++ packages/frontend/src/components/Strategies/Bull/BullStrategyMetrics.tsx
@@ -1,27 +1,49 @@
 import React from 'react'
-import { Typography, Box } from '@material-ui/core'
+import { Typography, Box, Tooltip } from '@material-ui/core'
+import { Tooltips } from '@constants/enums'
+import InfoIcon from '@material-ui/icons/InfoOutlined'
 import { createStyles, makeStyles } from '@material-ui/core/styles'
 
 import Metric from '@components/Metric'
-import { useAtom, useAtomValue } from 'jotai'
+import { useAtomValue } from 'jotai'
 import { bullCRAtom } from '@state/bull/atoms'
 import { crabStrategyCollatRatioAtomV2 } from '@state/crab/atoms'
 import { currentImpliedFundingAtom, dailyHistoricalFundingAtom } from '@state/controller/atoms'
 import { formatNumber } from '@utils/formatter'
 
-const useStyles = makeStyles(() =>
+const useLabelStyles = makeStyles((theme) =>
   createStyles({
-    metricLabel: {
+    labelContainer: {
+      display: 'flex',
+      alignItems: 'center',
+      color: 'rgba(255, 255, 255, 0.5)',
+    },
+    label: {
       fontSize: '15px',
       fontWeight: 500,
       width: 'max-content',
-      color: 'rgba(255, 255, 255, 0.5)',
+    },
+    infoIcon: {
+      fontSize: '15px',
+      marginLeft: theme.spacing(0.5),
     },
   }),
 )
 
+const Label: React.FC<{ label: string; tooltipTitle: string }> = ({ label, tooltipTitle }) => {
+  const classes = useLabelStyles()
+
+  return (
+    <div className={classes.labelContainer}>
+      <Typography className={classes.label}>{label}</Typography>
+      <Tooltip title={tooltipTitle}>
+        <InfoIcon fontSize="small" className={classes.infoIcon} />
+      </Tooltip>
+    </div>
+  )
+}
+
 const BullStrategyMetrics: React.FC = () => {
-  const classes = useStyles()
   const bullCr = useAtomValue(bullCRAtom).times(100)
   const crabCr = useAtomValue(crabStrategyCollatRatioAtomV2)
   const dailyHistoricalFunding = useAtomValue(dailyHistoricalFundingAtom)
@@ -30,39 +52,60 @@ const BullStrategyMetrics: React.FC = () => {
   return (
     <Box display="flex" alignItems="center" flexWrap="wrap" gridGap="12px">
       <Metric
-        isSmall
         flexBasis="250px"
-        label={<Typography className={classes.metricLabel}>ETH Price</Typography>}
+        label={<Label label="ETH Price" tooltipTitle={Tooltips.SpotPrice} />}
         value={`$1,119.07`}
       />
       <Metric
-        isSmall
         flexBasis="250px"
-        label={<Typography className={classes.metricLabel}>Current Implied Premium</Typography>}
+        label={
+          <Label
+            label="Current Implied Premium"
+            tooltipTitle={`${Tooltips.StrategyEarnFunding}. ${Tooltips.CurrentImplFunding}`}
+          />
+        }
         value={`${formatNumber(currentImpliedFunding * 100)}%`}
       />
       <Metric
-        isSmall
         flexBasis="250px"
-        label={<Typography className={classes.metricLabel}>Historical Daily Premium</Typography>}
+        label={
+          <Label
+            label="Historical Daily Premium"
+            tooltipTitle={`${
+              Tooltips.StrategyEarnFunding
+            }. ${`Historical daily premium based on the last ${dailyHistoricalFunding.period} hours. Calculated using a ${dailyHistoricalFunding.period} hour TWAP of Mark - Index`}`}
+          />
+        }
         value={`${formatNumber(dailyHistoricalFunding.funding * 100)}%`}
       />
       <Metric
-        isSmall
         flexBasis="250px"
-        label={<Typography className={classes.metricLabel}>Last rebalance</Typography>}
+        label={
+          <Label
+            label="Last rebalance"
+            tooltipTitle={
+              'Last rebalanced at ' +
+              new Date().toLocaleString(undefined, {
+                day: 'numeric',
+                month: 'long',
+                hour: 'numeric',
+                minute: 'numeric',
+                timeZoneName: 'long',
+              }) +
+              '. Rebalances approximately 3 times a week (on MWF) or every 20% ETH price move'
+            }
+          />
+        }
         value={`11/18, 8:44 AM`}
       />
       <Metric
-        isSmall
         flexBasis="250px"
-        label={<Typography className={classes.metricLabel}>Stacks ETH if between</Typography>}
+        label={<Label label="Stack ETH if between" tooltipTitle={Tooltips.BullStrategyProfitThreshold} />}
         value={`~$1,128 - $1,293`}
       />
       <Metric
-        isSmall
         flexBasis="250px"
-        label={<Typography className={classes.metricLabel}>Collateralization Ratio</Typography>}
+        label={<Label label="Collateralization Ratio" tooltipTitle={Tooltips.StrategyCollRatio} />}
         value={`${(bullCr.lt(crabCr) ? bullCr : crabCr).toFixed(0)}%`}
       />
     </Box>
--- packages/frontend/src/state/bull/atoms.ts
+++ packages/frontend/src/state/bull/atoms.ts
@@ -1,7 +1,7 @@
 import BigNumber from 'bignumber.js'
 import { atom } from 'jotai'
 
-export const visibleStrategyRebalancesAtom = atom<number>(10)
+export const visibleStrategyRebalancesAtom = atom<number>(3)
 export const bullCrabBalanceAtom = atom(new BigNumber(0))
 export const bullSupplyAtom = atom(new BigNumber(0))
 export const bullEulerWethCollatPerShareAtom = atom(new BigNumber(0))
