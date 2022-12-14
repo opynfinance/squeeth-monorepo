fa070e7451de3069d89a3ef8fec5cba4738dcf11
KMKoushik <koushikmohan1996@gmail.com>
2022-12-15 08:47:47 +1100

    Fix env

--- packages/frontend/src/components/Strategies/Crab/CrabTradeV2/Deposit.tsx
+++ packages/frontend/src/components/Strategies/Crab/CrabTradeV2/Deposit.tsx
@@ -65,7 +65,8 @@ enum DepositSteps {
   DEPOSIT = 'Deposit',
 }
 
-const OTC_PRICE_IMPACT_THRESHOLD = Number(process.env.OTC_PRICE_IMPACT_THRESHOLD) || 1
+const OTC_PRICE_IMPACT_THRESHOLD = Number(process.env.NEXT_PUBLIC_OTC_PRICE_IMPACT_THRESHOLD) || 1
+console.log(OTC_PRICE_IMPACT_THRESHOLD, 'Threshold', process.env.NEXT_PUBLIC_OTC_PRICE_IMPACT_THRESHOLD)
 
 const CrabDeposit: React.FC<CrabDepositProps> = ({ maxCap, depositedAmount }) => {
   const classes = useStyles()
