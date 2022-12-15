82124d826fb7b2b06d4e8c910d7a8284b2dd5ca2
Haythem Sellami <sellamihaythem96@gmail.com>
2022-12-15 13:10:48 +0100

    zen-bull-vault circleci config

--- packages/bull-vault/src/interface/IEulerMarkets.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity =0.7.6;
-
-interface IEulerMarkets {
-    function underlyingToEToken(address underlying) external view returns (address);
-    function underlyingToDToken(address underlying) external view returns (address);
-    function enterMarket(uint256 subAccountId, address newMarket) external;
-}
