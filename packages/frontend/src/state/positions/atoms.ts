94e9f33c5ee4dbcfe6d00f5eca128ea79cfc2600
KM Koushik <koushikmohan1996@gmail.com>
2022-12-18 18:14:41 +1100

    Add bull profit threshold (#847)
    * Add bull profit threshold
    
    * Add review changes

--- packages/frontend/src/state/positions/atoms.ts
+++ packages/frontend/src/state/positions/atoms.ts
@@ -26,6 +26,7 @@ import {
   BULL_STRATEGY,
   WETH_E_TOKEN,
   AUCTION_BULL,
+  EULER_SIMPLE_LENS,
 } from '@constants/address'
 import { networkIdAtom, web3Atom } from '../wallet/atoms'
 import { BIG_ZERO } from '@constants/index'
@@ -70,6 +71,7 @@ export const addressesAtom = atom((get) => {
     bullStrategy: BULL_STRATEGY[networkId].toLowerCase(),
     wethEToken: WETH_E_TOKEN[networkId].toLowerCase(),
     auctionBull: AUCTION_BULL[networkId].toLowerCase(),
+    eulerSimpleLens: EULER_SIMPLE_LENS[networkId].toLowerCase(),
   }
 })
 
