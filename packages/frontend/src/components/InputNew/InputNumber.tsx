b00a7417f7db7011f00e320a23fc913c90497505
nikhil arora <narora200@gmail.com>
2022-12-23 16:33:32 +0530

    fix: fix ref not passing through

--- packages/frontend/src/components/InputNew/InputNumber.tsx
+++ packages/frontend/src/components/InputNew/InputNumber.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import InputBase, { InputBaseProps } from './InputBase'
 
 const DECIMAL_REGEX = RegExp('^[0-9]*[.]{1}[0-9]*$')
@@ -7,43 +8,47 @@ interface InputNumberCustomProps extends InputBaseProps {
 }
 export type InputNumberProps = InputNumberCustomProps
 
-const InputNumber: React.FC<InputNumberProps> = ({ value, onInputChange = () => {}, inputProps, ...props }) => {
-  const handleChange = (val: string) => {
-    if (isNaN(Number(val))) {
-      return onInputChange('0')
-    }
-
-    if (Number(val) < 0) {
-      return onInputChange('0')
-    }
-
-    if (Number(val) !== 0) {
-      // if it is integer, remove leading zeros
-      if (!DECIMAL_REGEX.test(val)) {
-        val = Number(val).toString()
+const InputNumber = React.forwardRef<any, InputNumberProps>(
+  ({ value, onInputChange = () => {}, inputProps, ...props }, ref) => {
+    const handleChange = (val: string) => {
+      if (isNaN(Number(val))) {
+        return onInputChange('0')
       }
-    } else {
-      // remain input box w single zero, but keep zero when have decimal
-      val = val.replace(/^[0]+/g, '0')
-      // if it is no value
-      if (val.length === 0) {
-        val = '0'
+
+      if (Number(val) < 0) {
+        return onInputChange('0')
       }
+
+      if (Number(val) !== 0) {
+        // if it is integer, remove leading zeros
+        if (!DECIMAL_REGEX.test(val)) {
+          val = Number(val).toString()
+        }
+      } else {
+        // remain input box w single zero, but keep zero when have decimal
+        val = val.replace(/^[0]+/g, '0')
+        // if it is no value
+        if (val.length === 0) {
+          val = '0'
+        }
+      }
+
+      return onInputChange(val)
     }
 
-    return onInputChange(val)
-  }
-
-  return (
-    <InputBase
-      value={value}
-      onChange={(event) => handleChange(event.target.value)}
-      placeholder="0"
-      autoComplete="false"
-      inputProps={{ inputmode: 'numeric', pattern: '[0-9]*', ...inputProps }}
-      {...props}
-    />
-  )
-}
+    return (
+      <InputBase
+        ref={ref}
+        value={value}
+        onChange={(event) => handleChange(event.target.value)}
+        placeholder="0"
+        autoComplete="false"
+        inputProps={{ inputmode: 'numeric', pattern: '[0-9]*', ...inputProps }}
+        {...props}
+      />
+    )
+  },
+)
+InputNumber.displayName = 'InputNumber'
 
 export default InputNumber
