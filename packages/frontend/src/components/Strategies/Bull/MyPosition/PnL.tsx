00d8a926fb6608017e5340b3b2f0b41e535d8dbd
nikhil arora <narora200@gmail.com>
2023-01-10 23:16:15 +0530

    feat: horizontal alignment of pnl

--- packages/frontend/src/components/Strategies/Bull/MyPosition/PnL.tsx
+++ packages/frontend/src/components/Strategies/Bull/MyPosition/PnL.tsx
@@ -1,77 +1,57 @@
 import { Typography, Box, CircularProgress } from '@material-ui/core'
-import React, { memo } from 'react'
+import React from 'react'
 import clsx from 'clsx'
 import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp'
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown'
-import { useAtomValue } from 'jotai'
+import BigNumber from 'bignumber.js'
 
-import { bullEthPnlAtom, bullEthPnlPerctAtom } from '@state/bull/atoms'
 import useStyles from '@components/Strategies/styles'
 import { formatNumber } from '@utils/formatter'
-import SharePnL from '@components/Strategies/SharePnL'
 
-const PnL: React.FC = () => {
-  const bullEthPnL = useAtomValue(bullEthPnlAtom)
-  const bullEthPnlPerct = useAtomValue(bullEthPnlPerctAtom)
+interface PnLProps {
+  isPnlLoading: Boolean
+  bullEthPnl: BigNumber
+  bullEthPnlPercent: BigNumber
+}
+
+const PnL: React.FC<PnLProps> = ({ isPnlLoading, bullEthPnl, bullEthPnlPercent }) => {
   const classes = useStyles()
-
-  const isPnlLoading = !bullEthPnL.isFinite()
 
   if (isPnlLoading) {
     return (
-      <Box display="flex" alignItems="center" gridGap="12px">
+      <Box display="flex" alignItems="center" gridGap="8px">
         <CircularProgress size="1rem" className={classes.loadingSpinner} />
         <Typography className={classes.text}>fetching pnl...</Typography>
       </Box>
     )
   }
 
-  const pnlPercent = bullEthPnlPerct.toNumber()
-  const pnlFormatted = formatNumber(pnlPercent)
-  const pnlText = pnlPercent > 0 ? `+${pnlFormatted}%` : `${pnlFormatted}%`
-
-  const sharePnLText = `I’m earning ${pnlText} stacking ETH with the Opyn Zen Bull Strategy`
-  const sharePnLUrl = 'squeeth.com/strategies/bull'
-
-  const isPnlPositive = bullEthPnL.isGreaterThanOrEqualTo(0)
+  const isPnlPositive = bullEthPnl.isGreaterThanOrEqualTo(0)
+  const textClassName = clsx(
+    classes.description,
+    classes.textSemibold,
+    classes.textMonospace,
+    isPnlPositive ? classes.colorSuccess : classes.colorError,
+  )
 
   return (
-    <Box display="flex" flexDirection="column" gridGap="12px">
-      <Box display="flex" alignItems="center" gridGap="8px">
+    <Box display="flex" gridGap="8px" flexWrap="wrap" position="relative" top="2px">
+      <Box display="flex" gridGap="8px">
         <Box display="flex" marginLeft="-6px">
           {isPnlPositive ? (
-            <ArrowDropUpIcon className={classes.colorSuccess} />
+            <ArrowDropUpIcon fontSize="small" className={classes.colorSuccess} />
           ) : (
-            <ArrowDropDownIcon className={classes.colorError} />
+            <ArrowDropDownIcon fontSize="small" className={classes.colorError} />
           )}
-
-          <Typography
-            className={clsx(
-              classes.description,
-              classes.textSemibold,
-              classes.textMonospace,
-              isPnlPositive ? classes.colorSuccess : classes.colorError,
-            )}
-          >
-            {formatNumber(bullEthPnlPerct.toNumber()) + '%'}
-          </Typography>
+          <Typography className={textClassName}>{formatNumber(bullEthPnlPercent.toNumber()) + '%'}</Typography>
         </Box>
 
-        <Typography
-          className={clsx(
-            classes.description,
-            classes.textSemibold,
-            classes.textMonospace,
-            isPnlPositive ? classes.colorSuccess : classes.colorError,
-          )}
-        >
+        <Typography className={textClassName}>
           ({isPnlPositive && '+'}
-          {formatNumber(bullEthPnL.toNumber(), 4) + ' ETH'})
+          {formatNumber(bullEthPnl.toNumber(), 4) + ' ETH'})
         </Typography>
-        <Typography className={classes.description}>since deposit</Typography>
       </Box>
-
-      <SharePnL text={sharePnLText} url={sharePnLUrl} />
+      <Typography className={classes.description}>since deposit</Typography>
     </Box>
   )
 }
