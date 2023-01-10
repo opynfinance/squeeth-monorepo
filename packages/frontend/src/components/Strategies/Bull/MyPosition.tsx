31668c36f4e2ad15c07f09973094dafcb14cb320
nikhil arora <narora200@gmail.com>
2023-01-10 12:24:52 +0530

    style: design ready for bull

--- packages/frontend/src/components/Strategies/Bull/MyPosition.tsx
+++ packages/frontend/src/components/Strategies/Bull/MyPosition.tsx
@@ -1,10 +1,13 @@
-import { Typography, Box, CircularProgress } from '@material-ui/core'
+import { Typography, Box, CircularProgress, ButtonBase } from '@material-ui/core'
 import React, { memo } from 'react'
 import clsx from 'clsx'
 import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp'
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown'
-
+import TwitterIcon from '@material-ui/icons/Twitter'
+import TelegramIcon from '@material-ui/icons/Telegram'
 import { useAtomValue } from 'jotai'
+import { makeStyles, createStyles } from '@material-ui/core/styles'
+
 import {
   bullCurrentETHPositionAtom,
   bullCurrentUSDCPositionAtom,
@@ -15,6 +18,42 @@ import {
 } from '@state/bull/atoms'
 import useStyles from '@components/Strategies/styles'
 import { formatCurrency, formatNumber } from '@utils/formatter'
+
+const useSharePnLStyles = makeStyles((theme) =>
+  createStyles({
+    buttonRoot: {
+      border: '1px solid #fff',
+      borderRadius: '100%',
+      width: '26px',
+      height: '26px',
+      backgroundColor: '#101010',
+      '&:hover': {
+        backgroundColor: theme.palette.background.lightStone,
+      },
+      transition: 'background-color 0.1s ease-in',
+    },
+    icon: {
+      width: '14px',
+      height: '14px',
+    },
+  }),
+)
+
+const SharePnL: React.FC<{ pnlPercent: number }> = ({ pnlPercent }) => {
+  const classes = useSharePnLStyles()
+
+  return (
+    <Box display="flex" gridGap="8px" alignItems="center">
+      <ButtonBase classes={{ root: classes.buttonRoot }}>
+        <TwitterIcon className={classes.icon} />
+      </ButtonBase>
+
+      <ButtonBase classes={{ root: classes.buttonRoot }}>
+        <TelegramIcon className={classes.icon} />
+      </ButtonBase>
+    </Box>
+  )
+}
 
 const BullPosition: React.FC = () => {
   const bullPosition = useAtomValue(bullCurrentETHPositionAtom)
@@ -44,29 +83,42 @@ const BullPosition: React.FC = () => {
   const isPnlPositive = bullEthPnL.isGreaterThanOrEqualTo(0)
 
   return (
-    <Box display="flex" flexDirection="column" gridGap="8px">
-      <Typography variant="h4" className={classes.sectionTitle}>
-        My Zen Bull Position
-      </Typography>
-
-      <Box display="flex" gridGap="12px" alignItems="baseline">
-        <Typography className={clsx(classes.heading, classes.textMonospace)}>
-          {formatNumber(bullPosition.toNumber(), 4) + ' ETH'}
+    <Box display="flex" flexDirection="column" gridGap="12px">
+      <Box display="flex" flexDirection="column" gridGap="8px">
+        <Typography variant="h4" className={classes.sectionTitle}>
+          My Zen Bull Position
         </Typography>
-        <Typography className={clsx(classes.description, classes.textMonospace)}>
-          {formatCurrency(bullUsdcPosition.toNumber())}
-        </Typography>
-      </Box>
 
-      <Box display="flex" alignItems="center" gridGap="8px">
-        {bullEthPnL.isFinite() ? (
-          <>
-            <Box display="flex" marginLeft="-6px">
-              {isPnlPositive ? (
-                <ArrowDropUpIcon className={classes.colorSuccess} />
-              ) : (
-                <ArrowDropDownIcon className={classes.colorError} />
-              )}
+        <Box display="flex" gridGap="12px" alignItems="baseline">
+          <Typography className={clsx(classes.heading, classes.textMonospace)}>
+            {formatNumber(bullPosition.toNumber(), 4) + ' ETH'}
+          </Typography>
+          <Typography className={clsx(classes.description, classes.textMonospace)}>
+            {formatCurrency(bullUsdcPosition.toNumber())}
+          </Typography>
+        </Box>
+
+        <Box display="flex" alignItems="center" gridGap="8px">
+          {bullEthPnL.isFinite() ? (
+            <>
+              <Box display="flex" marginLeft="-6px">
+                {isPnlPositive ? (
+                  <ArrowDropUpIcon className={classes.colorSuccess} />
+                ) : (
+                  <ArrowDropDownIcon className={classes.colorError} />
+                )}
+
+                <Typography
+                  className={clsx(
+                    classes.description,
+                    classes.textSemibold,
+                    classes.textMonospace,
+                    isPnlPositive ? classes.colorSuccess : classes.colorError,
+                  )}
+                >
+                  {formatNumber(bullEthPnlPerct.toNumber()) + '%'}
+                </Typography>
+              </Box>
 
               <Typography
                 className={clsx(
@@ -76,30 +128,21 @@ const BullPosition: React.FC = () => {
                   isPnlPositive ? classes.colorSuccess : classes.colorError,
                 )}
               >
-                {formatNumber(bullEthPnlPerct.toNumber()) + '%'}
+                ({isPnlPositive && '+'}
+                {formatNumber(bullEthPnL.toNumber(), 4) + ' ETH'})
               </Typography>
+              <Typography className={classes.description}>since deposit</Typography>
+            </>
+          ) : (
+            <Box display="flex" alignItems="center" gridGap="12px">
+              <CircularProgress size="1rem" className={classes.loadingSpinner} />
+              <Typography className={classes.text}>fetching pnl...</Typography>
             </Box>
-
-            <Typography
-              className={clsx(
-                classes.description,
-                classes.textSemibold,
-                classes.textMonospace,
-                isPnlPositive ? classes.colorSuccess : classes.colorError,
-              )}
-            >
-              ({isPnlPositive && '+'}
-              {formatNumber(bullEthPnL.toNumber(), 4) + ' ETH'})
-            </Typography>
-            <Typography className={classes.description}>since deposit</Typography>
-          </>
-        ) : (
-          <Box display="flex" alignItems="center" gridGap="12px">
-            <CircularProgress size="1rem" className={classes.loadingSpinner} />
-            <Typography className={classes.text}>fetching pnl...</Typography>
-          </Box>
-        )}
+          )}
+        </Box>
       </Box>
+
+      <SharePnL pnlPercent={bullEthPnlPerct.toNumber()} />
     </Box>
   )
 }
