8d5cdf51e8b1604551fceb9dbdde73814af20353
nikhil arora <narora200@gmail.com>
2023-01-11 17:08:02 +0530

    feat: add /share-pnl page meta attributes

--- packages/frontend/pages/share-pnl.tsx
+++ packages/frontend/pages/share-pnl.tsx
@@ -1,11 +1,25 @@
 import Head from 'next/head'
+import { Box, Typography } from '@material-ui/core'
 
 const SharePnl = () => {
   return (
-    <Head>
-      <title>The post title</title>
-      <meta property="og:image" content="/api/og?title=my post title" />
-    </Head>
+    <>
+      <Head>
+        <title>The post title</title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Squeeth" />
+        <meta
+          name="twitter:description"
+          content="Squeeth is a new financial primitive in DeFi that gives traders exposure to ETH²"
+        />
+        <meta name="twitter:image" content="https://squeeth.opyn.co/api/pnl?title=my-post-title" />
+        <meta property="og:image" content="https://squeeth.opyn.co/api/pnl?title=my-post-title" />
+      </Head>
+
+      <Box marginTop="20px" marginLeft="20px">
+        <Typography variant="h3">/share-pnl page</Typography>
+      </Box>
+    </>
   )
 }
 
