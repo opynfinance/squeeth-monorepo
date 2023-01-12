bb3b1e5de03033a986560fb66cf1c5338f6a3e4a
nikhil arora <narora200@gmail.com>
2023-01-12 16:48:43 +0530

    in progress

--- packages/frontend/pages/api/pnl.tsx
+++ packages/frontend/pages/api/pnl.tsx
@@ -2,46 +2,69 @@ import { ImageResponse } from '@vercel/og'
 import { NextRequest } from 'next/server'
 import Image from 'next/image'
 import React from 'react'
-import { Box, Typography } from '@material-ui/core'
-import { makeStyles, createStyles } from '@material-ui/core/styles'
 
-import Emoji from '@components/Emoji'
+import { getCrabPnlV2ChartData } from '@utils/pricer'
+import logo from '../../public/images/logo.svg'
 
 export const config = {
   runtime: 'experimental-edge',
 }
 
-const useStyles = makeStyles((theme) => createStyles({}))
-
 // default timestamp = 1672901127170
 
 const UI: React.FC<{ depositTimestamp: number }> = ({ depositTimestamp }) => {
-  const classes = useStyles()
-
   return (
-    <div>
-      <Box display="flex" justifyContent="space-between">
-        <Box display="flex" gridGap="12px"></Box>
-      </Box>
+    <div
+      style={{
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundColor: '#191B1C',
+        padding: '50px 200px',
+      }}
+    >
+      <div tw="flex text-4xl text-white font-bold">Crabber - Stacking USDC</div>
+
+      <img
+        alt="logo"
+        width="256"
+        src={'https://squeeth.opyn.co/images/logo.svg'}
+        style={{
+          borderRadius: 128,
+        }}
+      />
+
+      <div tw="flex text-4xl text-white font-regular">{depositTimestamp}</div>
     </div>
   )
 }
 
-export default function handler(req: NextRequest) {
+export default async function handler(req: NextRequest) {
   try {
     const { searchParams } = new URL(req.url)
-    const hasTimestamp = searchParams.has('depositTimestamp')
-    if (!hasTimestamp) {
+
+    const depositTimestamp = searchParams.get('depositTimestamp')
+    if (!depositTimestamp) {
       return new Response(`Missing "depositTimestamp" query param`, {
         status: 400,
       })
     }
 
-    const depositTimestamp = searchParams.get('depositTimestamp')
+    const depositDate = new Date(depositTimestamp)
+    const today = new Date()
+
+    const data = await getCrabPnlV2ChartData(
+      Number(depositDate.valueOf().toString().slice(0, -3)),
+      Number(today.valueOf().toString().slice(0, -3)),
+    )
+
+    console.log({ data })
 
     return new ImageResponse(<UI depositTimestamp={Number(depositTimestamp)} />, {
       width: 1200,
       height: 630,
+      emoji: 'twemoji',
     })
   } catch (e: any) {
     console.log(`${e.message}`)
