e476f424ec694a278acc8cd8a5d2c6288c009314
nikhil arora <narora200@gmail.com>
2023-01-12 12:19:33 +0530

    in progress

--- packages/frontend/pages/api/pnl.tsx
+++ packages/frontend/pages/api/pnl.tsx
@@ -1,72 +1,48 @@
 import { ImageResponse } from '@vercel/og'
 import { NextRequest } from 'next/server'
 import Image from 'next/image'
+import React from 'react'
+import { Box, Typography } from '@material-ui/core'
+import { makeStyles, createStyles } from '@material-ui/core/styles'
+
+import Emoji from '@components/Emoji'
 
 export const config = {
   runtime: 'experimental-edge',
 }
 
+const useStyles = makeStyles((theme) => createStyles({}))
+
+// default timestamp = 1672901127170
+
+const UI: React.FC<{ depositTimestamp: number }> = ({ depositTimestamp }) => {
+  const classes = useStyles()
+
+  return (
+    <div>
+      <Box display="flex" justifyContent="space-between">
+        <Box display="flex" gridGap="12px"></Box>
+      </Box>
+    </div>
+  )
+}
+
 export default function handler(req: NextRequest) {
   try {
     const { searchParams } = new URL(req.url)
+    const hasTimestamp = searchParams.has('depositTimestamp')
+    if (!hasTimestamp) {
+      return new Response(`Missing "depositTimestamp" query param`, {
+        status: 400,
+      })
+    }
 
-    // ?title=<title>
-    const hasTitle = searchParams.has('title')
-    const title = hasTitle ? searchParams.get('title')?.slice(0, 100) : 'My default title'
+    const depositTimestamp = searchParams.get('depositTimestamp')
 
-    return new ImageResponse(
-      (
-        <div
-          style={{
-            backgroundColor: 'black',
-            backgroundSize: '150px 150px',
-            height: '100%',
-            width: '100%',
-            display: 'flex',
-            textAlign: 'center',
-            alignItems: 'center',
-            justifyContent: 'center',
-            flexDirection: 'column',
-            flexWrap: 'nowrap',
-          }}
-        >
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              justifyItems: 'center',
-            }}
-          >
-            <Image
-              alt="Vercel"
-              height={200}
-              src="data:image/svg+xml,%3Csvg width='116' height='100' fill='white' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M57.5 0L115 100H0L57.5 0z' /%3E%3C/svg%3E"
-              style={{ margin: '0 30px' }}
-              width={232}
-            />
-          </div>
-          <div
-            style={{
-              fontSize: 60,
-              fontStyle: 'normal',
-              letterSpacing: '-0.025em',
-              color: 'white',
-              marginTop: 30,
-              padding: '0 120px',
-              lineHeight: 1.4,
-              whiteSpace: 'pre-wrap',
-            }}
-          >
-            {title}
-          </div>
-        </div>
-      ),
-      {
-        width: 1200,
-        height: 630,
-      },
-    )
+    return new ImageResponse(<UI depositTimestamp={Number(depositTimestamp)} />, {
+      width: 1200,
+      height: 630,
+    })
   } catch (e: any) {
     console.log(`${e.message}`)
     return new Response(`Failed to generate the image`, {
