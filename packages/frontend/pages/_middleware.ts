f1b3ec38bee4fd0af1bcb04981b26f1f628f9797
KM Koushik <koushikmohan1996@gmail.com>
2023-01-16 18:58:40 +1100

    Add CF country code (#920)

--- packages/frontend/pages/_middleware.ts
+++ packages/frontend/pages/_middleware.ts
@@ -4,6 +4,10 @@ export default function middleware(request: NextRequest) {
   const country = request.geo?.country
   const url = request.nextUrl
 
+  const cloudfareCountry = request.headers.get('cf-ipcountry')
+
+  console.log('country', cloudfareCountry, country)
+
   if (url.searchParams.has('ct') && url.searchParams.get('ct') === String(country)) {
     return NextResponse.next()
   }
