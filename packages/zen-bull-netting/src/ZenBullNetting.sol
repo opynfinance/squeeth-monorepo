33172e775430ee6a569ecfd8320c2e156279604a
Haythem Sellami <sellamihaythem96@gmail.com>
2023-01-16 13:37:47 +0100

    feat: owner setters

--- packages/zen-bull-netting/src/ZenBullNetting.sol
+++ packages/zen-bull-netting/src/ZenBullNetting.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.13;
 
-// interface
-import { IERC20 } from "openzeppelin/token/ERC20/IERC20.sol";
 // contract
 import { Ownable } from "openzeppelin/access/Ownable.sol";
 import { EIP712 } from "openzeppelin/utils/cryptography/draft-EIP712.sol";
@@ -17,9 +15,8 @@ contract ZenBullNetting is Ownable, EIP712 {
     bytes32 private constant _ZENBULL_NETTING_TYPEHASH = keccak256(
         "Order(uint256 bidId,address trader,uint256 quantity,uint256 price,bool isBuying,uint256 expiry,uint256 nonce)"
     );
-    /// @dev OTC price tolerance cannot exceed 20%
+    // @dev OTC price tolerance cannot exceed 20%
     uint256 public constant MAX_OTC_PRICE_TOLERANCE = 2e17; // 20%
-    /// @dev min auction TWAP
     uint32 public constant MIN_AUCTION_TWAP = 180 seconds;
 
     /// @dev owner sets to true when starting auction
@@ -38,17 +35,6 @@ contract ZenBullNetting is Ownable, EIP712 {
     /// @dev twap period to use for auction calculations
     uint32 public auctionTwapPeriod;
 
-    /// @dev WETH token address
-    address private weth;
-
-    /// @dev array of WETH deposit receipts
-    Receipt[] public deposits;
-
-    /// @dev WEth amount to deposit for an address
-    mapping(address => uint256) public wethBalance;
-    /// @dev indexes of deposit receipts of an address
-    mapping(address => uint256[]) public userDepositsIndex;
-
     /// @dev order struct for a signed order from market maker
     struct Order {
         uint256 bidId;
@@ -63,16 +49,6 @@ contract ZenBullNetting is Ownable, EIP712 {
         bytes32 s;
     }
 
-    /// @dev receipt used to store deposits and withdraws
-    struct Receipt {
-        /// @dev address of the depositor or withdrawer
-        address sender;
-        /// @dev usdc amount to queue for deposit or crab amount to queue for withdrawal
-        uint256 amount;
-        /// @dev time of deposit
-        uint256 timestamp;
-    }
-
     event SetMinBullAmount(uint256 oldAmount, uint256 newAmount);
     event SetMinWethAmount(uint256 oldAmount, uint256 newAmount);
     event SetDepositsIndex(uint256 oldDepositsIndex, uint256 newDepositsIndex);
@@ -80,19 +56,10 @@ contract ZenBullNetting is Ownable, EIP712 {
     event SetAuctionTwapPeriod(uint32 previousTwap, uint32 newTwap);
     event SetOTCPriceTolerance(uint256 previousTolerance, uint256 newOtcPriceTolerance);
     event ToggledAuctionLive(bool isAuctionLive);
-    event QueueWeth(
-        address indexed depositor,
-        uint256 amount,
-        uint256 depositorsBalance,
-        uint256 indexed receiptIndex
-    );
-    event DequeueWeth(address indexed depositor, uint256 amount, uint256 depositorsBalance);
 
-    constructor(address _weth) EIP712("ZenBullNetting", "1") {
+    constructor() EIP712("ZenBullNetting", "1") {
         otcPriceTolerance = 5e16; // 5%
         auctionTwapPeriod = 420 seconds;
-
-        weth = _weth;
     }
 
     /**
@@ -120,8 +87,8 @@ contract ZenBullNetting is Ownable, EIP712 {
     }
 
     /**
-     * @notice set minBullAmount
-     * @param _amount the number to be set as minBullAmount
+     * @notice set minCrabAmount
+     * @param _amount the number to be set as minCrab
      */
     function setMinBullAmount(uint256 _amount) external onlyOwner {
         emit SetMinBullAmount(minBullAmount, _amount);
@@ -154,7 +121,7 @@ contract ZenBullNetting is Ownable, EIP712 {
      * @param _auctionTwapPeriod the twap period, in seconds
      */
     function setAuctionTwapPeriod(uint32 _auctionTwapPeriod) external onlyOwner {
-        require(_auctionTwapPeriod >= MIN_AUCTION_TWAP, "ZBN01");
+        require(_auctionTwapPeriod >= MIN_AUCTION_TWAP, "N18");
 
         emit SetAuctionTwapPeriod(auctionTwapPeriod, _auctionTwapPeriod);
 
@@ -167,83 +134,10 @@ contract ZenBullNetting is Ownable, EIP712 {
      */
     function setOTCPriceTolerance(uint256 _otcPriceTolerance) external onlyOwner {
         // Tolerance cannot be more than 20%
-        require(_otcPriceTolerance <= MAX_OTC_PRICE_TOLERANCE, "ZBN02");
+        require(_otcPriceTolerance <= MAX_OTC_PRICE_TOLERANCE, "N19");
 
         emit SetOTCPriceTolerance(otcPriceTolerance, _otcPriceTolerance);
 
         otcPriceTolerance = _otcPriceTolerance;
-    }
-
-    /**
-     * @notice queue USDC for deposit into crab strategy
-     * @param _amount USDC amount to deposit
-     */
-    function queueWeth(uint256 _amount) external {
-        require(_amount >= minWethAmount, "ZBN03");
-
-        IERC20(weth).transferFrom(msg.sender, address(this), _amount);
-
-        // update weth balance of user, add their receipt, and receipt index to user deposits index
-        wethBalance[msg.sender] = wethBalance[msg.sender] + _amount;
-        deposits.push(Receipt(msg.sender, _amount, block.timestamp));
-        userDepositsIndex[msg.sender].push(deposits.length - 1);
-
-        emit QueueWeth(msg.sender, _amount, wethBalance[msg.sender], deposits.length - 1);
-    }
-
-    /**
-     * @notice withdraw WETH from queue
-     * @param _amount WETH amount to dequeue
-     * @param _force forceWithdraw if deposited more than a week ago
-     */
-    function dequeueWeth(uint256 _amount, bool _force) external {
-        require(!isAuctionLive || _force, "ZBN04");
-
-        wethBalance[msg.sender] = wethBalance[msg.sender] - _amount;
-
-        require(wethBalance[msg.sender] >= minWethAmount || wethBalance[msg.sender] == 0, "ZBN05");
-
-        // start withdrawing from the users last deposit
-        uint256 toWithdraw = _amount;
-        uint256 lastDepositIndex = userDepositsIndex[msg.sender].length;
-        for (uint256 i = lastDepositIndex; i > 0; i--) {
-            Receipt storage receipt = deposits[userDepositsIndex[msg.sender][i - 1]];
-
-            if (_force) {
-                require(block.timestamp > receipt.timestamp + 1 weeks, "ZBN06");
-            }
-            if (receipt.amount > toWithdraw) {
-                receipt.amount -= toWithdraw;
-                break;
-            } else {
-                toWithdraw -= receipt.amount;
-                delete deposits[userDepositsIndex[msg.sender][i - 1]];
-                userDepositsIndex[msg.sender].pop();
-            }
-        }
-
-        IERC20(weth).transfer(msg.sender, _amount);
-
-        emit DequeueWeth(msg.sender, _amount, wethBalance[msg.sender]);
-    }
-
-    /**
-     * @notice get the sum of queued WETH
-     * @return sum WETH amount in queue
-     */
-    function depositsQueued() external view returns (uint256) {
-        uint256 j = depositsIndex;
-        uint256 sum;
-        while (j < deposits.length) {
-            sum = sum + deposits[j].amount;
-            j++;
-        }
-        return sum;
-    }
-
-    function getDepositReceipt(uint256 _index) external view returns (address, uint256, uint256) {
-        Receipt memory receipt = deposits[_index];
-
-        return (receipt.sender, receipt.amount, receipt.timestamp);
     }
 }
--- packages/zen-bull-netting/test/unit-test/ZenBullNettingUnit.t.sol
+++ packages/zen-bull-netting/test/unit-test/ZenBullNettingUnit.t.sol
@@ -4,18 +4,15 @@ pragma abicoder v2;
 
 // test dependency
 import { console } from "forge-std/console.sol";
-import { ZenBullNettingBaseSetup } from "../ZenBullNettingBaseSetup.t.sol";
-//interface
-import { IERC20 } from "openzeppelin/token/ERC20/IERC20.sol";
+import { ZenBullNettingBaseSetup } from "../ZenBullNettingBaseSetup.sol";
 
 /**
  * Unit tests
  */
 contract ZenBullNettingUnit is ZenBullNettingBaseSetup {
+
     uint256 public user1Pk;
     address public user1;
-
-    uint256 minWeth = 5e18;
 
     function setUp() public override {
         ZenBullNettingBaseSetup.setUp();
@@ -24,10 +21,6 @@ contract ZenBullNettingUnit is ZenBullNettingBaseSetup {
         user1 = vm.addr(user1Pk);
 
         vm.label(user1, "User1");
-
-        // some WETH rich address
-        vm.prank(0x57757E3D981446D585Af0D9Ae4d7DF6D64647806);
-        IERC20(WETH).transfer(user1, 5000e18);
     }
 
     function testOwner() public {
@@ -54,127 +47,5 @@ contract ZenBullNettingUnit is ZenBullNettingBaseSetup {
         vm.prank(user1);
         vm.expectRevert(bytes("Ownable: caller is not the owner"));
         zenBullNetting.toggleAuctionLive();
-    }
-
-    function testSetMinWethAmount() public {
-        _setMinWethAmount();
-        assertEq(zenBullNetting.minWethAmount(), minWeth);
-    }
-
-    function testSetMinWethAmountWhenCallerNotOwner() public {
-        vm.prank(user1);
-        vm.expectRevert(bytes("Ownable: caller is not the owner"));
-        zenBullNetting.setMinWethAmount(minWeth);
-    }
-
-    function testSetMinBullAmount() public {
-        vm.prank(owner);
-        zenBullNetting.setMinBullAmount(3e18);
-
-        assertEq(zenBullNetting.minBullAmount(), 3e18);
-    }
-
-    function testSetMinBullAmountWhenCallerNotOwner() public {
-        vm.prank(user1);
-        vm.expectRevert(bytes("Ownable: caller is not the owner"));
-        zenBullNetting.setMinBullAmount(3e18);
-    }
-
-    function testSetDepositsIndex() public {
-        vm.prank(owner);
-        zenBullNetting.setDepositsIndex(2);
-
-        assertEq(zenBullNetting.depositsIndex(), 2);
-    }
-
-    function testSetDepositsIndexWhenCallerNotOwner() public {
-        vm.prank(user1);
-        vm.expectRevert(bytes("Ownable: caller is not the owner"));
-        zenBullNetting.setDepositsIndex(2);
-    }
-
-    function testSetWithdrawsIndex() public {
-        vm.prank(owner);
-        zenBullNetting.setWithdrawsIndex(4);
-
-        assertEq(zenBullNetting.withdrawsIndex(), 4);
-    }
-
-    function testSetWithdrawsIndexWhenCallerNotOwner() public {
-        vm.prank(user1);
-        vm.expectRevert(bytes("Ownable: caller is not the owner"));
-        zenBullNetting.setWithdrawsIndex(2);
-    }
-
-    function testSetAuctionTwapPeriod() public {
-        vm.prank(owner);
-        zenBullNetting.setAuctionTwapPeriod(42069);
-
-        assertEq(zenBullNetting.auctionTwapPeriod(), 42069);
-    }
-
-    function testSetAuctionTwapPeriodWhenCallerNotOwner() public {
-        vm.prank(user1);
-        vm.expectRevert(bytes("Ownable: caller is not the owner"));
-        zenBullNetting.setAuctionTwapPeriod(42069);
-    }
-
-    function testSetAuctionTwapPeriodWhenPeriodLessThanMinimum() public {
-        uint32 minAuctionTwap = zenBullNetting.MIN_AUCTION_TWAP();
-        vm.prank(owner);
-        vm.expectRevert(bytes("ZBN01"));
-        zenBullNetting.setAuctionTwapPeriod(minAuctionTwap - 1);
-    }
-
-    function testSetOTCPriceTolerance() public {
-        vm.prank(owner);
-        zenBullNetting.setOTCPriceTolerance(2e16);
-
-        assertEq(zenBullNetting.otcPriceTolerance(), 2e16);
-    }
-
-    function testSetOTCPriceToleranceWhenCallerNotOwner() public {
-        vm.prank(user1);
-        vm.expectRevert(bytes("Ownable: caller is not the owner"));
-        zenBullNetting.setOTCPriceTolerance(2e16);
-    }
-
-    function testSetOTCPriceToleranceWhenPeriodLessThanMinimum() public {
-        uint256 maxOtcPriceTolerance = zenBullNetting.MAX_OTC_PRICE_TOLERANCE();
-        vm.prank(owner);
-        vm.expectRevert(bytes("ZBN02"));
-        zenBullNetting.setOTCPriceTolerance(maxOtcPriceTolerance + 1);
-    }
-
-    function testQueueWeth() public {
-        uint256 amount = 100e18;
-        uint256 user1WethBalanceBefore = IERC20(WETH).balanceOf(user1);
-        uint256 zenBullNettingWethBalanceBefore = IERC20(WETH).balanceOf(address(zenBullNetting));
-
-        _queueWeth(user1, amount);
-
-        assertEq(zenBullNetting.wethBalance(user1), amount);
-        assertEq(
-            IERC20(WETH).balanceOf(address(zenBullNetting)) - zenBullNettingWethBalanceBefore,
-            amount
-        );
-        assertEq(IERC20(WETH).balanceOf(user1) + amount, user1WethBalanceBefore);
-        assertEq(zenBullNetting.depositsQueued(), amount);
-    }
-
-    function testQueueWethWhenAmountLessThanMinAmount() public {
-        _setMinWethAmount();
-        uint256 amount = zenBullNetting.minWethAmount() - 1;
-
-        vm.startPrank(user1);
-        IERC20(WETH).approve(address(zenBullNetting), amount);
-        vm.expectRevert(bytes("ZBN03"));
-        zenBullNetting.queueWeth(amount);
-        vm.stopPrank();
-    }
-
-    function _setMinWethAmount() internal {
-        vm.prank(owner);
-        zenBullNetting.setMinWethAmount(minWeth);
     }
 }
