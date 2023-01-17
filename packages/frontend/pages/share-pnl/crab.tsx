2594cd497a4e8ae0f7c559ef63fb8a55d5272031
nikhil arora <narora200@gmail.com>
2023-01-17 12:05:57 +0530

    feat: add more meta tags

--- packages/frontend/pages/share-pnl/crab.tsx
+++ packages/frontend/pages/share-pnl/crab.tsx
@@ -10,17 +10,32 @@ const SharePnl = () => {
   const pnl = router.query.pnl
   const strategy = 'crab'
 
+  const url = 'https://squeeth.opyn.co'
+  const title = `Opyn Crab Strategy - Stack USDC`
+  const description = `Stack USDC when ETH is flat`
   const ogImageUrl = `${SQUEETH_BASE_URL}/api/pnl?strategy=${strategy}&depositedAt=${depositedAt}&pnl=${pnl}`
 
   return (
     <>
       <Head>
-        <title>Opyn Crab Strategy - Stack USDC</title>
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Opyn Crab Strategy - Stack USDC" />
-        <meta name="twitter:description" content="Stack USDC when ETH is flat" />
-        <meta name="twitter:image" content={ogImageUrl} />
+        {/* Primary Meta Tags */}
+        <title>{title}</title>
+        <meta name="title" content={title} />
+        <meta name="description" content={description} />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
         <meta property="og:image" content={ogImageUrl} />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={url} />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={ogImageUrl} />
       </Head>
 
       <Box marginTop="20px" marginLeft="20px">
