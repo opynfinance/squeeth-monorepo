e875084ee5f6a0eb006e54c0c21cc7d125e4a6a2
Haythem Sellami <17862704+haythemsellami@users.noreply.github.com>
2023-01-18 12:22:38 +0100

    Feat: owner setters functions (#925)
    * feat: owner setters
    
    * Lint
    
    * fix
    
    * testing
    
    * lint
    
    * fix
    
    * add comment
    
    * lint

---
+++ packages/zen-bull-netting/src/ZenBullNetting.sol
@@ -0,0 +1,144 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+
+// contract
+import { Ownable } from "openzeppelin/access/Ownable.sol";
+import { EIP712 } from "openzeppelin/utils/cryptography/draft-EIP712.sol";
+
+/**
+ * @dev ZenBullNetting contract
+ * @notice Contract for Netting Deposits and Withdrawals in ZenBull
+ * @author Opyn team
+ */
+contract ZenBullNetting is Ownable, EIP712 {
+    /// @dev typehash for signed orders
+    bytes32 private constant _ZENBULL_NETTING_TYPEHASH = keccak256(
+        "Order(uint256 bidId,address trader,uint256 quantity,uint256 price,bool isBuying,uint256 expiry,uint256 nonce)"
+    );
+    /// @dev OTC price tolerance cannot exceed 20%
+    uint256 public constant MAX_OTC_PRICE_TOLERANCE = 2e17; // 20%
+    /// @dev min auction TWAP
+    uint32 public constant MIN_AUCTION_TWAP = 180 seconds;
+
+    /// @dev owner sets to true when starting auction
+    bool public isAuctionLive;
+
+    /// @dev min WETH amounts to withdraw or deposit via netting
+    uint256 public minWethAmount;
+    /// @dev min ZenBull amounts to withdraw or deposit via netting
+    uint256 public minBullAmount;
+    /// @dev array index of last processed deposits
+    uint256 public depositsIndex;
+    /// @dev array index of last processed withdraws
+    uint256 public withdrawsIndex;
+    // @dev OTC price must be within this distance of the uniswap twap price
+    uint256 public otcPriceTolerance;
+    /// @dev twap period to use for auction calculations
+    uint32 public auctionTwapPeriod;
+
+    /// @dev order struct for a signed order from market maker
+    struct Order {
+        uint256 bidId;
+        address trader;
+        uint256 quantity;
+        uint256 price;
+        bool isBuying;
+        uint256 expiry;
+        uint256 nonce;
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+    }
+
+    event SetMinBullAmount(uint256 oldAmount, uint256 newAmount);
+    event SetMinWethAmount(uint256 oldAmount, uint256 newAmount);
+    event SetDepositsIndex(uint256 oldDepositsIndex, uint256 newDepositsIndex);
+    event SetWithdrawsIndex(uint256 oldWithdrawsIndex, uint256 newWithdrawsIndex);
+    event SetAuctionTwapPeriod(uint32 previousTwap, uint32 newTwap);
+    event SetOTCPriceTolerance(uint256 previousTolerance, uint256 newOtcPriceTolerance);
+    event ToggledAuctionLive(bool isAuctionLive);
+
+    constructor() EIP712("ZenBullNetting", "1") {
+        otcPriceTolerance = 5e16; // 5%
+        auctionTwapPeriod = 420 seconds;
+    }
+
+    /**
+     * @dev view function to get the domain seperator used in signing
+     */
+    function DOMAIN_SEPARATOR() external view returns (bytes32) {
+        return _domainSeparatorV4();
+    }
+
+    /**
+     * @dev toggles the value of isAuctionLive
+     */
+    function toggleAuctionLive() external onlyOwner {
+        isAuctionLive = !isAuctionLive;
+        emit ToggledAuctionLive(isAuctionLive);
+    }
+
+    /**
+     * @notice set min Weth amount
+     * @param _amount the amount to be set as minWethAmount
+     */
+    function setMinWethAmount(uint256 _amount) external onlyOwner {
+        emit SetMinWethAmount(minWethAmount, _amount);
+        minWethAmount = _amount;
+    }
+
+    /**
+     * @notice set minBullAmount
+     * @param _amount the number to be set as minBullAmount
+     */
+    function setMinBullAmount(uint256 _amount) external onlyOwner {
+        emit SetMinBullAmount(minBullAmount, _amount);
+
+        minBullAmount = _amount;
+    }
+
+    /**
+     * @notice set the depositIndex so that we want to skip processing some deposits
+     * @param _newDepositsIndex the new deposits index
+     */
+    function setDepositsIndex(uint256 _newDepositsIndex) external onlyOwner {
+        emit SetDepositsIndex(depositsIndex, _newDepositsIndex);
+
+        depositsIndex = _newDepositsIndex;
+    }
+
+    /**
+     * @notice set the withdraw index so that we want to skip processing some withdraws
+     * @param _newWithdrawsIndex the new withdraw index
+     */
+    function setWithdrawsIndex(uint256 _newWithdrawsIndex) external onlyOwner {
+        emit SetWithdrawsIndex(withdrawsIndex, _newWithdrawsIndex);
+
+        withdrawsIndex = _newWithdrawsIndex;
+    }
+
+    /**
+     * @notice owner can set the twap period in seconds that is used for obtaining TWAP prices
+     * @param _auctionTwapPeriod the twap period, in seconds
+     */
+    function setAuctionTwapPeriod(uint32 _auctionTwapPeriod) external onlyOwner {
+        require(_auctionTwapPeriod >= MIN_AUCTION_TWAP, "ZBN01");
+
+        emit SetAuctionTwapPeriod(auctionTwapPeriod, _auctionTwapPeriod);
+
+        auctionTwapPeriod = _auctionTwapPeriod;
+    }
+
+    /**
+     * @notice owner can set a threshold, scaled by 1e18 that determines the maximum discount of a clearing sale price to the current uniswap twap price
+     * @param _otcPriceTolerance the OTC price tolerance, in percent, scaled by 1e18
+     */
+    function setOTCPriceTolerance(uint256 _otcPriceTolerance) external onlyOwner {
+        // Tolerance cannot be more than 20%
+        require(_otcPriceTolerance <= MAX_OTC_PRICE_TOLERANCE, "ZBN02");
+
+        emit SetOTCPriceTolerance(otcPriceTolerance, _otcPriceTolerance);
+
+        otcPriceTolerance = _otcPriceTolerance;
+    }
+}
---
+++ packages/zen-bull-netting/test/unit-test/ZenBullNettingUnit.t.sol
@@ -0,0 +1,142 @@
+pragma solidity ^0.8.13;
+
+pragma abicoder v2;
+
+// test dependency
+import { console } from "forge-std/console.sol";
+import { ZenBullNettingBaseSetup } from "../ZenBullNettingBaseSetup.t.sol";
+
+/**
+ * Unit tests
+ */
+contract ZenBullNettingUnit is ZenBullNettingBaseSetup {
+    uint256 public user1Pk;
+    address public user1;
+
+    function setUp() public override {
+        ZenBullNettingBaseSetup.setUp();
+
+        user1Pk = 0xA12CD;
+        user1 = vm.addr(user1Pk);
+
+        vm.label(user1, "User1");
+    }
+
+    function testOwner() public {
+        assertEq(zenBullNetting.owner(), owner);
+    }
+
+    function testDeployment() public {
+        assertEq(zenBullNetting.MAX_OTC_PRICE_TOLERANCE(), 2e17);
+        assertEq(zenBullNetting.MIN_AUCTION_TWAP(), 180);
+        assertEq(zenBullNetting.otcPriceTolerance(), 5e16);
+        assertEq(zenBullNetting.auctionTwapPeriod(), 420);
+    }
+
+    function testToggleAuctionLive() public {
+        assertEq(zenBullNetting.isAuctionLive(), false);
+
+        vm.prank(owner);
+        zenBullNetting.toggleAuctionLive();
+
+        assertEq(zenBullNetting.isAuctionLive(), true);
+    }
+
+    function testToggleAuctionLiveWhenCallerNotOwner() public {
+        vm.prank(user1);
+        vm.expectRevert(bytes("Ownable: caller is not the owner"));
+        zenBullNetting.toggleAuctionLive();
+    }
+
+    function testSetMinWethAmount() public {
+        vm.prank(owner);
+        zenBullNetting.setMinWethAmount(5e18);
+
+        assertEq(zenBullNetting.minWethAmount(), 5e18);
+    }
+
+    function testSetMinWethAmountWhenCallerNotOwner() public {
+        vm.prank(user1);
+        vm.expectRevert(bytes("Ownable: caller is not the owner"));
+        zenBullNetting.setMinWethAmount(5e18);
+    }
+
+    function testSetMinBullAmount() public {
+        vm.prank(owner);
+        zenBullNetting.setMinBullAmount(3e18);
+
+        assertEq(zenBullNetting.minBullAmount(), 3e18);
+    }
+
+    function testSetMinBullAmountWhenCallerNotOwner() public {
+        vm.prank(user1);
+        vm.expectRevert(bytes("Ownable: caller is not the owner"));
+        zenBullNetting.setMinBullAmount(3e18);
+    }
+
+    function testSetDepositsIndex() public {
+        vm.prank(owner);
+        zenBullNetting.setDepositsIndex(2);
+
+        assertEq(zenBullNetting.depositsIndex(), 2);
+    }
+
+    function testSetDepositsIndexWhenCallerNotOwner() public {
+        vm.prank(user1);
+        vm.expectRevert(bytes("Ownable: caller is not the owner"));
+        zenBullNetting.setDepositsIndex(2);
+    }
+
+    function testSetWithdrawsIndex() public {
+        vm.prank(owner);
+        zenBullNetting.setWithdrawsIndex(4);
+
+        assertEq(zenBullNetting.withdrawsIndex(), 4);
+    }
+
+    function testSetWithdrawsIndexWhenCallerNotOwner() public {
+        vm.prank(user1);
+        vm.expectRevert(bytes("Ownable: caller is not the owner"));
+        zenBullNetting.setWithdrawsIndex(2);
+    }
+
+    function testSetAuctionTwapPeriod() public {
+        vm.prank(owner);
+        zenBullNetting.setAuctionTwapPeriod(42069);
+
+        assertEq(zenBullNetting.auctionTwapPeriod(), 42069);
+    }
+
+    function testSetAuctionTwapPeriodWhenCallerNotOwner() public {
+        vm.prank(user1);
+        vm.expectRevert(bytes("Ownable: caller is not the owner"));
+        zenBullNetting.setAuctionTwapPeriod(42069);
+    }
+
+    function testSetAuctionTwapPeriodWhenPeriodLessThanMinimum() public {
+        uint32 minAuctionTwap = zenBullNetting.MIN_AUCTION_TWAP();
+        vm.prank(owner);
+        vm.expectRevert(bytes("ZBN01"));
+        zenBullNetting.setAuctionTwapPeriod(minAuctionTwap - 1);
+    }
+
+    function testSetOTCPriceTolerance() public {
+        vm.prank(owner);
+        zenBullNetting.setOTCPriceTolerance(2e16);
+
+        assertEq(zenBullNetting.otcPriceTolerance(), 2e16);
+    }
+
+    function testSetOTCPriceToleranceWhenCallerNotOwner() public {
+        vm.prank(user1);
+        vm.expectRevert(bytes("Ownable: caller is not the owner"));
+        zenBullNetting.setOTCPriceTolerance(2e16);
+    }
+
+    function testSetOTCPriceToleranceWhenPeriodLessThanMinimum() public {
+        uint256 maxOtcPriceTolerance = zenBullNetting.MAX_OTC_PRICE_TOLERANCE();
+        vm.prank(owner);
+        vm.expectRevert(bytes("ZBN02"));
+        zenBullNetting.setOTCPriceTolerance(maxOtcPriceTolerance + 1);
+    }
+}
