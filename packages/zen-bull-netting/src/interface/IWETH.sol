a18197e1010699a29451dec1fc6a8bf1b8cc1999
Haythem Sellami <sellamihaythem96@gmail.com>
2023-01-19 14:07:48 +0100

    net at price

--- packages/zen-bull-netting/src/interface/IWETH.sol
+++ packages/zen-bull-netting/src/interface/IWETH.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import { IERC20 } from "openzeppelin/token/ERC20/IERC20.sol";
+import { IERC20 } from "openzeppelin/interfaces/IERC20.sol";
 
 interface IWETH is IERC20 {
     function deposit() external payable;
