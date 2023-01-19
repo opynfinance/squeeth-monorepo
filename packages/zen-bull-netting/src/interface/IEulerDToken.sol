708b2d0c4ea42eec0e8ae6a6e5fac1102bf8b8e7
Haythem Sellami <sellamihaythem96@gmail.com>
2023-01-19 14:10:17 +0100

    clean

--- packages/zen-bull-netting/src/interface/IEulerDToken.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity =0.7.6;
-
-interface IEulerDToken {
-    function balanceOf(address account) external view returns (uint256);
-    function borrow(uint256 subAccountId, uint256 amount) external;
-    function repay(uint256 subAccountId, uint256 amount) external;
-}
--- packages/zen-bull-netting/src/interface/IEulerEToken.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity =0.7.6;
-
-interface IEulerEToken {
-    function balanceOf(address account) external view returns (uint256);
-    function balanceOfUnderlying(address account) external view returns (uint256);
-    function deposit(uint256 subAccountId, uint256 amount) external;
-    function withdraw(uint256 subAccountId, uint256 amount) external;
-}
