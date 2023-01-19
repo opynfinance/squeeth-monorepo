a0530ffea2d2c1d2ce116c03f3ac90ab9847d635
Haythem Sellami <sellamihaythem96@gmail.com>
2023-01-19 12:34:56 +0100

    revert changes'

---
+++ packages/frontend/middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const cloudfareCountry = request.headers.get('cf-ipcountry')
+  const country = cloudfareCountry ?? request.geo?.country
+  const url = request.nextUrl
+
+  console.log('country', cloudfareCountry, country)
+
+  if (url.searchParams.has('ct') && url.searchParams.get('ct') === String(country)) {
+    return NextResponse.next()
+  }
+
+  url.searchParams.set('ct', country!)
+  return NextResponse.redirect(url)
+}
+
+/*
+  matcher for excluding public assets/api routes/_next
+  link: https://github.com/vercel/next.js/discussions/36308#discussioncomment-3758041
+*/
+export const config = {
+  matcher: '/((?!api|static|.*\\..*|_next).*)',
+}
