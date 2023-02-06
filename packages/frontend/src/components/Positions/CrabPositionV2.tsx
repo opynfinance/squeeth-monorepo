2e699b59e17de280033268126c6a460287b1aa05
nikhil arora <narora200@gmail.com>
2023-02-06 17:50:44 +0530

    style: crab v2 ui ready

--- packages/frontend/src/components/Positions/CrabPositionV2.tsx
+++ packages/frontend/src/components/Positions/CrabPositionV2.tsx
@@ -1,15 +1,20 @@
 import { Tooltip, Typography } from '@material-ui/core'
 import BigNumber from 'bignumber.js'
 import { useAtomValue } from 'jotai'
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import InfoIcon from '@material-ui/icons/InfoOutlined'
+import clsx from 'clsx'
 
 import { useCurrentCrabPositionValue, useCurrentCrabPositionValueV2, useSetStrategyData } from '@state/crab/hooks'
 import { crabStrategyCollatRatioAtom, usdcQueuedAtom, crabQueuedAtom, crabUSDValueAtom } from '@state/crab/atoms'
 import { Tooltips, USDC_DECIMALS } from '@constants/index'
 import { toTokenAmount } from '@utils/calculations'
-import { formatCurrency } from '@utils/formatter'
+import { formatCurrency, formatNumber } from '@utils/formatter'
 import useStyles from './useStyles'
+
+const Loading: React.FC<{ isSmall?: boolean }> = ({ isSmall = false }) => {
+  return <Typography variant={isSmall ? 'caption' : 'body1'}>loading...</Typography>
+}
 
 type CrabPositionV2Type = {
   depositedEth: BigNumber
@@ -23,13 +28,11 @@ type CrabPositionV2Type = {
 }
 
 const CrabPositionV2: React.FC<CrabPositionV2Type> = ({
-  depositedEth,
   depositedUsd,
   loading,
   pnlWMidPriceInPerct,
   pnlWMidPriceInUSD,
   currentCrabPositionValue,
-  currentCrabPositionValueInETH,
   version,
 }) => {
   const classes = useStyles()
@@ -49,6 +52,9 @@ const CrabPositionV2: React.FC<CrabPositionV2Type> = ({
   const initiatedDepositAmount = toTokenAmount(usdcQueued, USDC_DECIMALS)
   const initiatedWithdrawalAmount = toTokenAmount(crabQueued, 18).times(toTokenAmount(crabUsdValue, 18))
 
+  const showInitiatedDeposit = usdcQueued.isGreaterThan('100')
+  const showInitiatedWithdrawal = crabQueued.isGreaterThan('10000000000')
+
   return (
     <div className={classes.position} id="pos-page-crab">
       <div className={classes.positionTitle}>
@@ -59,78 +65,119 @@ const CrabPositionV2: React.FC<CrabPositionV2Type> = ({
       </div>
       <div className={classes.shortPositionData}>
         <div className={classes.innerPositionData}>
-          <div style={{ width: '50%' }}>
-            <Typography variant="caption" component="span" color="textSecondary">
-              Deposited Amount
-            </Typography>
-            <Tooltip title={Tooltips.CrabMigratedDeposit}>
-              <InfoIcon fontSize="small" className={classes.infoIcon} />
-            </Tooltip>
-            <Typography variant="body1">$ {depositedUsd.toFixed(2)}</Typography>
-            <Typography variant="body2" color="textSecondary">
-              <span id="pos-page-crab-deposited-amount">{depositedEth.toFixed(6)}</span>
-              &nbsp; ETH
+          <div className={classes.positionColumn}>
+            <div className={classes.titleWithTooltip}>
+              <Typography variant="caption" component="span" color="textSecondary">
+                Deposited Amount
+              </Typography>
+              <Tooltip title={Tooltips.CrabMigratedDeposit}>
+                <InfoIcon fontSize="small" className={classes.infoIcon} />
+              </Tooltip>
+            </div>
+            <Typography variant="body1" className={classes.textMonospace}>
+              {formatCurrency(depositedUsd.toNumber())}
             </Typography>
           </div>
-          <div style={{ width: '50%' }}>
-            <Typography variant="caption" component="span" color="textSecondary">
+
+          <div className={classes.positionColumn}>
+            <Typography variant="caption" color="textSecondary">
               Current Position
             </Typography>
-            <Typography variant="body1">{!loading ? `$ ${currentCrabPositionValue.toFixed(2)}` : 'Loading'}</Typography>
-            <Typography variant="body2" color="textSecondary">
-              {!loading ? `${currentCrabPositionValueInETH.toFixed(6)}  ETH` : 'Loading'}
-            </Typography>
-          </div>
-        </div>
-        <div className={classes.innerPositionData} style={{ marginTop: '16px' }}>
-          <div style={{ width: '50%' }}>
-            <Typography variant="caption" component="span" color="textSecondary">
-              Unrealized P&L
-            </Typography>
-            <Tooltip title={Tooltips.CrabPnL}>
-              <InfoIcon fontSize="small" className={classes.infoIcon} />
-            </Tooltip>
-            <Typography
-              variant="body1"
-              className={pnlWMidPriceInUSD.isLessThan(0) ? classes.red : classes.green}
-              id="pos-page-crab-pnl-amount"
-            >
-              {!loading ? '$' + `${pnlWMidPriceInUSD.toFixed(2)}` : 'Loading'}
-            </Typography>
-            <Typography variant="caption" className={pnlWMidPriceInPerct.isLessThan(0) ? classes.red : classes.green}>
-              {!loading ? `${pnlWMidPriceInPerct.toFixed(2)}` + '%' : 'Loading'}
-            </Typography>
+
+            {loading ? (
+              <Loading />
+            ) : (
+              <Typography variant="body1" className={classes.textMonospace}>
+                {formatCurrency(currentCrabPositionValue.toNumber())}
+              </Typography>
+            )}
           </div>
         </div>
 
-        <div className={classes.innerPositionData}>
-          {/* ignore dust amount */}
-          {usdcQueued.isGreaterThan('100') && (
-            <div style={{ width: '50%', marginTop: '16px' }}>
+        <div className={clsx(classes.innerPositionData, classes.rowMarginTop)}>
+          <div className={classes.positionColumn}>
+            <div className={classes.titleWithTooltip}>
               <Typography variant="caption" component="span" color="textSecondary">
-                Initiated Deposit
+                Unrealized P&L
               </Typography>
-              <Tooltip title={Tooltips.InitiatedDeposit}>
+              <Tooltip title={Tooltips.CrabPnL}>
                 <InfoIcon fontSize="small" className={classes.infoIcon} />
               </Tooltip>
-              <Typography variant="body1">
-                {!loading ? formatCurrency(Number(initiatedDepositAmount)) : 'Loading'}
+            </div>
+
+            {loading ? (
+              <Loading />
+            ) : (
+              <Typography
+                variant="body1"
+                className={clsx(classes.textMonospace, pnlWMidPriceInUSD.isLessThan(0) ? classes.red : classes.green)}
+                id="pos-page-crab-pnl-amount"
+              >
+                {formatCurrency(pnlWMidPriceInUSD.toNumber())}
               </Typography>
+            )}
+
+            {loading ? (
+              <Loading isSmall />
+            ) : (
+              <Typography
+                variant="caption"
+                className={clsx(classes.textMonospace, pnlWMidPriceInPerct.isLessThan(0) ? classes.red : classes.green)}
+              >
+                {pnlWMidPriceInPerct.isPositive() && '+'}
+                {formatNumber(pnlWMidPriceInPerct.toNumber()) + '%'}
+              </Typography>
+            )}
+          </div>
+        </div>
+
+        <div
+          className={clsx(
+            classes.innerPositionData,
+            showInitiatedDeposit && showInitiatedWithdrawal ? classes.rowMarginTop : '',
+          )}
+        >
+          {/* ignore dust amount */}
+          {showInitiatedDeposit && (
+            <div className={classes.positionColumn}>
+              <div className={classes.titleWithTooltip}>
+                <Typography variant="caption" component="span" color="textSecondary">
+                  Initiated Deposit
+                </Typography>
+                <Tooltip title={Tooltips.InitiatedDeposit}>
+                  <InfoIcon fontSize="small" className={classes.infoIcon} />
+                </Tooltip>
+              </div>
+
+              {loading ? (
+                <Loading />
+              ) : (
+                <Typography variant="body1" className={classes.textMonospace}>
+                  {formatCurrency(initiatedDepositAmount.toNumber())}
+                </Typography>
+              )}
             </div>
           )}
 
           {/* ignore dust amount */}
-          {crabQueued.isGreaterThan('10000000000') && (
-            <div style={{ width: '50%', marginTop: '16px' }}>
-              <Typography variant="caption" component="span" color="textSecondary">
-                Initiated Withdrawal
-              </Typography>
-              <Tooltip title={Tooltips.InitiatedWithdrawal}>
-                <InfoIcon fontSize="small" className={classes.infoIcon} />
-              </Tooltip>
-              <Typography variant="body1">
-                {!loading ? formatCurrency(Number(initiatedWithdrawalAmount)) : 'Loading'}
-              </Typography>
+          {showInitiatedWithdrawal && (
+            <div className={classes.positionColumn}>
+              <div className={classes.titleWithTooltip}>
+                <Typography variant="caption" component="span" color="textSecondary">
+                  Initiated Withdrawal
+                </Typography>
+                <Tooltip title={Tooltips.InitiatedWithdrawal}>
+                  <InfoIcon fontSize="small" className={classes.infoIcon} />
+                </Tooltip>
+              </div>
+
+              {loading ? (
+                <Loading />
+              ) : (
+                <Typography variant="body1" className={classes.textMonospace}>
+                  {formatCurrency(initiatedWithdrawalAmount.toNumber())}
+                </Typography>
+              )}
             </div>
           )}
         </div>
