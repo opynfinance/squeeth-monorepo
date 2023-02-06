6d8b8fedcb58be2bbb7cc5dba5b1785c5c65c5bb
nikhil arora <narora200@gmail.com>
2023-02-06 16:34:12 +0530

    style: short squeeth ui ready

--- packages/frontend/src/components/Positions/ShortSqueeth.tsx
+++ packages/frontend/src/components/Positions/ShortSqueeth.tsx
@@ -1,22 +1,25 @@
 import { Tooltip, Typography } from '@material-ui/core'
-import {
-  useComputeSwaps,
-  useFirstValidVault,
-  useLPPositionsQuery,
-  useShortRealizedPnl,
-} from 'src/state/positions/hooks'
-import useStyles from './useStyles'
-import Link from 'next/link'
 import { useAtomValue } from 'jotai'
-import { loadingAtom } from 'src/state/crab/atoms'
-import { useCurrentShortPositionValue, useShortGain, useShortUnrealizedPNL } from 'src/state/pnl/hooks'
+import Link from 'next/link'
 import InfoIcon from '@material-ui/icons/InfoOutlined'
-import { Tooltips } from '../../constants'
+import BigNumber from 'bignumber.js'
+import clsx from 'clsx'
+
+import { useComputeSwaps, useFirstValidVault, useLPPositionsQuery, useShortRealizedPnl } from '@state/positions/hooks'
+import { loadingAtom } from '@state/crab/atoms'
+import { useCurrentShortPositionValue, useShortGain, useShortUnrealizedPNL } from '@state/pnl/hooks'
+import { Tooltips } from '@constants/index'
 import { useVaultData } from '@hooks/useVaultData'
 import { HidePnLText } from '@components/HidePnLText'
-import { isToHidePnLAtom } from 'src/state/positions/atoms'
-import { PnLType } from '../../types'
+import { isToHidePnLAtom } from '@state/positions/atoms'
+import { PnLType } from 'src/types'
 import { PnLTooltip } from '@components/PnLTooltip'
+import { formatCurrency, formatNumber } from '@utils/formatter'
+import useStyles from './useStyles'
+
+const Loading = () => {
+  return <Typography variant="body1">loading...</Typography>
+}
 
 export default function ShortSqueeth() {
   const classes = useStyles()
@@ -41,90 +44,126 @@ export default function ShortSqueeth() {
       </div>
       <div className={classes.shortPositionData}>
         <div className={classes.innerPositionData}>
-          <div style={{ width: '50%' }}>
+          <div className={classes.positionColumn}>
             <Typography variant="caption" component="span" color="textSecondary">
               oSQTH Amount
             </Typography>
-            {isPositionLoading ? (
-              <Typography variant="body1">Loading</Typography>
+
+            {isPositionLoading && squeethAmount.isEqualTo(0) ? (
+              <Loading />
             ) : (
-              <Typography variant="body1" id="pos-page-short-osqth-bal">
-                {squeethAmount.toFixed(8) + ' oSQTH'}
+              <Typography variant="body1">
+                <Typography component="span" id="pos-page-short-osqth-bal" className={classes.textMonospace}>
+                  {formatNumber(squeethAmount.toNumber(), 6)}
+                </Typography>
+                &nbsp; oSQTH
               </Typography>
             )}
           </div>
-          <div style={{ width: '50%' }}>
+          <div className={classes.positionColumn}>
             <Typography variant="caption" component="span" color="textSecondary">
               Position Value
             </Typography>
-            {isPositionLoading ? (
-              <Typography variant="body1">Loading</Typography>
+
+            {isPnLLoading && shortPositionValue.isEqualTo(0) ? (
+              <Loading />
             ) : (
-              <Typography variant="body1">
-                {isPnLLoading && shortPositionValue.isEqualTo(0) ? 'Loading' : '$' + shortPositionValue.toFixed(2)}
+              <Typography variant="body1" className={classes.textMonospace}>
+                {formatCurrency(shortPositionValue.toNumber())}
               </Typography>
             )}
           </div>
         </div>
         <div className={classes.innerPositionData} style={{ marginTop: '16px' }}>
-          <div style={{ width: '50%' }}>
-            <Typography variant="caption" component="span" color="textSecondary">
-              Liquidation Price
-            </Typography>
-            <Tooltip title={Tooltips.LiquidationPrice}>
-              <InfoIcon fontSize="small" className={classes.infoIcon} />
-            </Tooltip>
-            <Typography variant="body1">
-              {isVaultLoading && existingLiqPrice.isEqualTo(0) ? 'Loading' : '$' + existingLiqPrice.toFixed(2)}
-            </Typography>
+          <div className={classes.positionColumn}>
+            <div className={classes.titleWithTooltip}>
+              <Typography variant="caption" color="textSecondary">
+                Liquidation Price
+              </Typography>
+              <Tooltip title={Tooltips.LiquidationPrice}>
+                <InfoIcon fontSize="small" className={classes.infoIcon} />
+              </Tooltip>
+            </div>
+
+            {isVaultLoading && existingLiqPrice.isEqualTo(0) ? (
+              <Loading />
+            ) : (
+              <Typography variant="body1" className={classes.textMonospace}>
+                {formatCurrency(existingLiqPrice.toNumber())}
+              </Typography>
+            )}
           </div>
-          <div style={{ width: '50%' }}>
+          <div className={classes.positionColumn}>
             <Typography variant="caption" component="span" color="textSecondary">
               Collateral (Amt / Ratio)
             </Typography>
-            <Typography variant="body1">
-              {isVaultLoading && existingCollat.isEqualTo(0) ? 'Loading' : existingCollat.toFixed(4)} ETH (
-              {existingCollatPercent}%)
-            </Typography>
+
+            {isVaultLoading && existingCollat.isEqualTo(0) ? (
+              <Loading />
+            ) : (
+              <Typography variant="body1" className={classes.textMonospace}>
+                {formatNumber(existingCollat.toNumber(), 4)} ETH ({formatNumber(existingCollatPercent)}%)
+              </Typography>
+            )}
           </div>
         </div>
-        {isToHidePnL ? (
-          <HidePnLText />
-        ) : (
-          <div className={classes.innerPositionData} style={{ marginTop: '16px' }}>
-            <div style={{ width: '50%' }}>
-              <div className={classes.pnlTitle}>
-                <Typography variant="caption" component="span" color="textSecondary">
-                  Unrealized P&L
-                </Typography>
-                <PnLTooltip pnlType={PnLType.Unrealized} />
-              </div>
-              {isPositionLoading || shortUnrealizedPNL.loading ? (
-                <Typography variant="body1">Loading</Typography>
-              ) : (
-                <>
-                  <Typography variant="body1" className={shortGain.isLessThan(0) ? classes.red : classes.green}>
-                    $ {shortUnrealizedPNL.usd.toFixed(2)} ({shortUnrealizedPNL.eth.toFixed(5)} ETH)
+
+        <div className={classes.rowMarginTop}>
+          {isToHidePnL ? (
+            <HidePnLText isSmall />
+          ) : (
+            <div className={classes.innerPositionData}>
+              <div className={classes.positionColumn}>
+                <div className={classes.pnlTitle}>
+                  <Typography variant="caption" component="span" color="textSecondary">
+                    Unrealized P&L
                   </Typography>
-                  <Typography variant="caption" className={shortGain.isLessThan(0) ? classes.red : classes.green}>
-                    {(shortGain || 0).toFixed(2)}%
-                  </Typography>
-                </>
-              )}
-            </div>
-            <div style={{ width: '50%' }}>
-              <div className={classes.pnlTitle}>
-                <Typography variant="caption" component="span" color="textSecondary">
-                  Realized P&L
-                </Typography>
-                <PnLTooltip pnlType={PnLType.Realized} />
+                  <PnLTooltip pnlType={PnLType.Unrealized} />
+                </div>
+
+                {isPositionLoading || shortUnrealizedPNL.loading ? (
+                  <Loading />
+                ) : (
+                  <>
+                    <Typography
+                      variant="body1"
+                      className={clsx(classes.textMonospace, shortGain.isLessThan(0) ? classes.red : classes.green)}
+                    >
+                      {formatCurrency(shortUnrealizedPNL.usd.toNumber())} (
+                      {formatNumber(shortUnrealizedPNL.eth.toNumber(), 4)} ETH)
+                    </Typography>
+                    <Typography
+                      variant="caption"
+                      className={clsx(classes.textMonospace, shortGain.isLessThan(0) ? classes.red : classes.green)}
+                    >
+                      {shortGain.isPositive() && '+'}
+                      {formatNumber(shortGain?.toNumber() ?? 0)}%
+                    </Typography>
+                  </>
+                )}
               </div>
-              <Typography variant="body1" className={shortRealizedPNL.gte(0) ? classes.green : classes.red}>
-                $ {swapsLoading ? 'Loading' : shortRealizedPNL.toFixed(2)}
-              </Typography>
+              <div className={classes.positionColumn}>
+                <div className={classes.pnlTitle}>
+                  <Typography variant="caption" component="span" color="textSecondary">
+                    Realized P&L
+                  </Typography>
+                  <PnLTooltip pnlType={PnLType.Realized} />
+                </div>
+
+                {swapsLoading ? (
+                  <Loading />
+                ) : (
+                  <Typography
+                    variant="body1"
+                    className={clsx(classes.textMonospace, shortRealizedPNL.gte(0) ? classes.green : classes.red)}
+                  >
+                    {formatCurrency(shortRealizedPNL.toNumber())}
+                  </Typography>
+                )}
+              </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </div>
   )
