3ff601eb73a409f2d4166a992f03ad15c5d03eec
nikhil arora <narora200@gmail.com>
2023-02-06 16:50:04 +0530

    style: lped squeeth done

--- packages/frontend/src/components/Positions/LPedSqueeth.tsx
+++ packages/frontend/src/components/Positions/LPedSqueeth.tsx
@@ -1,10 +1,16 @@
-import { useVaultData } from '@hooks/useVaultData'
 import { Link, Tooltip, Typography } from '@material-ui/core'
 import BigNumber from 'bignumber.js'
-import { useFirstValidVault, useLpDebt } from 'src/state/positions/hooks'
-import useStyles from './useStyles'
 import InfoIcon from '@material-ui/icons/InfoOutlined'
-import { Tooltips } from '../../constants'
+
+import { useVaultData } from '@hooks/useVaultData'
+import { useFirstValidVault, useLpDebt } from '@state/positions/hooks'
+import { Tooltips } from '@constants/index'
+import { formatNumber, formatCurrency } from '@utils/formatter'
+import useStyles from './useStyles'
+
+const Loading = () => {
+  return <Typography variant="body1">loading...</Typography>
+}
 
 interface Props {
   vaultExists: boolean
@@ -19,45 +25,59 @@ export default function LPedSqueeth({ vaultExists }: Props) {
   return (
     <div className={classes.position}>
       <div className={classes.positionTitle}>
-        <Typography>LPed Squeeth</Typography>
+        <Typography>LP&apos;ed Squeeth</Typography>
         <Typography className={classes.link} id="lp-vault-link">
           {vaultExists ? <Link href={`vault/${vaultId}`}>Manage</Link> : null}
         </Typography>
       </div>
       <div className={classes.shortPositionData}>
         <div className={classes.innerPositionData}>
-          <div style={{ width: '50%' }}>
+          <div className={classes.positionColumn}>
             <Typography variant="caption" component="span" color="textSecondary">
               Amount
             </Typography>
-            <Typography variant="body1">
-              <span id="pos-page-lped-osqth-bal">{lpedSqueeth.toFixed(8)}</span>
-              &nbsp; oSQTH
+            <Typography variant="body1" className={classes.textMonospace}>
+              <span id="pos-page-lped-osqth-bal">{formatNumber(lpedSqueeth.toNumber(), 6)}</span> oSQTH
             </Typography>
           </div>
         </div>
         <div className={classes.innerPositionData} style={{ marginTop: '16px' }}>
           {new BigNumber(existingLiqPrice).isFinite() ? (
-            <div style={{ width: '50%' }}>
-              <Typography variant="caption" component="span" color="textSecondary">
-                Liquidation Price
-              </Typography>
-              <Tooltip title={Tooltips.LiquidationPrice}>
-                <InfoIcon fontSize="small" className={classes.infoIcon} />
-              </Tooltip>
-              <Typography variant="body1">
-                ${isVaultLoading && existingLiqPrice.isEqualTo(0) ? 'Loading' : existingLiqPrice.toFixed(2)}
-              </Typography>
+            <div className={classes.positionColumn}>
+              <div className={classes.titleWithTooltip}>
+                <Typography variant="caption" component="span" color="textSecondary">
+                  Liquidation Price
+                </Typography>
+                <Tooltip title={Tooltips.LiquidationPrice}>
+                  <InfoIcon fontSize="small" className={classes.infoIcon} />
+                </Tooltip>
+              </div>
+
+              {isVaultLoading && existingLiqPrice.isEqualTo(0) ? (
+                <Loading />
+              ) : (
+                <Typography variant="body1" className={classes.textMonospace}>
+                  {formatCurrency(existingLiqPrice.toNumber())}
+                </Typography>
+              )}
             </div>
           ) : null}
-          <div style={{ width: '50%' }}>
+
+          <div className={classes.positionColumn}>
             <Typography variant="caption" component="span" color="textSecondary">
               Collateral (Amt / Ratio)
             </Typography>
-            <Typography variant="body1">
-              {isVaultLoading && existingCollat.isEqualTo(0) ? 'Loading' : existingCollat.toFixed(4)} ETH
-              {new BigNumber(existingCollatPercent).isFinite() ? ' (' + existingCollatPercent + ' %)' : null}
-            </Typography>
+
+            {isVaultLoading && existingCollat.isEqualTo(0) ? (
+              <Loading />
+            ) : (
+              <Typography variant="body1" className={classes.textMonospace}>
+                {formatNumber(existingCollat.toNumber(), 4)} ETH
+                {new BigNumber(existingCollatPercent).isFinite()
+                  ? ' (' + formatNumber(existingCollatPercent) + '%)'
+                  : null}
+              </Typography>
+            )}
           </div>
         </div>
       </div>
