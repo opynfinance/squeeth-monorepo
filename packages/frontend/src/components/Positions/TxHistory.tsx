17f472d74de5782863103f14f6b7b43d8007222b
nikhil arora <narora200@gmail.com>
2023-02-08 14:05:09 +0530

    style: tx history responsiveness

--- packages/frontend/src/components/Positions/TxHistory.tsx
+++ packages/frontend/src/components/Positions/TxHistory.tsx
@@ -1,4 +1,4 @@
-import { createStyles, IconButton, makeStyles, Typography } from '@material-ui/core'
+import { createStyles, makeStyles, Typography, useTheme, useMediaQuery } from '@material-ui/core'
 import OpenInNewIcon from '@material-ui/icons/OpenInNew'
 import { useAtomValue } from 'jotai'
 import clsx from 'clsx'
@@ -10,8 +10,9 @@ import { useUsdAmount } from '@hooks/useUsdAmount'
 import { networkIdAtom } from '@state/wallet/atoms'
 import { useETHPrice } from '@hooks/useETHPrice'
 import { normFactorAtom } from '@state/controller/atoms'
-import useCommonStyles from './useStyles'
 import { formatCurrency, formatNumber } from '@utils/formatter'
+import { LinkButton } from '@components/Button'
+import useCommonStyles from './useStyles'
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -39,12 +40,41 @@ const useStyles = makeStyles((theme) =>
       borderRadius: theme.spacing(1),
       display: 'flex',
       justifyContent: 'space-between',
+      alignItems: 'center',
+      flexWrap: 'wrap',
+      width: '100%',
+      gridGap: '12px',
     },
-    txItemCol: {
-      width: '30%',
+    itemHeaderCol: {
+      flexBasis: '200px',
+      [theme.breakpoints.down('xs')]: {
+        flexBasis: '100%',
+        marginBottom: theme.spacing(1),
+      },
+    },
+    itemDataCol: {
+      flexBasis: '380px',
+      flex: 1,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gridGap: '12px',
+
+      [theme.breakpoints.down('xs')]: {
+        flexBasis: '280px',
+      },
     },
     txItemVal: {
-      width: '30%',
+      flex: 1,
+      flexBasis: '120px',
+    },
+    txItemCTA: {
+      textAlign: 'right',
+      flexBasis: '144px',
+
+      [theme.breakpoints.down('xs')]: {
+        flexBasis: '40px',
+      },
     },
   }),
 )
@@ -55,9 +85,12 @@ const TxHistory: React.FC = () => {
   const ethPrice = useETHPrice()
   const normalizationFactor = useAtomValue(normFactorAtom)
 
+  const { getUsdAmt } = useUsdAmount()
+
+  const theme = useTheme()
   const classes = useStyles()
   const commonClasses = useCommonStyles()
-  const { getUsdAmt } = useUsdAmount()
+  const isMobileBreakpoint = useMediaQuery(theme.breakpoints.down('xs'))
 
   if (loading) {
     return (
@@ -79,99 +112,109 @@ const TxHistory: React.FC = () => {
     <>
       {transactions.map((tx, index) => (
         <div className={classes.historyItem} key={tx.timestamp + index}>
-          <div className={classes.txItemCol}>
-            <Typography variant="body2">{tx.transactionType}</Typography>
+          <div className={classes.itemHeaderCol}>
+            <Typography variant="body2" className={commonClasses.fontMedium}>
+              {tx.transactionType}
+            </Typography>
             <Typography variant="caption" color="textSecondary" className={commonClasses.textMonospace}>
               {new Date(Number(tx.timestamp) * 1000).toDateString()}
             </Typography>
           </div>
-          {tx.transactionType === TransactionType.CRAB_FLASH_DEPOSIT ||
-          tx.transactionType === TransactionType.CRAB_FLASH_WITHDRAW ||
-          tx.transactionType === TransactionType.BULL_FLASH_DEPOSIT ||
-          tx.transactionType === TransactionType.BULL_FLASH_WITHDRAW ? (
-            <>
-              <div className={classes.txItemVal}>
-                <Typography
-                  variant="body2"
-                  className={clsx(
-                    commonClasses.textMonospace,
-                    tx.transactionType === TransactionType.CRAB_FLASH_WITHDRAW ||
-                      tx.transactionType === TransactionType.BULL_FLASH_WITHDRAW
-                      ? classes.red
-                      : classes.green,
-                  )}
-                >
-                  {formatNumber(tx.ethAmount.toNumber(), 4)} WETH
-                </Typography>
-                <Typography variant="caption" color="textSecondary" className={commonClasses.textMonospace}>
-                  {formatCurrency(getUsdAmt(tx.ethAmount, tx.timestamp).toNumber())}
-                </Typography>
-              </div>
-              <div className={classes.txItemVal} />
-            </>
-          ) : (
-            <>
-              <div className={classes.txItemVal}>
-                {tx.transactionType != TransactionType.OTC_DEPOSIT &&
-                tx.transactionType != TransactionType.OTC_WITHDRAW ? (
-                  <>
-                    <Typography
-                      variant="body2"
-                      className={clsx(
-                        commonClasses.textMonospace,
-                        tx.transactionType === TransactionType.BUY ||
-                          tx.transactionType === TransactionType.BURN_SHORT ||
-                          tx.transactionType === TransactionType.REMOVE_LIQUIDITY
-                          ? classes.green
-                          : classes.red,
-                      )}
-                    >
-                      {formatNumber(tx.squeethAmount.toNumber(), 6)} oSQTH
-                    </Typography>
-                    <Typography variant="caption" color="textSecondary" className={commonClasses.textMonospace}>
-                      {formatCurrency(
-                        tx.squeethAmount
-                          .times(ethPrice)
-                          .times(ethPrice)
-                          .div(10000)
-                          .times(normalizationFactor)
-                          .toNumber(),
-                      )}
-                    </Typography>
-                  </>
-                ) : null}
-              </div>
-              <div className={classes.txItemVal}>
-                <Typography
-                  variant="body2"
-                  className={clsx(
-                    commonClasses.textMonospace,
-                    tx.transactionType === TransactionType.BUY ||
-                      tx.transactionType === TransactionType.BURN_SHORT ||
-                      tx.transactionType === TransactionType.ADD_LIQUIDITY
-                      ? classes.red
-                      : classes.green,
-                  )}
-                >
-                  {tx.transactionType === TransactionType.CRAB_V2_USDC_FLASH_DEPOSIT ||
-                  tx.transactionType === TransactionType.CRAB_V2_USDC_FLASH_WITHDRAW
-                    ? `${formatNumber(tx.usdValue.toNumber(), 2)} USDC`
-                    : `${formatNumber(tx.ethAmount.toNumber(), 4)} WETH`}
-                </Typography>
-                <Typography variant="caption" color="textSecondary" className={commonClasses.textMonospace}>
-                  {tx.transactionType === TransactionType.CRAB_V2_USDC_FLASH_DEPOSIT ||
-                  tx.transactionType === TransactionType.CRAB_V2_USDC_FLASH_WITHDRAW
-                    ? `${formatNumber(tx.ethAmount.toNumber(), 4)} WETH`
-                    : `${formatCurrency(tx.usdValue.toNumber())}`}
-                </Typography>
-              </div>
-            </>
-          )}
 
-          <div>
-            <IconButton size="small" href={`${EtherscanPrefix[networkId]}/${tx.txId}`} target="_blank">
-              <OpenInNewIcon style={{ fontSize: '16px' }} color="secondary" />
-            </IconButton>
+          <div className={classes.itemDataCol}>
+            {tx.transactionType === TransactionType.CRAB_FLASH_DEPOSIT ||
+            tx.transactionType === TransactionType.CRAB_FLASH_WITHDRAW ||
+            tx.transactionType === TransactionType.BULL_FLASH_DEPOSIT ||
+            tx.transactionType === TransactionType.BULL_FLASH_WITHDRAW ? (
+              <>
+                <div className={classes.txItemVal}>
+                  <Typography
+                    variant="body2"
+                    className={clsx(
+                      commonClasses.textMonospace,
+                      tx.transactionType === TransactionType.CRAB_FLASH_WITHDRAW ||
+                        tx.transactionType === TransactionType.BULL_FLASH_WITHDRAW
+                        ? classes.red
+                        : classes.green,
+                    )}
+                  >
+                    {formatNumber(tx.ethAmount.toNumber(), 4)} WETH
+                  </Typography>
+                  <Typography variant="caption" color="textSecondary" className={commonClasses.textMonospace}>
+                    {formatCurrency(getUsdAmt(tx.ethAmount, tx.timestamp).toNumber())}
+                  </Typography>
+                </div>
+                <div className={classes.txItemVal} />
+              </>
+            ) : (
+              <>
+                <div className={classes.txItemVal}>
+                  {tx.transactionType != TransactionType.OTC_DEPOSIT &&
+                  tx.transactionType != TransactionType.OTC_WITHDRAW ? (
+                    <>
+                      <Typography
+                        variant="body2"
+                        className={clsx(
+                          commonClasses.textMonospace,
+                          tx.transactionType === TransactionType.BUY ||
+                            tx.transactionType === TransactionType.BURN_SHORT ||
+                            tx.transactionType === TransactionType.REMOVE_LIQUIDITY
+                            ? classes.green
+                            : classes.red,
+                        )}
+                      >
+                        {formatNumber(tx.squeethAmount.toNumber(), 4)} oSQTH
+                      </Typography>
+                      <Typography variant="caption" color="textSecondary" className={commonClasses.textMonospace}>
+                        {formatCurrency(
+                          tx.squeethAmount
+                            .times(ethPrice)
+                            .times(ethPrice)
+                            .div(10000)
+                            .times(normalizationFactor)
+                            .toNumber(),
+                        )}
+                      </Typography>
+                    </>
+                  ) : null}
+                </div>
+                <div className={classes.txItemVal}>
+                  <Typography
+                    variant="body2"
+                    className={clsx(
+                      commonClasses.textMonospace,
+                      tx.transactionType === TransactionType.BUY ||
+                        tx.transactionType === TransactionType.BURN_SHORT ||
+                        tx.transactionType === TransactionType.ADD_LIQUIDITY
+                        ? classes.red
+                        : classes.green,
+                    )}
+                  >
+                    {tx.transactionType === TransactionType.CRAB_V2_USDC_FLASH_DEPOSIT ||
+                    tx.transactionType === TransactionType.CRAB_V2_USDC_FLASH_WITHDRAW
+                      ? `${formatNumber(tx.usdValue.toNumber(), 2)} USDC`
+                      : `${formatNumber(tx.ethAmount.toNumber(), 4)} WETH`}
+                  </Typography>
+                  <Typography variant="caption" color="textSecondary" className={commonClasses.textMonospace}>
+                    {tx.transactionType === TransactionType.CRAB_V2_USDC_FLASH_DEPOSIT ||
+                    tx.transactionType === TransactionType.CRAB_V2_USDC_FLASH_WITHDRAW
+                      ? `${formatNumber(tx.ethAmount.toNumber(), 4)} WETH`
+                      : `${formatCurrency(tx.usdValue.toNumber())}`}
+                  </Typography>
+                </div>
+              </>
+            )}
+
+            <div className={classes.txItemCTA}>
+              <LinkButton size="small" href={`${EtherscanPrefix[networkId]}/${tx.txId}`}>
+                {!isMobileBreakpoint && (
+                  <Typography variant="body2" color="primary" component="span">
+                    View Transaction
+                  </Typography>
+                )}
+                <OpenInNewIcon style={{ fontSize: '16px', marginLeft: '8px' }} color="primary" />
+              </LinkButton>
+            </div>
           </div>
         </div>
       ))}
