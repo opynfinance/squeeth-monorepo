2643151e6ed713395bc4d65194a5bfaf34facedf
KMKoushik <koushikmohan1996@gmail.com>
2023-08-29 17:10:58 +1000

    Add blocked.tsx

---
+++ packages/frontend/pages/blocked.tsx
@@ -0,0 +1,12 @@
+import { getMarkdown } from '@utils/markdown'
+import MarkdownPage from '@components/MarkdownPage'
+
+const BlockedPage = (props: any) => {
+  return (
+    <div>
+      Seems you are using an VPN service, which is a violation of our terms of service.
+    </div>
+  )
+}
+
+export default BlockedPage
