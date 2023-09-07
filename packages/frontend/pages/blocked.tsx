1aa85a0453a295fe53edf22c13b02f71cbd4398f
KMKoushik <koushikmohan1996@gmail.com>
2023-09-08 09:37:33 +1000

    Update styles

--- packages/frontend/pages/blocked.tsx
+++ packages/frontend/pages/blocked.tsx
@@ -1,11 +1,33 @@
-import { getMarkdown } from '@utils/markdown'
-import MarkdownPage from '@components/MarkdownPage'
+import React from 'react'
+import { Box, Typography } from '@material-ui/core'
+import { createStyles, makeStyles } from '@material-ui/core/styles'
 
-const BlockedPage = (props: any) => {
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    container: {
+      width: '50%',
+      justifyContent: 'center',
+      display: 'flex',
+      alignItems: 'center',
+      margin: 'auto',
+      marginTop: theme.spacing(10),
+    },
+    title: {
+      marginTop: theme.spacing(10),
+    },
+  }),
+)
+
+const BlockedPage: React.FC = () => {
+  const classes = useStyles()
+
   return (
-    <div>
-      Seems you are using an VPN service, which is a violation of our terms of service.
-    </div>
+    <Box className={classes.container}>
+      <Typography align="center" variant="h6" className={classes.title}>
+        Seems you are using a VPN service, which is a violation of our terms of service. Please disconnect from your VPN
+        and refresh the page to continue using our service.
+      </Typography>
+    </Box>
   )
 }
 
