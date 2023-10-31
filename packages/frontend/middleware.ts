25ea00f17710a7f8214e86c19f9db0e162060be7
Anand Narayan <i@codeanand.com>
2023-10-31 14:59:52 -0700

    third layer of vpn check via ipqs ENG-1789 (#1006)
    * third layer of vpn check via ipqs
    
    * added ips for non cloudflare deployments like test deployments
    
    * added console logs for debugging this has to be removed laste
    
    * moved to fetch and add blocked to not run middleware
    
    * added ip whitelisting
    
    * removed unused vars and added country wording to blocked page

--- packages/frontend/middleware.ts
+++ packages/frontend/middleware.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { Redis } from '@upstash/redis'
+import { isVPN } from 'src/server/ipqs'
 
 const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL!,
@@ -11,15 +12,25 @@ export async function middleware(request: NextRequest) {
   const country = cloudflareCountry ?? request.geo?.country
   const url = request.nextUrl
 
-  const ip = request.headers.get('cf-connecting-ip')
+  const ip = request.headers.get('cf-connecting-ip') || request.headers.get('x-forwarded-for') || request.ip
 
-  if (ip) {
+  const allowedIPs = (process.env.WHITELISTED_IPS || '').split(',')
+  const isIPWhitelisted = ip && allowedIPs.includes(ip)
+
+  if (ip && !isIPWhitelisted) {
     const redisData = await redis.get(ip)
     const isIPBlocked = !!redisData
 
     console.log('ip', ip, isIPBlocked, url.protocol, url.host, '/blocked')
 
     if (isIPBlocked && url.pathname !== '/blocked') {
+      return NextResponse.redirect(`${url.protocol}//${url.host}/blocked`)
+    }
+
+    const isFromVpn = await isVPN(ip)
+    if (isFromVpn && url.pathname !== '/blocked') {
+      await redis.set(ip, 1)
+      console.log('vpnip', ip, isFromVpn, '/blocked')
       return NextResponse.redirect(`${url.protocol}//${url.host}/blocked`)
     }
   }
@@ -39,5 +50,5 @@ export async function middleware(request: NextRequest) {
   link: https://github.com/vercel/next.js/discussions/36308#discussioncomment-3758041
 */
 export const config = {
-  matcher: '/((?!api|static|.*\\..*|_next).*)',
+  matcher: '/((?!api|static|.*\\..*|_next|blocked).*)',
 }
