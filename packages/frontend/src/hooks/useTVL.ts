14327a85aae585545a0c03926c7869fb38e93f92
nikhil arora <narora200@gmail.com>
2024-05-13 15:02:44 +0400

    fix: fix tvl

--- packages/frontend/src/hooks/useTVL.ts
+++ packages/frontend/src/hooks/useTVL.ts
@@ -2,15 +2,16 @@ import { useQuery } from 'react-query'
 
 export function useTVL() {
   const { data } = useQuery('tvl', async () => {
-    if (!process.env.NEXT_PUBLIC_DEFILLAMA_ENDPOINT) return
-
-    const data = await fetch(`${process.env.NEXT_PUBLIC_DEFILLAMA_ENDPOINT}/tvl/opyn`, {
+    const tvlResponse = await fetch('/api/tvl', {
       method: 'GET',
       headers: { 'Content-Type': 'application/json' },
     })
 
-    const tvl: any = await data.json()
+    if (!tvlResponse.ok) {
+      throw new Error('Network response was not ok')
+    }
 
+    const tvl = await tvlResponse.json()
     return Math.round(tvl / 1e6)
   })
 
