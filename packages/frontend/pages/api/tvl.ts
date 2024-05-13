e14c2f53b6868b87638f8e34c52aaf49d4ddd142
nikhil <narora200@gmail.com>
2024-05-13 17:20:12 +0530

    fix: fix tvl (#1029)
    * fix: fix tvl
    
    * console
    
    * log more
    
    * remove logs

---
+++ packages/frontend/pages/api/tvl.ts
@@ -0,0 +1,19 @@
+import axios from 'axios'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const handleRequest = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'GET') {
+    try {
+      const response = await axios.get(`${process.env.DEFILLAMA_ENDPOINT}/tvl/opyn`)
+      res.status(200).json(response.data)
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching data from Defillama' })
+    }
+  } else {
+    // Handle any other HTTP method
+    res.setHeader('Allow', ['GET'])
+    res.status(405).end(`Method ${req.method} Not Allowed`)
+  }
+}
+
+export default handleRequest
