f76a455b9f20827ca945815f204783b2d692b994
nikhil arora <narora200@gmail.com>
2024-05-13 15:35:32 +0400

    remove logs

--- packages/frontend/pages/api/tvl.ts
+++ packages/frontend/pages/api/tvl.ts
@@ -2,13 +2,11 @@ import axios from 'axios'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 const handleRequest = async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log({ endpoint: process.env.DEFILLAMA_ENDPOINT })
   if (req.method === 'GET') {
     try {
       const response = await axios.get(`${process.env.DEFILLAMA_ENDPOINT}/tvl/opyn`)
       res.status(200).json(response.data)
     } catch (error) {
-      console.error('Error fetching data from Defillama:', { error })
       res.status(500).json({ error: 'Error fetching data from Defillama' })
     }
   } else {
