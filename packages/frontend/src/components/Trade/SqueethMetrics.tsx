849dbe0971770c57f9bb157e07b25b4705cfe5f9
nikhil arora <narora200@gmail.com>
2024-11-04 20:31:29 +0400

    fix eth settlement price

--- packages/frontend/src/components/Trade/SqueethMetrics.tsx
+++ packages/frontend/src/components/Trade/SqueethMetrics.tsx
@@ -6,6 +6,7 @@ import { normFactorAtom, indexForSettlementAtom } from '@state/controller/atoms'
 import { formatCurrency, formatNumber } from '@utils/formatter'
 import Metric, { MetricLabel } from '@components/Metric'
 import { Tooltips } from '@constants/enums'
+import { INDEX_SCALE } from '@constants/index'
 
 const SqueethMetrics: React.FC<BoxProps> = (props) => {
   const normFactor = useAtomValue(normFactorAtom)
@@ -15,7 +16,7 @@ const SqueethMetrics: React.FC<BoxProps> = (props) => {
     <Box display="flex" alignItems="center" flexWrap="wrap" gridGap="12px" {...props}>
       <Metric
         label={<MetricLabel label="ETH Settlement Price" tooltipTitle={'ETH Settlement Price'} />}
-        value={formatCurrency(indexForSettlement.toNumber())}
+        value={formatCurrency(indexForSettlement.times(INDEX_SCALE).toNumber())}
       />
 
       <Metric
